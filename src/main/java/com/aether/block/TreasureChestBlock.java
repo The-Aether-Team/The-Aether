b1efcfa1c962a1cb703620b500c4d6126d7c58c5
raptor4694 <raptor4694@gmail.com>
2020-11-02 17:15:56 -0800

    Fixed Treasure Chest lid not opening

--- src/main/java/com/aether/block/TreasureChestBlock.java
+++ src/main/java/com/aether/block/TreasureChestBlock.java
@@ -1,8 +1,5 @@
 package com.aether.block;
 
-import static net.minecraft.block.ChestBlock.getDirectionToAttached;
-
-import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.function.BiFunction;
@@ -16,91 +13,38 @@ import com.aether.item.DungeonKeyItem;
 import com.aether.tileentity.AetherTileEntityTypes;
 import com.aether.tileentity.TreasureChestTileEntity;
 
-import it.unimi.dsi.fastutil.floats.Float2FloatFunction;
-import net.minecraft.block.AbstractChestBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ChestBlock;
-import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.IWaterLoggable;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.passive.CatEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.fluid.Fluids;
-import net.minecraft.fluid.IFluidState;
 import net.minecraft.inventory.DoubleSidedInventory;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.inventory.container.ChestContainer;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
-import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.pathfinding.PathType;
-import net.minecraft.state.BooleanProperty;
-import net.minecraft.state.DirectionProperty;
-import net.minecraft.state.EnumProperty;
-import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.state.properties.ChestType;
-import net.minecraft.stats.Stat;
-import net.minecraft.stats.Stats;
-import net.minecraft.tileentity.IChestLid;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityMerger;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.Rotation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.shapes.ISelectionContext;
-import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.DistExecutor.SafeRunnable;
 
-public class TreasureChestBlock extends AbstractChestBlock<TreasureChestTileEntity> implements IWaterLoggable {
-	public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
-	public static final EnumProperty<ChestType> TYPE = BlockStateProperties.CHEST_TYPE;
-	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-	protected static final VoxelShape SHAPE_NORTH = Block.makeCuboidShape(1.0, 0.0, 0.0, 15.0, 14.0, 15.0);
-	protected static final VoxelShape SHAPE_SOUTH = Block.makeCuboidShape(1.0, 0.0, 1.0, 15.0, 14.0, 16.0);
-	protected static final VoxelShape SHAPE_WEST = Block.makeCuboidShape(0.0, 0.0, 1.0, 15.0, 14.0, 15.0);
-	protected static final VoxelShape SHAPE_EAST = Block.makeCuboidShape(1.0, 0.0, 1.0, 16.0, 14.0, 15.0);
-	protected static final VoxelShape SHAPE = Block.makeCuboidShape(1.0, 0.0, 1.0, 15.0, 14.0, 15.0);
-
-	private static final TileEntityMerger.ICallback<TreasureChestTileEntity, Optional<IInventory>> field_220109_i = new TileEntityMerger.ICallback<TreasureChestTileEntity, Optional<IInventory>>() {
-		@Override
-		public Optional<IInventory> func_225539_a_(TreasureChestTileEntity chest1, TreasureChestTileEntity chest2) {
-			return Optional.of(new DoubleSidedInventory(chest1, chest2));
-		}
-
-		@Override
-		public Optional<IInventory> func_225538_a_(TreasureChestTileEntity chest) {
-			return Optional.of(chest);
-		}
-
-		@Override
-		public Optional<IInventory> func_225537_b_() {
-			return Optional.empty();
-		}
-	};
+public class TreasureChestBlock extends ChestBlock implements IWaterLoggable {
 	private static final TileEntityMerger.ICallback<TreasureChestTileEntity, Optional<INamedContainerProvider>> getContainerProvider = new TileEntityMerger.ICallback<TreasureChestTileEntity, Optional<INamedContainerProvider>>() {
 		@Override
 		public Optional<INamedContainerProvider> func_225539_a_(TreasureChestTileEntity chest1, TreasureChestTileEntity chest2) {
@@ -142,8 +86,9 @@ public class TreasureChestBlock extends AbstractChestBlock<TreasureChestTileEnti
 		}
 	};
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	protected TreasureChestBlock(Block.Properties properties, Supplier<TileEntityType<? extends TreasureChestTileEntity>> tileEntityTypeIn) {
-		super(properties, tileEntityTypeIn);
+		super(properties, (Supplier) tileEntityTypeIn);
 		this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH).with(TYPE, ChestType.SINGLE).with(WATERLOGGED, false));
 	}
 	
@@ -166,7 +111,6 @@ public class TreasureChestBlock extends AbstractChestBlock<TreasureChestTileEnti
 	 * returns its solidified counterpart. Note that this method should ideally consider only the specific face passed
 	 * in.
 	 */
-	@SuppressWarnings("deprecation")
 	@Override
 	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn,
 		BlockPos currentPos, BlockPos facingPos) {
@@ -188,107 +132,17 @@ public class TreasureChestBlock extends AbstractChestBlock<TreasureChestTileEnti
 
 		return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
 	}
-
-	@Override
-	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-		if (state.get(TYPE) == ChestType.SINGLE) {
-			return SHAPE;
-		}
-		else {
-			switch (getDirectionToAttached(state)) {
-				case NORTH:
-				default:
-					return SHAPE_NORTH;
-				case SOUTH:
-					return SHAPE_SOUTH;
-				case WEST:
-					return SHAPE_WEST;
-				case EAST:
-					return SHAPE_EAST;
-			}
-		}
-	}
-
-	@Override
-	public BlockState getStateForPlacement(BlockItemUseContext context) {
-		ChestType chesttype = ChestType.SINGLE;
-		Direction direction = context.getPlacementHorizontalFacing().getOpposite();
-		IFluidState ifluidstate = context.getWorld().getFluidState(context.getPos());
-		boolean flag = context.func_225518_g_(); /// is secondary use
-		Direction direction1 = context.getFace();
-		if (direction1.getAxis().isHorizontal() && flag) {
-			Direction direction2 = this.getDirectionToAttach(context, direction1.getOpposite());
-			if (direction2 != null && direction2.getAxis() != direction1.getAxis()) {
-				direction = direction2;
-				chesttype = direction2.rotateYCCW() == direction1.getOpposite()? ChestType.RIGHT : ChestType.LEFT;
-			}
-		}
-
-		if (chesttype == ChestType.SINGLE && !flag) {
-			if (direction == this.getDirectionToAttach(context, direction.rotateY())) {
-				chesttype = ChestType.LEFT;
-			}
-			else if (direction == this.getDirectionToAttach(context, direction.rotateYCCW())) {
-				chesttype = ChestType.RIGHT;
-			}
-		}
-
-		return this.getDefaultState().with(FACING, direction).with(TYPE, chesttype).with(WATERLOGGED,
-			Boolean.valueOf(ifluidstate.getFluid() == Fluids.WATER));
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	public IFluidState getFluidState(BlockState state) {
-		return state.get(WATERLOGGED)? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
-	}
-
-	/**
-	 * Returns facing pointing to a chest to form a double chest with, null otherwise
-	 */
-	@Nullable
-	private Direction getDirectionToAttach(BlockItemUseContext context, Direction direction) {
-		BlockState blockstate = context.getWorld().getBlockState(context.getPos().offset(direction));
-		return blockstate.getBlock() == this && blockstate.get(TYPE) == ChestType.SINGLE? blockstate.get(FACING) : null;
-	}
-
-	/**
-	 * Called by ItemBlocks after a block is set in the world, to allow post-place logic
-	 */
-	@Override
-	public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
-		if (stack.hasDisplayName()) {
-			TileEntity tileentity = worldIn.getTileEntity(pos);
-			if (tileentity instanceof TreasureChestTileEntity) {
-				((TreasureChestTileEntity)tileentity).setCustomName(stack.getDisplayName());
-			}
-		}
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
-		if (state.getBlock() != newState.getBlock()) {
-			TileEntity tileentity = worldIn.getTileEntity(pos);
-			if (tileentity instanceof IInventory) {
-				InventoryHelper.dropInventoryItems(worldIn, pos, (IInventory)tileentity);
-				worldIn.updateComparatorOutputLevel(pos, this);
-			}
-
-			super.onReplaced(state, worldIn, pos, newState, isMoving);
-		}
-	}
 	
-	@SuppressWarnings("serial")
-	private static void playLocalLockedSound(BlockPos pos) {
-		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> new SafeRunnable() {
-			@Override
-			public void run() {
-				Minecraft.getInstance().getConnection().getWorld().playSound(pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_CHEST_LOCKED, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
-				//Minecraft.getInstance().getSoundHandler().play(new SimpleSound(SoundEvents.BLOCK_CHEST_LOCKED, SoundCategory.BLOCKS, 1.0F, 1.0F, chest1.getPos().getX(), chest1.getPos().getY(), chest1.getPos().getZ()));
-			}
-		});
-	}
+//	@SuppressWarnings("serial")
+//	private static void playLocalLockedSound(BlockPos pos) {
+//		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> new SafeRunnable() {
+//			@Override
+//			public void run() {
+//				Minecraft.getInstance().getConnection().getWorld().playSound(pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_CHEST_LOCKED, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+//				//Minecraft.getInstance().getSoundHandler().play(new SimpleSound(SoundEvents.BLOCK_CHEST_LOCKED, SoundCategory.BLOCKS, 1.0F, 1.0F, chest1.getPos().getX(), chest1.getPos().getY(), chest1.getPos().getZ()));
+//			}
+//		});
+//	}
 	
 	private static final TileEntityMerger.ICallback<TreasureChestTileEntity, BiFunction<PlayerEntity, Hand, ActionResultType>> unlock = new TileEntityMerger.ICallback<TreasureChestTileEntity, BiFunction<PlayerEntity, Hand, ActionResultType>>() {
 
@@ -401,15 +255,7 @@ public class TreasureChestBlock extends AbstractChestBlock<TreasureChestTileEnti
 		}
 	}
 
-	protected Stat<ResourceLocation> getOpenStat() {
-		return Stats.CUSTOM.get(Stats.OPEN_CHEST);
-	}
-
-	@Nullable
-	public static IInventory func_226916_a_(TreasureChestBlock chest, BlockState state, World world, BlockPos pos, boolean p_226916_4_) {
-		return chest.combine(state, world, pos, p_226916_4_).apply(field_220109_i).orElse((IInventory)null);
-	}
-
+	@SuppressWarnings("unchecked")
 	@Override
 	public TileEntityMerger.ICallbackWrapper<? extends TreasureChestTileEntity> combine(BlockState state, World worldIn, BlockPos pos, boolean p_225536_4_) {
 		BiPredicate<IWorld, BlockPos> bipredicate;
@@ -448,7 +294,7 @@ public class TreasureChestBlock extends AbstractChestBlock<TreasureChestTileEnti
 			};
 		}
 
-		return TileEntityMerger.func_226924_a_(this.tileEntityType.get(), ChestBlock::func_226919_h_, ChestBlock::getDirectionToAttached, FACING, state, worldIn, pos, bipredicate);
+		return TileEntityMerger.func_226924_a_((TileEntityType<? extends TreasureChestTileEntity>) this.tileEntityType.get(), ChestBlock::func_226919_h_, ChestBlock::getDirectionToAttached, FACING, state, worldIn, pos, bipredicate);
 	}
 
 	@Override
@@ -456,108 +302,10 @@ public class TreasureChestBlock extends AbstractChestBlock<TreasureChestTileEnti
 	public INamedContainerProvider getContainer(BlockState state, World worldIn, BlockPos pos) {
 		return this.combine(state, worldIn, pos, false).apply(getContainerProvider).orElse((INamedContainerProvider)null);
 	}
-
-	@OnlyIn(Dist.CLIENT)
-	public static TileEntityMerger.ICallback<TreasureChestTileEntity, Float2FloatFunction> func_226917_a_(final IChestLid ichestlid) {
-		return new TileEntityMerger.ICallback<TreasureChestTileEntity, Float2FloatFunction>() {
-			@Override
-			public Float2FloatFunction func_225539_a_(TreasureChestTileEntity chest1, TreasureChestTileEntity chest2) {
-				return (partialTicks) -> Math.max(chest1.getLidAngle(partialTicks), chest2.getLidAngle(partialTicks));
-			}
-
-			@Override
-			public Float2FloatFunction func_225538_a_(TreasureChestTileEntity chest) {
-				return chest::getLidAngle;
-			}
-
-			@Override
-			public Float2FloatFunction func_225537_b_() {
-				return ichestlid::getLidAngle;
-			}
-		};
-	}
 	
 	@Override
 	public TileEntity createNewTileEntity(IBlockReader worldIn) {
 		return new TreasureChestTileEntity();
 	}
-
-	public static boolean isBlocked(IWorld world, BlockPos pos) {
-		return isBelowSolidBlock(world, pos) || isCatSittingOn(world, pos);
-	}
-
-	private static boolean isBelowSolidBlock(IBlockReader world, BlockPos posIn) {
-		BlockPos blockpos = posIn.up();
-		return world.getBlockState(blockpos).isNormalCube(world, blockpos);
-	}
-
-	private static boolean isCatSittingOn(IWorld world, BlockPos pos) {
-		List<CatEntity> list = world.getEntitiesWithinAABB(CatEntity.class, new AxisAlignedBB(pos.getX(), pos.getY() + 1, pos.getZ(), pos.getX() + 1, pos.getY() + 2, pos.getZ() + 1));
-		if (!list.isEmpty()) {
-			for (CatEntity catentity : list) {
-				if (catentity.isSitting()) {
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
-	/**
-	 * @deprecated call via {@link IBlockState#hasComparatorInputOverride()} whenever possible.
-	 * Implementing/overriding is fine.
-	 */
-	@Deprecated
-	@Override
-	public boolean hasComparatorInputOverride(BlockState state) {
-		return true;
-	}
-
-	/**
-	 * @deprecated call via {@link IBlockState#getComparatorInputOverride(World,BlockPos)} whenever possible.
-	 * Implementing/overriding is fine.
-	 */
-	@Deprecated
-	@Override
-	public int getComparatorInputOverride(BlockState blockState, World worldIn, BlockPos pos) {
-		return Container.calcRedstoneFromInventory(func_226916_a_(this, blockState, worldIn, pos, false));
-	}
-
-	/**
-	 * Returns the blockstate with the given rotation from the passed blockstate. If inapplicable, returns the passed
-	 * blockstate.
-	 * 
-	 * @deprecated call via {@link IBlockState#withRotation(Rotation)} whenever possible.
-	 * Implementing/overriding is fine.
-	 */
-	@Deprecated
-	@Override
-	public BlockState rotate(BlockState state, Rotation rot) {
-		return state.with(FACING, rot.rotate(state.get(FACING)));
-	}
-
-	/**
-	 * Returns the blockstate with the given mirror of the passed blockstate. If inapplicable, returns the passed
-	 * blockstate.
-	 * 
-	 * @deprecated call via {@link IBlockState#withMirror(Mirror)} whenever possible.
-	 * Implementing/overriding is fine.
-	 */
-	@Deprecated
-	@Override
-	public BlockState mirror(BlockState state, Mirror mirrorIn) {
-		return state.rotate(mirrorIn.toRotation(state.get(FACING)));
-	}
-
-	@Override
-	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-		builder.add(FACING, TYPE, WATERLOGGED);
-	}
-
-	@Override
-	public boolean allowsMovement(BlockState state, IBlockReader worldIn, BlockPos pos, PathType type) {
-		return false;
-	}
-	
+		
 }
