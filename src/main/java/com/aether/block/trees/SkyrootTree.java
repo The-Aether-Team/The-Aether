7e3d0fc413ba01e8d6cee55e642814d4276f7d2f
quek <55364841+quek04@users.noreply.github.com>
2020-12-07 18:18:40 -0600

    fixed skyroot sapling growth

--- src/main/java/com/aether/block/trees/SkyrootTree.java
+++ src/main/java/com/aether/block/trees/SkyrootTree.java
@@ -1,8 +1,11 @@
 package com.aether.block.trees;
 
+import com.aether.block.AetherBlocks;
 import net.minecraft.block.trees.Tree;
-import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.foliageplacer.BlobFoliagePlacer;
+import net.minecraft.world.gen.trunkplacer.StraightTrunkPlacer;
 
 import javax.annotation.Nullable;
 import java.util.Random;
@@ -12,7 +15,7 @@ public class SkyrootTree extends Tree {
 	@Nullable
 	@Override
 	protected ConfiguredFeature<BaseTreeFeatureConfig, ?> getTreeFeature(Random randomIn, boolean largeHive) {
-		return null; //TODO
+		return Feature.TREE.withConfiguration((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(AetherBlocks.SKYROOT_LOG.getDefaultState()), new SimpleBlockStateProvider(AetherBlocks.SKYROOT_LEAVES.getDefaultState()), new BlobFoliagePlacer(FeatureSpread.func_242252_a(2), FeatureSpread.func_242252_a(0), 3), new StraightTrunkPlacer(4, 2, 0), new TwoLayerFeature(1, 0, 1))).setIgnoreVines().build());
 	}
 
 }
