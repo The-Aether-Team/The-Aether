a335a54429acd06c150e9471cfdea2115dd20e6e
quek <55364841+quek04@users.noreply.github.com>
2020-12-07 19:19:45 -0600

    golden oak saplings work

--- src/main/java/com/aether/block/trees/GoldenOakTree.java
+++ src/main/java/com/aether/block/trees/GoldenOakTree.java
@@ -1,10 +1,15 @@
 package com.aether.block.trees;
 
+import com.aether.block.AetherBlocks;
 import net.minecraft.block.trees.Tree;
-import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.foliageplacer.FancyFoliagePlacer;
+import net.minecraft.world.gen.trunkplacer.FancyTrunkPlacer;
 
 import javax.annotation.Nullable;
+import java.util.OptionalInt;
 import java.util.Random;
 
 public class GoldenOakTree extends Tree {
@@ -12,6 +17,6 @@ public class GoldenOakTree extends Tree {
 	@Nullable
 	@Override
 	protected ConfiguredFeature<BaseTreeFeatureConfig, ?> getTreeFeature(Random randomIn, boolean largeHive) {
-		return null; //TODO
+		return Feature.TREE.withConfiguration((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(AetherBlocks.GOLDEN_OAK_LOG.getDefaultState()), new SimpleBlockStateProvider(AetherBlocks.GOLDEN_OAK_LEAVES.getDefaultState()), new FancyFoliagePlacer(FeatureSpread.func_242252_a(2), FeatureSpread.func_242252_a(4), 4), new FancyTrunkPlacer(3, 11, 0), new TwoLayerFeature(0, 0, 0, OptionalInt.of(4)))).setIgnoreVines().func_236702_a_(Heightmap.Type.MOTION_BLOCKING).build());
 	}
 }
