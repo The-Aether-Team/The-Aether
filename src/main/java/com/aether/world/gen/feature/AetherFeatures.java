70e349e8bef5eaf49de1a19cb0baf9cafe22fa46
quek <55364841+quek04@users.noreply.github.com>
2021-01-04 17:12:43 -0600

    crystal trees, i'll figure out the apples later

--- src/main/java/com/aether/world/gen/feature/AetherFeatures.java
+++ src/main/java/com/aether/world/gen/feature/AetherFeatures.java
@@ -36,6 +36,8 @@ public class AetherFeatures {
     public static final RegistryObject<Feature<NoFeatureConfig>> GOLD_AERCLOUD = FEATURES.register("gold_aercloud", () -> new GoldAercloudFeature(NoFeatureConfig.field_236558_a_));
     public static final RegistryObject<Feature<NoFeatureConfig>> PINK_AERCLOUD = FEATURES.register("pink_aercloud", () -> new PinkAercloudFeature(NoFeatureConfig.field_236558_a_));
 
+    public static final RegistryObject<Feature<NoFeatureConfig>> CRYSTAL_TREE = FEATURES.register("crystal_tree", () -> new CrystalTreeFeature(NoFeatureConfig.field_236558_a_));
+
     public static void registerConfiguredFeatures() {
         RuleTest HOLYSTONE = new BlockMatchRuleTest(AetherBlocks.HOLYSTONE);
 
@@ -45,6 +47,8 @@ public class AetherFeatures {
         register("blue_aercloud", BLUE_AERCLOUD.get().withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG).range(96).square().chance(5));
         register("gold_aercloud", GOLD_AERCLOUD.get().withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG).range(160).square().chance(5));
         register("pink_aercloud", PINK_AERCLOUD.get().withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG).range(160).square().chance(7));
+
+        register("crystal_tree", CRYSTAL_TREE.get().withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG).square().chance(15));
 
         register("tree_skyroot", Feature.TREE.withConfiguration((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(AetherBlocks.SKYROOT_LOG.getDefaultState()), new SimpleBlockStateProvider(AetherBlocks.SKYROOT_LEAVES.getDefaultState()), new BlobFoliagePlacer(FeatureSpread.func_242252_a(2), FeatureSpread.func_242252_a(0), 3), new StraightTrunkPlacer(4, 2, 0), new TwoLayerFeature(1, 0, 1))).setIgnoreVines().build()).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT));
         register("tree_golden_oak", Feature.TREE.withConfiguration((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(AetherBlocks.GOLDEN_OAK_LOG.getDefaultState()), new SimpleBlockStateProvider(AetherBlocks.GOLDEN_OAK_LEAVES.getDefaultState()), new FancyFoliagePlacer(FeatureSpread.func_242252_a(2), FeatureSpread.func_242252_a(4), 4), new FancyTrunkPlacer(3, 11, 0), new TwoLayerFeature(0, 0, 0, OptionalInt.of(4)))).setIgnoreVines().func_236702_a_(Heightmap.Type.MOTION_BLOCKING).build()).withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(0, 0.03F, 1))));
---
+++ src/main/java/com/aether/world/gen/feature/CrystalTreeFeature.java
@@ -0,0 +1,61 @@
+package com.aether.world.gen.feature;
+
+import com.aether.Aether;
+import com.mojang.serialization.Codec;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.gen.feature.template.*;
+
+import java.util.Random;
+
+public class CrystalTreeFeature extends Feature<NoFeatureConfig> {
+
+    private static final ResourceLocation TREE = new ResourceLocation(Aether.MODID, "crystal_tree");
+
+    public CrystalTreeFeature(Codec<NoFeatureConfig> codec) {
+        super(codec);
+    }
+
+    @Override
+    public boolean generate(ISeedReader reader, ChunkGenerator generator, Random rand, BlockPos pos, NoFeatureConfig config) {
+        Rotation rotation = Rotation.randomRotation(rand);
+
+        TemplateManager templatemanager = reader.getWorld().getServer().getTemplateManager();
+        Template tree = templatemanager.getTemplateDefaulted(TREE);
+        ChunkPos chunkpos = new ChunkPos(pos);
+        MutableBoundingBox mutableboundingbox = new MutableBoundingBox(chunkpos.getXStart(), 0, chunkpos.getZStart(), chunkpos.getXEnd(), 256, chunkpos.getZEnd());
+        PlacementSettings placementsettings = (new PlacementSettings()).setRotation(rotation).setBoundingBox(mutableboundingbox).setRandom(rand).addProcessor(BlockIgnoreStructureProcessor.AIR_AND_STRUCTURE_BLOCK);
+        BlockPos blockpos = tree.transformedSize(rotation);
+        int x = rand.nextInt(16 - blockpos.getX());
+        int z = rand.nextInt(16 - blockpos.getZ());
+        int y = 150 + rand.nextInt(100);
+
+        /*
+        for(int i1 = 0; i1 < blockpos.getX(); ++i1) {
+            for(int j1 = 0; j1 < blockpos.getZ(); ++j1) {
+                y = Math.min(y, reader.getHeight(Heightmap.Type.OCEAN_FLOOR_WG, pos.getX() + i1 + x, pos.getZ() + j1 + z));
+            }
+        }
+
+        int k1 = Math.max(y - 15 - rand.nextInt(10), 10);
+        */
+
+        BlockPos blockpos1 = tree.getZeroPositionWithTransform(pos.add(x, y, z), Mirror.NONE, rotation);
+
+        //IntegrityProcessor integrityprocessor = new IntegrityProcessor(0.9F);
+        //placementsettings.clearProcessors().addProcessor(integrityprocessor);
+        tree.func_237146_a_(reader, blockpos1, blockpos1, placementsettings, rand, 4);
+        //placementsettings.removeProcessor(integrityprocessor);
+
+        return true;
+    }
+}
