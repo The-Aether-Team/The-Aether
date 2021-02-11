9b5e160fa2bc2845b40c7702e021d75f5b3c8bc6
reetamb <57857847+reetamb@users.noreply.github.com>
2021-02-10 19:26:50 -0800

    crystal fruit leaves generate FINALLY (crystal tree chunk errors still exist tho)

--- src/main/java/com/aether/world/gen/feature/CrystalTreeFeature.java
+++ src/main/java/com/aether/world/gen/feature/CrystalTreeFeature.java
@@ -1,6 +1,7 @@
 package com.aether.world.gen.feature;
 
 import com.aether.Aether;
+import com.aether.block.AetherBlocks;
 import com.mojang.serialization.Codec;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Mirror;
@@ -28,35 +29,31 @@ public class CrystalTreeFeature extends Feature<NoFeatureConfig> {
 
     @Override
     public boolean generate(ISeedReader reader, ChunkGenerator generator, Random rand, BlockPos pos, NoFeatureConfig config) {
-        Rotation rotation = Rotation.randomRotation(rand);
 
         TemplateManager templatemanager = reader.getWorld().getServer().getTemplateManager();
         Template tree = templatemanager.getTemplateDefaulted(TREE);
         ChunkPos chunkpos = new ChunkPos(pos);
         MutableBoundingBox mutableboundingbox = new MutableBoundingBox(chunkpos.getXStart(), 0, chunkpos.getZStart(), chunkpos.getXEnd(), 256, chunkpos.getZEnd());
-        PlacementSettings placementsettings = (new PlacementSettings()).setRotation(rotation).setBoundingBox(mutableboundingbox).setRandom(rand).addProcessor(BlockIgnoreStructureProcessor.AIR_AND_STRUCTURE_BLOCK);
-        BlockPos blockpos = tree.transformedSize(rotation);
+        PlacementSettings placementsettings = (new PlacementSettings()).setBoundingBox(mutableboundingbox).setRandom(rand).addProcessor(BlockIgnoreStructureProcessor.AIR_AND_STRUCTURE_BLOCK);
+        BlockPos blockpos = tree.getSize();
         int x = rand.nextInt(16 - blockpos.getX());
         int z = rand.nextInt(16 - blockpos.getZ());
         int y = 75 + rand.nextInt(50);
 
-        /*
-        for(int i1 = 0; i1 < blockpos.getX(); ++i1) {
-            for(int j1 = 0; j1 < blockpos.getZ(); ++j1) {
-                y = Math.min(y, reader.getHeight(Heightmap.Type.OCEAN_FLOOR_WG, pos.getX() + i1 + x, pos.getZ() + j1 + z));
+
+        BlockPos blockpos1 = tree.getZeroPositionWithTransform(pos.add(x, y, z), Mirror.NONE, Rotation.NONE);
+
+        tree.func_237146_a_(reader, blockpos1, blockpos1, placementsettings, rand, 4);
+        for (int x1 = blockpos1.getX(); x1 < blockpos1.add(blockpos).getX(); x1++) {
+            for (int y1 = blockpos1.getY(); y1 < blockpos1.add(blockpos).getY(); y1++) {
+                for (int z1 = blockpos1.getZ(); z1 < blockpos1.add(blockpos).getZ(); z1++) {
+                    BlockPos blockpos2 = new BlockPos(x1, y1, z1);
+                    if (reader.getBlockState(blockpos2).getBlock() == AetherBlocks.CRYSTAL_LEAVES.getBlock() && rand.nextInt(5) == 2) {
+                        reader.setBlockState(blockpos2, AetherBlocks.CRYSTAL_FRUIT_LEAVES.getDefaultState(), 19);
+                    }
+                }
             }
         }
-
-        int k1 = Math.max(y - 15 - rand.nextInt(10), 10);
-        */
-
-        BlockPos blockpos1 = tree.getZeroPositionWithTransform(pos.add(x, y, z), Mirror.NONE, rotation);
-
-//        IntegrityProcessor integrityprocessor = new IntegrityProcessor(0.9F);
-//        placementsettings.clearProcessors().addProcessor(integrityprocessor);
-        tree.func_237146_a_(reader, blockpos1, blockpos1, placementsettings, rand, 4);
-        //placementsettings.removeProcessor(integrityprocessor);
-
         return true;
     }
 }
