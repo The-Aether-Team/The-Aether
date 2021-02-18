6cfee43ba3206264b9aba114631ba16f55939681
bconlon <bconlon@gildedgames.com>
2021-02-17 21:47:26 -0800

    Aerogel variant blocks now have property opacity.

--- src/main/java/com/aether/block/construction/AerogelBlock.java
+++ src/main/java/com/aether/block/construction/AerogelBlock.java
@@ -7,12 +7,12 @@ import net.minecraft.world.IBlockReader;
 
 import net.minecraft.block.AbstractBlock;
 
-public class AerogelBlock extends Block {
-
+public class AerogelBlock extends Block
+{
 	public AerogelBlock(AbstractBlock.Properties properties) {
 		super(properties);
 	}
-	
+
 	@Override
 	public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
 		return 3;
@@ -22,5 +22,4 @@ public class AerogelBlock extends Block {
 	public boolean isTransparent(BlockState state) {
 		return true;
 	}
-	
 }
