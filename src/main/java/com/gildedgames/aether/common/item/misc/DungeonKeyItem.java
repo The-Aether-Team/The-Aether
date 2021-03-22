aed141eef7f26e1303f774c2a7a4a66691858120
BuildTools <unconfigured@null.spigotmc.org>
2021-03-21 20:53:29 -0400

    Added Lock functionality

--- src/main/java/com/gildedgames/aether/common/item/misc/DungeonKeyItem.java
+++ src/main/java/com/gildedgames/aether/common/item/misc/DungeonKeyItem.java
@@ -1,23 +1,27 @@
 package com.gildedgames.aether.common.item.misc;
 
-import java.util.function.Supplier;
-
+import com.gildedgames.aether.common.entity.tile.TreasureChestTileEntity;
+import com.gildedgames.aether.core.api.registers.DungeonType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
 import org.apache.commons.lang3.Validate;
 
-import com.gildedgames.aether.core.api.registers.DungeonType;
+import java.util.function.Supplier;
 
-import net.minecraft.item.Item;
+public class DungeonKeyItem extends Item {
+    private final Supplier<DungeonType> dungeonTypeGetter;
 
-public class DungeonKeyItem extends Item
-{
-	private final Supplier<DungeonType> dungeonTypeGetter;
-	
-	public DungeonKeyItem(Supplier<DungeonType> dungeonTypeGetter, Item.Properties properties) {
-		super(properties);
-		this.dungeonTypeGetter = Validate.notNull(dungeonTypeGetter);
-	}
-	
-	public DungeonType getDungeonType() {
-		return dungeonTypeGetter.get();
-	}
+    public DungeonKeyItem(Supplier<DungeonType> dungeonTypeGetter, Item.Properties properties) {
+        super(properties);
+        this.dungeonTypeGetter = Validate.notNull(dungeonTypeGetter);
+    }
+
+    public DungeonType getDungeonType() {
+        return dungeonTypeGetter.get();
+    }
+
 }
