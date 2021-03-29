7de7e27b5357aa4400bd6127ef07d281ad956da4
quek <55364841+quek04@users.noreply.github.com>
2021-03-28 21:14:45 -0500

    some gloves functionality

--- src/main/java/com/gildedgames/aether/common/item/accessories/gloves/GoldGlovesItem.java
+++ src/main/java/com/gildedgames/aether/common/item/accessories/gloves/GoldGlovesItem.java
@@ -1,13 +1,13 @@
 package com.gildedgames.aether.common.item.accessories.gloves;
 
-import com.gildedgames.aether.common.item.accessories.AccessoryItem;
+import com.gildedgames.aether.common.item.accessories.glove.GlovesItem;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 
-public class GoldGlovesItem extends AccessoryItem
+public class GoldGlovesItem extends GlovesItem
 {
-    public GoldGlovesItem(Properties properties) {
-        super(properties);
+    public GoldGlovesItem(Properties properties, double damage) {
+        super(properties, damage);
     }
 
     @Override
