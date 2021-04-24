12fe5f569d392d2a3f57df394e5300af874f961c
bconlon <bconlon@gildedgames.com>
2021-04-23 21:18:19 -0700

    Properly functioning Repulsion Shield rendering.

--- src/main/java/com/gildedgames/aether/client/AetherClient.java
+++ src/main/java/com/gildedgames/aether/client/AetherClient.java
@@ -1,10 +1,15 @@
 package com.gildedgames.aether.client;
 
+import com.gildedgames.aether.client.renderer.accessory.layer.RepulsionShieldLayer;
 import com.gildedgames.aether.client.renderer.dimension.AetherSkyRenderInfo;
 import com.gildedgames.aether.common.registry.AetherDimensions;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.PlayerRenderer;
+import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.world.DimensionRenderInfo;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class AetherClient
@@ -12,6 +17,7 @@ public class AetherClient
     public static void clientInitialization() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(AetherClient::clientSetup);
+        modEventBus.addListener(AetherClient::clientComplete);
     }
 
     public static void clientSetup(FMLClientSetupEvent event) {
@@ -26,6 +32,14 @@ public class AetherClient
             AetherRendering.registerWoodTypeAtlases();
 
             DimensionRenderInfo.EFFECTS.put(AetherDimensions.AETHER_DIMENSION.location(), new AetherSkyRenderInfo());
+        });
+    }
+
+    public static void clientComplete(FMLLoadCompleteEvent event) {
+        event.enqueueWork(() -> {
+            for (PlayerRenderer render : Minecraft.getInstance().getEntityRenderDispatcher().getSkinMap().values()) {
+                render.addLayer(new RepulsionShieldLayer<>(render, new BipedModel(1.1F)));
+            }
         });
     }
 }
--- src/main/java/com/gildedgames/aether/common/item/accessories/misc/RepulsionShieldItem.java
+++ src/main/java/com/gildedgames/aether/common/item/accessories/misc/RepulsionShieldItem.java
@@ -1,20 +1,8 @@
 package com.gildedgames.aether.common.item.accessories.misc;
 
 import com.gildedgames.aether.Aether;
-import com.gildedgames.aether.client.renderer.accessory.model.RepulsionShieldModel;
 import com.gildedgames.aether.common.item.accessories.AccessoryItem;
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.model.BipedModel;
-import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.vector.Vector3d;
-import top.theillusivec4.curios.api.type.capability.ICurio;
 
 public class RepulsionShieldItem extends AccessoryItem
 {
@@ -23,30 +11,6 @@ public class RepulsionShieldItem extends AccessoryItem
 
     public RepulsionShieldItem(Properties properties) {
         super(properties);
-    }
-
-    @Override
-    public boolean canRender(String identifier, int index, LivingEntity living, ItemStack stack) {
-        return true;
-    }
-
-    @Override
-    public void renderModel(BipedModel<?> model, String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack stack) {
-        RepulsionShieldModel shield = new RepulsionShieldModel();
-        ResourceLocation texture;
-        Vector3d motion = livingEntity.getDeltaMovement();
-
-        if (motion.x() == 0.0 && (motion.y() == -0.0784000015258789 || motion.y() == 0.0) && motion.z() == 0.0) {
-            texture = this.getRepulsionShieldTexture();
-        } else {
-            texture = this.getRepulsionShieldInactiveTexture();
-        }
-
-        IVertexBuilder vertexBuilder = ItemRenderer.getArmorFoilBuffer(renderTypeBuffer, RenderType.entityTranslucent(texture), false, stack.isEnchanted());
-        shield.prepareMobModel(livingEntity, limbSwing, limbSwingAmount, partialTicks);
-        shield.setupAnim(livingEntity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
-        ICurio.RenderHelper.followBodyRotations(livingEntity, shield);
-        shield.renderToBuffer(matrixStack, vertexBuilder, light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 
     public ResourceLocation getRepulsionShieldTexture() {
