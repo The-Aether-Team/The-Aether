16b8f38b0561410c469a5343a9fd7795ac0bf6ba
bconlon <bconlon@gildedgames.com>
2021-04-29 20:16:06 -0700

    Renamed the Aether dimension from aether to the_aether

--- src/main/java/com/gildedgames/aether/common/registry/AetherDimensions.java
+++ src/main/java/com/gildedgames/aether/common/registry/AetherDimensions.java
@@ -9,7 +9,7 @@ import net.minecraft.world.World;
 
 public class AetherDimensions {
 
-	public static final RegistryKey<DimensionType> AETHER_DIMENSION = RegistryKey.create(Registry.DIMENSION_TYPE_REGISTRY, new ResourceLocation(Aether.MODID, "aether"));
-	public static final RegistryKey<World> AETHER_WORLD = RegistryKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(Aether.MODID, "aether"));
+	public static final RegistryKey<DimensionType> AETHER_DIMENSION = RegistryKey.create(Registry.DIMENSION_TYPE_REGISTRY, new ResourceLocation(Aether.MODID, "the_aether"));
+	public static final RegistryKey<World> AETHER_WORLD = RegistryKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(Aether.MODID, "the_aether"));
 
 }
