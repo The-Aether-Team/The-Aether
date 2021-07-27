f30ce633a9b8066cda4d6b8017584bf8c8ab7bd2
bconlon <bconlon@gildedgames.com>
2021-07-27 16:57:08 -0700

    System for dispensing tagged portal activation items crashes on servers. I'll fix this later.

--- src/main/java/com/gildedgames/aether/common/event/listeners/WorldListener.java
+++ src/main/java/com/gildedgames/aether/common/event/listeners/WorldListener.java
@@ -16,24 +16,24 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber
 public class WorldListener
 {
-    @SubscribeEvent
-    public static void onWorldLoad(WorldEvent.Load event) {
-        for (Item item : AetherTags.Items.AETHER_PORTAL_ACTIVATION_ITEMS.getValues()) {
-            DispenserBlock.registerBehavior(item, new DefaultDispenseItemBehavior()
-            {
-                private final DefaultDispenseItemBehavior defaultDispenseItemBehavior = new DefaultDispenseItemBehavior();
-
-                @Override
-                protected ItemStack execute(IBlockSource source, ItemStack stack) {
-                    World world = source.getLevel();
-                    BlockPos blockpos = source.getPos().relative(source.getBlockState().getValue(DispenserBlock.FACING));
-                    if (AetherPortalBlock.fillPortalBlocksWithoutContext(world, blockpos, stack)) {
-                        return stack;
-                    } else {
-                        return this.defaultDispenseItemBehavior.dispense(source, stack);
-                    }
-                }
-            });
-        }
-    }
+//    @SubscribeEvent
+//    public static void onWorldLoad(WorldEvent.Load event) {
+//        for (Item item : AetherTags.Items.AETHER_PORTAL_ACTIVATION_ITEMS.getValues()) {
+//            DispenserBlock.registerBehavior(item, new DefaultDispenseItemBehavior()
+//            {
+//                private final DefaultDispenseItemBehavior defaultDispenseItemBehavior = new DefaultDispenseItemBehavior();
+//
+//                @Override
+//                protected ItemStack execute(IBlockSource source, ItemStack stack) {
+//                    World world = source.getLevel();
+//                    BlockPos blockpos = source.getPos().relative(source.getBlockState().getValue(DispenserBlock.FACING));
+//                    if (AetherPortalBlock.fillPortalBlocksWithoutContext(world, blockpos, stack)) {
+//                        return stack;
+//                    } else {
+//                        return this.defaultDispenseItemBehavior.dispense(source, stack);
+//                    }
+//                }
+//            });
+//        }
+//    }
 }
