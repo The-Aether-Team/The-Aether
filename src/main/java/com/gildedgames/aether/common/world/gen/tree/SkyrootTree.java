99486252aeb7f7c2ebe4b2f96e013b707ca178ed
bconlon <bconlon@gildedgames.com>
2021-07-29 17:03:47 -0700

    Grown trees have double drops.

--- src/main/java/com/gildedgames/aether/common/world/gen/tree/SkyrootTree.java
+++ src/main/java/com/gildedgames/aether/common/world/gen/tree/SkyrootTree.java
@@ -1,5 +1,6 @@
 package com.gildedgames.aether.common.world.gen.tree;
 
+import com.gildedgames.aether.common.block.state.properties.AetherBlockStateProperties;
 import com.gildedgames.aether.common.registry.AetherBlocks;
 import net.minecraft.block.trees.Tree;
 import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
@@ -15,6 +16,6 @@ public class SkyrootTree extends Tree
 	@Nullable
 	@Override
 	protected ConfiguredFeature<BaseTreeFeatureConfig, ?> getConfiguredFeature(Random randomIn, boolean largeHive) {
-		return Feature.TREE.configured((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(AetherBlocks.SKYROOT_LOG.get().defaultBlockState()), new SimpleBlockStateProvider(AetherBlocks.SKYROOT_LEAVES.get().defaultBlockState()), new BlobFoliagePlacer(FeatureSpread.fixed(2), FeatureSpread.fixed(0), 3), new StraightTrunkPlacer(4, 2, 0), new TwoLayerFeature(1, 0, 1))).ignoreVines().build());
+		return Feature.TREE.configured((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(AetherBlocks.SKYROOT_LOG.get().defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, true)), new SimpleBlockStateProvider(AetherBlocks.SKYROOT_LEAVES.get().defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, true)), new BlobFoliagePlacer(FeatureSpread.fixed(2), FeatureSpread.fixed(0), 3), new StraightTrunkPlacer(4, 2, 0), new TwoLayerFeature(1, 0, 1))).ignoreVines().build());
 	}
 }
