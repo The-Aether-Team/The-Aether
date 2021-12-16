0af520811ad45fcba6a017d2f006c9fcd6042a69
reetamb <57857847+reetamb@users.noreply.github.com>
2021-12-15 16:24:07 -0800

    Fixed stupid fetch issue

--- src/main/java/com/gildedgames/aether/common/registry/AetherFeatures.java
+++ src/main/java/com/gildedgames/aether/common/registry/AetherFeatures.java
@@ -49,49 +49,6 @@ public class AetherFeatures
     public static final RegistryObject<Feature<NoFeatureConfig>> HOLIDAY_TREE = FEATURES.register("holiday_tree", () -> new HolidayTreeFeature(NoFeatureConfig.CODEC));
 
     public static final RegistryObject<Feature<NoFeatureConfig>> HOLYSTONE_SPHERE = FEATURES.register("holystone_sphere", () -> new HolystoneSphereFeature(NoFeatureConfig.CODEC)); // This is for Gold Dungeons
-    
-    public static final RegistryObject<Feature<BlockStateFeatureConfig>> LAKE = FEATURES.register("lake", () -> new AetherLakeFeature(BlockStateFeatureConfig.CODEC));
-
-    public static void registerConfiguredFeatures() {
-        RuleTest HOLYSTONE = new BlockMatchRuleTest(AetherBlocks.HOLYSTONE.get());
-
-        register("quicksoil", QUICKSOIL.get().configured(IFeatureConfig.NONE).decorated(Placement.RANGE_VERY_BIASED.configured(new TopSolidRangeConfig(63, 0, 70))).squared().count(10));
-
-        register("cold_aercloud", COLD_AERCLOUD.get().configured(IFeatureConfig.NONE).range(128).squared().chance(5));
-        register("blue_aercloud", BLUE_AERCLOUD.get().configured(IFeatureConfig.NONE).range(96).squared().chance(5));
-        register("gold_aercloud", GOLD_AERCLOUD.get().configured(IFeatureConfig.NONE).range(160).squared().chance(5));
-        register("pink_aercloud", PINK_AERCLOUD.get().configured(IFeatureConfig.NONE).range(160).squared().chance(7));
-
-        register("crystal_tree", CRYSTAL_TREE.get().configured(IFeatureConfig.NONE).chance(30));
-        register("holiday_tree", HOLIDAY_TREE.get().configured(IFeatureConfig.NONE).chance(60));
-
-        register("water_lake", LAKE.get().configured(new BlockStateFeatureConfig(Blocks.WATER.defaultBlockState())).decorated(Placement.WATER_LAKE.configured(new ChanceConfig(4))));
-
-        register("tree_skyroot", Feature.TREE.configured(
-                (new BaseTreeFeatureConfig.Builder(
-                        new SimpleBlockStateProvider(AetherBlocks.SKYROOT_LOG.get().defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, true)),
-                        new SimpleBlockStateProvider(AetherBlocks.SKYROOT_LEAVES.get().defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, true)),
-                        new BlobFoliagePlacer(FeatureSpread.fixed(2), FeatureSpread.fixed(0), 3),
-                        new StraightTrunkPlacer(4, 2, 0),
-                        new TwoLayerFeature(1, 0, 1)))
-                        .ignoreVines().build()).decorated(Features.Placements.HEIGHTMAP_SQUARE));
-        register("tree_golden_oak", Feature.TREE.configured(
-                (new BaseTreeFeatureConfig.Builder(
-                        new SimpleBlockStateProvider(AetherBlocks.GOLDEN_OAK_LOG.get().defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, true)),
-                        new SimpleBlockStateProvider(AetherBlocks.GOLDEN_OAK_LEAVES.get().defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, true)),
-                        new FancyFoliagePlacer(FeatureSpread.fixed(2), FeatureSpread.fixed(4), 4),
-                        new FancyTrunkPlacer(3, 11, 0),
-                        new TwoLayerFeature(0, 0, 0, OptionalInt.of(4))))
-                        .ignoreVines().heightmap(Heightmap.Type.MOTION_BLOCKING).build())
-                .decorated(Placement.COUNT_EXTRA.configured(new AtSurfaceWithExtraConfig(0, 0.03F, 1))));
-
-        register("ore_aether_dirt", Feature.ORE.configured(new OreFeatureConfig(HOLYSTONE, AetherBlocks.AETHER_DIRT.get().defaultBlockState(), 33)).range(256).squared().count(10));
-        register("ore_icestone", Feature.ORE.configured(new OreFeatureConfig(HOLYSTONE, AetherBlocks.ICESTONE.get().defaultBlockState(), 16)).range(256).squared().count(10));
-        register("ore_ambrosium", Feature.ORE.configured(new OreFeatureConfig(HOLYSTONE, AetherBlocks.AMBROSIUM_ORE.get().defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, true), 16)).range(256).squared().count(10));
-        register("ore_zanite", Feature.ORE.configured(new OreFeatureConfig(HOLYSTONE, AetherBlocks.ZANITE_ORE.get().defaultBlockState(), 8)).range(256).squared().count(10));
-        register("ore_gravitite", Feature.ORE.configured(new OreFeatureConfig(HOLYSTONE, AetherBlocks.GRAVITITE_ORE.get().defaultBlockState(), 6)).range(256).squared().count(10));
-
-    public static final RegistryObject<Feature<NoFeatureConfig>> HOLYSTONE_SPHERE = FEATURES.register("holystone_sphere", () -> new HolystoneSphereFeature(NoFeatureConfig.CODEC));
 
     public static final class States
     {
@@ -154,9 +111,10 @@ public class AetherFeatures
         register("pink_aercloud", AetherFeatures.PINK_AERCLOUD.get().configured(IFeatureConfig.NONE).range(160).squared().chance(7));
 
 
-        register("crystal_tree", AetherFeatures.CRYSTAL_TREE.get().configured(IFeatureConfig.NONE).chance(30));
         register("tree_skyroot", Feature.TREE.configured(Configs.SKYROOT_TREE_CONFIG).decorated(Features.Placements.HEIGHTMAP_SQUARE));
         register("tree_golden_oak", Feature.TREE.configured(Configs.GOLDEN_OAK_TREE_CONFIG).decorated(Placement.COUNT_EXTRA.configured(new AtSurfaceWithExtraConfig(0, 0.03F, 1))));
+        register("crystal_tree", AetherFeatures.CRYSTAL_TREE.get().configured(IFeatureConfig.NONE).chance(30));
+        register("holiday_tree", AetherFeatures.HOLIDAY_TREE.get().configured(IFeatureConfig.NONE).chance(60));
 
         register("ore_aether_dirt", Feature.ORE.configured(new OreFeatureConfig(Tests.HOLYSTONE, States.AETHER_DIRT, 33)).range(256).squared().count(10));
         register("ore_icestone", Feature.ORE.configured(new OreFeatureConfig(Tests.HOLYSTONE, States.ICESTONE, 16)).range(256).squared().count(10));
