cad6cd62689005ec27002be5edd6ab59cf5758e6
Archie <azwerlein77@gmail.com>
2021-12-20 02:48:46 -0600

    Ported the sheepuff model

--- src/main/java/com/gildedgames/aether/client/renderer/entity/model/SheepuffModel.java
+++ src/main/java/com/gildedgames/aether/client/renderer/entity/model/SheepuffModel.java
@@ -4,6 +4,8 @@ import com.gildedgames.aether.common.entity.passive.SheepuffEntity;
 
 import net.minecraft.client.model.QuadrupedModel;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -12,14 +14,16 @@ public class SheepuffModel extends QuadrupedModel<SheepuffEntity>
 {
     private float headXRot;
 
-    public SheepuffModel() {
-        super(12, 0.0F, false, 8.0F, 4.0F, 2.0F, 2.0F, 24);
-        this.head = new ModelPart(this, 0, 0);
-        this.head.addBox(-3.0F, -4.0F, -6.0F, 6.0F, 6.0F, 8.0F, 0.0F);
-        this.head.setPos(0.0F, 6.0F, -8.0F);
-        this.body = new ModelPart(this, 28, 8);
-        this.body.addBox(-4.0F, -10.0F, -7.0F, 8.0F, 16.0F, 6.0F, 0.0F);
-        this.body.setPos(0.0F, 5.0F, 2.0F);
+    public SheepuffModel(ModelPart root) {
+        super(root, false, 8.0F, 4.0F, 2.0F, 2.0F, 24);
+    }
+
+    public static LayerDefinition createBodyLayer() {
+        MeshDefinition meshdefinition = QuadrupedModel.createBodyMesh(12, CubeDeformation.NONE);
+        PartDefinition partdefinition = meshdefinition.getRoot();
+        partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -4.0F, -6.0F, 6.0F, 6.0F, 8.0F), PartPose.offset(0.0F, 6.0F, -8.0F));
+        partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(28, 8).addBox(-4.0F, -10.0F, -7.0F, 8.0F, 16.0F, 6.0F), PartPose.offset(0.0F, 5.0F, 2.0F));
+        return LayerDefinition.create(meshdefinition, 64, 32);
     }
 
     public void prepareMobModel(SheepuffEntity sheepuff, float limbSwing, float limbSwingAmount, float partialTick) {
