af349e36f3920e948d24f4dcd572d6c1f4b99f6f
Archie <azwerlein77@gmail.com>
2021-12-20 19:15:27 -0600

    Resolving merge conflicts

--- src/main/java/com/gildedgames/aether/client/renderer/tile/ChestMimicTileEntityRenderer.java
+++ src/main/java/com/gildedgames/aether/client/renderer/tile/ChestMimicTileEntityRenderer.java
@@ -5,7 +5,8 @@ import com.gildedgames.aether.common.registry.AetherBlocks;
 import com.gildedgames.aether.common.block.dungeon.ChestMimicBlock;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.ChestBlock;
@@ -16,7 +17,6 @@ import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.client.renderer.blockentity.BrightnessCombiner;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
 import net.minecraft.world.level.block.state.properties.ChestType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.DoubleBlockCombiner;
@@ -27,91 +27,74 @@ import org.apache.logging.log4j.LogManager;
 
 import java.util.Calendar;
 
-public class ChestMimicTileEntityRenderer<T extends BlockEntity> implements BlockEntityRenderer<T> {
-//	private final ModelPart singleLid;
-//	private final ModelPart singleBottom;
-//	private final ModelPart singleLatch;
-//	private final ModelPart rightLid;
-//	private final ModelPart rightBottom;
-//	private final ModelPart rightLatch;
-//	private final ModelPart leftLid;
-//	private final ModelPart leftBottom;
-//	private final ModelPart leftLatch;
+public class ChestMimicTileEntityRenderer<T extends BlockEntity> implements BlockEntityRenderer<T>
+{
+	private final ModelPart singleLid;
+	private final ModelPart singleBottom;
+	private final ModelPart singleLock;
+	private final ModelPart rightLid;
+	private final ModelPart rightBottom;
+	private final ModelPart rightLock;
+	private final ModelPart leftLid;
+	private final ModelPart leftBottom;
+	private final ModelPart leftLock;
 	private boolean isChristmas;
 
-	public ChestMimicTileEntityRenderer(BlockEntityRenderDispatcher rendererDispatcherIn) {
-
+	public ChestMimicTileEntityRenderer(BlockEntityRendererProvider.Context context) {
 		Calendar calendar = Calendar.getInstance();
 		if (calendar.get(Calendar.MONTH) + 1 == 12 && calendar.get(Calendar.DATE) >= 24 && calendar.get(Calendar.DATE) <= 26) {
 			this.isChristmas = true;
 		}
 
-//		this.singleBottom = rendererDispatcherIn.
-
-//		this.singleBottom = new ModelPart(64, 64, 0, 19);
-//		this.singleBottom.addBox(1.0F, 0.0F, 1.0F, 14.0F, 10.0F, 14.0F, 0.0F);
-//		this.singleLid = new ModelPart(64, 64, 0, 0);
-//		this.singleLid.addBox(1.0F, 0.0F, 0.0F, 14.0F, 5.0F, 14.0F, 0.0F);
-//		this.singleLid.y = 9.0F;
-//		this.singleLid.z = 1.0F;
-//		this.singleLatch = new ModelPart(64, 64, 0, 0);
-//		this.singleLatch.addBox(7.0F, -1.0F, 15.0F, 2.0F, 4.0F, 1.0F, 0.0F);
-//		this.singleLatch.y = 8.0F;
-//		this.rightBottom = new ModelPart(64, 64, 0, 19);
-//		this.rightBottom.addBox(1.0F, 0.0F, 1.0F, 15.0F, 10.0F, 14.0F, 0.0F);
-//		this.rightLid = new ModelPart(64, 64, 0, 0);
-//		this.rightLid.addBox(1.0F, 0.0F, 0.0F, 15.0F, 5.0F, 14.0F, 0.0F);
-//		this.rightLid.y = 9.0F;
-//		this.rightLid.z = 1.0F;
-//		this.rightLatch = new ModelPart(64, 64, 0, 0);
-//		this.rightLatch.addBox(15.0F, -1.0F, 15.0F, 1.0F, 4.0F, 1.0F, 0.0F);
-//		this.rightLatch.y = 8.0F;
-//		this.leftBottom = new ModelPart(64, 64, 0, 19);
-//		this.leftBottom.addBox(0.0F, 0.0F, 1.0F, 15.0F, 10.0F, 14.0F, 0.0F);
-//		this.leftLid = new ModelPart(64, 64, 0, 0);
-//		this.leftLid.addBox(0.0F, 0.0F, 0.0F, 15.0F, 5.0F, 14.0F, 0.0F);
-//		this.leftLid.y = 9.0F;
-//		this.leftLid.z = 1.0F;
-//		this.leftLatch = new ModelPart(64, 64, 0, 0);
-//		this.leftLatch.addBox(0.0F, -1.0F, 15.0F, 1.0F, 4.0F, 1.0F, 0.0F);
-//		this.leftLatch.y = 8.0F;
+		ModelPart root = context.bakeLayer(ModelLayers.CHEST);
+		this.singleBottom = root.getChild("bottom");
+		this.singleLid = root.getChild("lid");
+		this.singleLock = root.getChild("lock");
+		ModelPart leftRoot = context.bakeLayer(ModelLayers.DOUBLE_CHEST_LEFT);
+		this.leftBottom = leftRoot.getChild("bottom");
+		this.leftLid = leftRoot.getChild("lid");
+		this.leftLock = leftRoot.getChild("lock");
+		ModelPart rightRoot = context.bakeLayer(ModelLayers.DOUBLE_CHEST_RIGHT);
+		this.rightBottom = rightRoot.getChild("bottom");
+		this.rightLid = rightRoot.getChild("lid");
+		this.rightLock = rightRoot.getChild("lock");
 	}
 
 	@Override
 	public void render(T tileEntityIn, float partialTicks, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn, int combinedOverlayIn) {
-//		Level world = tileEntityIn.getLevel();
-//		BlockState blockstate = world != null ? tileEntityIn.getBlockState() : AetherBlocks.CHEST_MIMIC.get().defaultBlockState().setValue(ChestMimicBlock.FACING, Direction.SOUTH);
-//		ChestType chesttype = blockstate.hasProperty(ChestBlock.TYPE) ? blockstate.getValue(ChestBlock.TYPE) : ChestType.SINGLE;
-//		Block block = blockstate.getBlock();
-//		if (block instanceof ChestMimicBlock) {
-//			ChestMimicBlock chestMimicBlock = (ChestMimicBlock) block;
-//			matrixStackIn.pushPose();
-//			float f = blockstate.getValue(ChestBlock.FACING).toYRot();
-//			matrixStackIn.translate(0.5D, 0.5D, 0.5D);
-//			matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(-f));
-//			matrixStackIn.translate(-0.5D, -0.5D, -0.5D);
-//			DoubleBlockCombiner.NeighborCombineResult<? extends ChestMimicTileEntity> icallbackwrapper;
-//			if (world != null) {
-//				icallbackwrapper = chestMimicBlock.combine(blockstate, world, tileEntityIn.getBlockPos(), true);
-//			} else {
-//				icallbackwrapper = DoubleBlockCombiner.Combiner::acceptNone;
-//			}
-//			combinedLightIn = icallbackwrapper.apply(new BrightnessCombiner<>()).applyAsInt(combinedLightIn);
-//			Material rendermaterial = this.getMaterial(tileEntityIn, chesttype);
-//			VertexConsumer ivertexbuilder = rendermaterial.buffer(bufferIn, RenderType::entityCutout);
-//			if (chesttype != ChestType.SINGLE) {
-//				if (chesttype == ChestType.LEFT) {
-//					this.renderModels(matrixStackIn, ivertexbuilder, this.leftLid, this.leftLatch, this.leftBottom, combinedLightIn, combinedOverlayIn);
-//				} else {
-//					this.renderModels(matrixStackIn, ivertexbuilder, this.rightLid, this.rightLatch, this.rightBottom, combinedLightIn, combinedOverlayIn);
-//				}
-//			} else {
-//				this.renderModels(matrixStackIn, ivertexbuilder, this.singleLid, this.singleLatch, this.singleBottom, combinedLightIn, combinedOverlayIn);
-//			}
-//			matrixStackIn.popPose();
-//		} else {
-//			LogManager.getLogger(ChestMimicTileEntityRenderer.class).warn("Invalid block used with ChestMimicTileEntityRenderer!");
-//		}
+		Level world = tileEntityIn.getLevel();
+		BlockState blockstate = world != null ? tileEntityIn.getBlockState() : AetherBlocks.CHEST_MIMIC.get().defaultBlockState().setValue(ChestMimicBlock.FACING, Direction.SOUTH);
+		ChestType chesttype = blockstate.hasProperty(ChestBlock.TYPE) ? blockstate.getValue(ChestBlock.TYPE) : ChestType.SINGLE;
+		Block block = blockstate.getBlock();
+		if (block instanceof ChestMimicBlock) {
+			ChestMimicBlock chestMimicBlock = (ChestMimicBlock) block;
+			matrixStackIn.pushPose();
+			float f = blockstate.getValue(ChestBlock.FACING).toYRot();
+			matrixStackIn.translate(0.5D, 0.5D, 0.5D);
+			matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(-f));
+			matrixStackIn.translate(-0.5D, -0.5D, -0.5D);
+			DoubleBlockCombiner.NeighborCombineResult<? extends ChestMimicTileEntity> icallbackwrapper;
+			if (world != null) {
+				icallbackwrapper = chestMimicBlock.combine(blockstate, world, tileEntityIn.getBlockPos(), true);
+			} else {
+				icallbackwrapper = DoubleBlockCombiner.Combiner::acceptNone;
+			}
+			combinedLightIn = icallbackwrapper.apply(new BrightnessCombiner<>()).applyAsInt(combinedLightIn);
+			Material rendermaterial = this.getMaterial(tileEntityIn, chesttype);
+			VertexConsumer ivertexbuilder = rendermaterial.buffer(bufferIn, RenderType::entityCutout);
+			if (chesttype != ChestType.SINGLE) {
+				if (chesttype == ChestType.LEFT) {
+					this.renderModels(matrixStackIn, ivertexbuilder, this.leftLid, this.leftLock, this.leftBottom, combinedLightIn, combinedOverlayIn);
+				} else {
+					this.renderModels(matrixStackIn, ivertexbuilder, this.rightLid, this.rightLock, this.rightBottom, combinedLightIn, combinedOverlayIn);
+				}
+			} else {
+				this.renderModels(matrixStackIn, ivertexbuilder, this.singleLid, this.singleLock, this.singleBottom, combinedLightIn, combinedOverlayIn);
+			}
+			matrixStackIn.popPose();
+		} else {
+			LogManager.getLogger(ChestMimicTileEntityRenderer.class).warn("Invalid block used with ChestMimicTileEntityRenderer!");
+		}
 	}
 
 	private void renderModels(PoseStack matrixStackIn, VertexConsumer bufferIn, ModelPart chestLid, ModelPart chestLatch, ModelPart chestBottom, int combinedLightIn, int combinedOverlayIn) {
--- src/main/java/com/gildedgames/aether/client/renderer/tile/CustomItemStackTileEntityRenderer.java
+++ src/main/java/com/gildedgames/aether/client/renderer/tile/CustomItemStackTileEntityRenderer.java
@@ -9,10 +9,11 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
-public class CustomItemStackTileEntityRenderer extends BlockEntityWithoutLevelRenderer {
+public class CustomItemStackTileEntityRenderer implements BlockEntityRenderer {
 	//private final Supplier<? extends BlockEntity> tileEntityCreator;
 	private /*final*/ BlockEntity tileEntity;
 	
