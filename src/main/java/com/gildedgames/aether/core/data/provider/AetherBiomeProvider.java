601d4ac4109625802fb70ccbd282814faab11e0a
Drullkus <5010174+Drullkus@users.noreply.github.com>
2021-12-28 12:04:07 -0800

    feat: Proper implementation of Aether Surface Rules (#303)
    * chore: Mapped features from the deleted biome.json to Generation Steps into a TODO
    
    * feat: Proper implementation of Aether Surface Rules

--- src/main/java/com/gildedgames/aether/core/data/provider/AetherBiomeProvider.java
+++ src/main/java/com/gildedgames/aether/core/data/provider/AetherBiomeProvider.java
@@ -1,21 +1,12 @@
 package com.gildedgames.aether.core.data.provider;
 
 import com.gildedgames.aether.client.registry.AetherSoundEvents;
-import com.gildedgames.aether.common.registry.AetherBlocks;
 import com.gildedgames.aether.common.registry.AetherEntityTypes;
 import com.gildedgames.aether.core.data.AetherFeatureData;
-import com.google.common.collect.ImmutableList;
-import net.minecraft.data.worldgen.SurfaceRuleData;
 import net.minecraft.sounds.Music;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.*;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.GenerationStep;
-import net.minecraft.world.level.levelgen.Noises;
-import net.minecraft.world.level.levelgen.SurfaceRules;
-import net.minecraft.world.level.levelgen.VerticalAnchor;
-import net.minecraft.world.level.levelgen.placement.CaveSurface;
 
 public class AetherBiomeProvider {
     public static Biome makeDefaultBiome() {
@@ -48,6 +39,26 @@ public class AetherBiomeProvider {
                         .addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(AetherEntityTypes.AERWHALE_TYPE, 2, 1, 1))
                         .build(),
                 new BiomeGenerationSettings.Builder()
+                        // TODO GenerationStep.Decoration.RAW_GENERATION
+                        //  "aether:quicksoil"
+                        //  "aether:crystal_tree"
+                        // TODO GenerationStep.Decoration.LAKES
+                        //  "aether:water_lake"
+                        // TODO GenerationStep.Decoration.UNDERGROUND_ORES
+                        //   "aether:ore_aether_dirt",
+                        //   "aether:ore_icestone",
+                        //   "aether:ore_ambrosium",
+                        //   "aether:ore_zanite",
+                        //   "aether:ore_gravitite"
+                        // TODO GenerationStep.Decoration.FLUID_SPRINGS
+                        //   "aether:spring_water"
+                        // TODO GenerationStep.Decoration.VEGETAL_DECORATION
+                        //   "aether:grass_patch",
+                        //   "aether:tall_grass_patch",
+                        //   "aether:aether_skylands_flowers",
+                        //   "aether:tree_skyroot",
+                        //   "aether:tree_golden_oak",
+                        //   "aether:holiday_tree"
                         .addFeature(GenerationStep.Decoration.TOP_LAYER_MODIFICATION, AetherFeatureData.COLD_AERCLOUD_FEATURE)
                         .addFeature(GenerationStep.Decoration.TOP_LAYER_MODIFICATION, AetherFeatureData.BLUE_AERCLOUD_FEATURE)
                         .addFeature(GenerationStep.Decoration.TOP_LAYER_MODIFICATION, AetherFeatureData.GOLDEN_AERCLOUD_FEATURE)
@@ -78,28 +89,5 @@ public class AetherBiomeProvider {
                 .temperatureAdjustment(temperatureModifier)
                 .build()
                 ;
-    }
-
-
-    private static final SurfaceRules.RuleSource GRASS_BLOCK = makeStateRule(AetherBlocks.AETHER_GRASS_BLOCK.get());
-    private static final SurfaceRules.RuleSource DIRT = makeStateRule(AetherBlocks.AETHER_DIRT.get());
-    private static final SurfaceRules.RuleSource QUICKSOIL = makeStateRule(AetherBlocks.QUICKSOIL.get());
-    private static final SurfaceRules.RuleSource HOLYSTONE = makeStateRule(AetherBlocks.HOLYSTONE.get());
-
-    private static SurfaceRules.RuleSource makeStateRule(Block p_194811_) {
-        return SurfaceRules.state(p_194811_.defaultBlockState());
-    }
-
-    public static SurfaceRules.RuleSource aetherSurfaceRules() {
-        SurfaceRules.ConditionSource surfacerules$conditionsource6 = SurfaceRules.waterBlockCheck(-1, 0);
-
-        SurfaceRules.RuleSource underwaterGrassToDirt = SurfaceRules.sequence(SurfaceRules.ifTrue(surfacerules$conditionsource6, GRASS_BLOCK), DIRT);
-
-        return SurfaceRules.sequence(SurfaceRules.ifTrue(surfaceNoiseAbove(1.0D), HOLYSTONE),
-                SurfaceRules.ifTrue(surfaceNoiseAbove(-1.0D), underwaterGrassToDirt), HOLYSTONE);
-    }
-
-    private static SurfaceRules.ConditionSource surfaceNoiseAbove(double p_194809_) {
-        return SurfaceRules.noiseCondition(Noises.SURFACE, p_194809_ / 8.25D, Double.MAX_VALUE);
     }
 }
