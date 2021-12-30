154e6ee95c39e9d6a3fdafb634e68c97ffae319d
baguchan <shurf012@gmail.com>
2021-12-30 14:40:26 +0900

    refactor

--- src/main/java/com/gildedgames/aether/client/renderer/entity/model/SheepuffModel.java
+++ src/main/java/com/gildedgames/aether/client/renderer/entity/model/SheepuffModel.java
@@ -4,11 +4,7 @@ import com.gildedgames.aether.common.entity.passive.SheepuffEntity;
 import net.minecraft.client.model.QuadrupedModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.CubeDeformation;
-import net.minecraft.client.model.geom.builders.CubeListBuilder;
-import net.minecraft.client.model.geom.builders.LayerDefinition;
-import net.minecraft.client.model.geom.builders.MeshDefinition;
-import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.model.geom.builders.*;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
