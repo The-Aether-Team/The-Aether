05c68fbc1a8a07451167821d54073066fdcefd5d
Drullkus <failbookftw@gmail.com>
2022-01-04 00:13:32 -0800

    fix: Updated elevation check for Aether Portals, fixes #345

--- src/main/java/com/gildedgames/aether/common/item/accessories/ring/IceRingItem.java
+++ src/main/java/com/gildedgames/aether/common/item/accessories/ring/IceRingItem.java
@@ -4,7 +4,6 @@ import com.gildedgames.aether.client.registry.AetherSoundEvents;
 import com.gildedgames.aether.common.item.accessories.abilities.FreezingItem;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import top.theillusivec4.curios.api.CuriosApi;
 
 public class IceRingItem extends RingItem implements FreezingItem
 {
@@ -14,7 +13,8 @@ public class IceRingItem extends RingItem implements FreezingItem
 
     @Override
     public void curioTick(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
-        int damage = this.freezeBlocks(livingEntity.level, livingEntity.blockPosition(), stack, 1.9f);
-        stack.hurtAndBreak(damage / 3, livingEntity, entity -> CuriosApi.getCuriosHelper().onBrokenCurio(identifier, index, entity));
+        stack.shrink(1);
+        //int damage = this.freezeBlocks(livingEntity.level, livingEntity.blockPosition(), stack, 1.9f);
+        //stack.hurtAndBreak(damage / 3, livingEntity, entity -> CuriosApi.getCuriosHelper().onBrokenCurio(identifier, index, entity));
     }
 }
