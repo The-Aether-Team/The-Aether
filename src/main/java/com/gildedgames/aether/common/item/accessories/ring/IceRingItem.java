20dde70da2fea11cee58f27b1a94ffe4ec09f4e3
Drullkus <failbookftw@gmail.com>
2022-01-04 00:48:46 -0800

    chore: Remove testing code

--- src/main/java/com/gildedgames/aether/common/item/accessories/ring/IceRingItem.java
+++ src/main/java/com/gildedgames/aether/common/item/accessories/ring/IceRingItem.java
@@ -4,6 +4,7 @@ import com.gildedgames.aether.client.registry.AetherSoundEvents;
 import com.gildedgames.aether.common.item.accessories.abilities.FreezingItem;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.CuriosApi;
 
 public class IceRingItem extends RingItem implements FreezingItem
 {
@@ -13,8 +14,7 @@ public class IceRingItem extends RingItem implements FreezingItem
 
     @Override
     public void curioTick(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
-        stack.shrink(1);
-        //int damage = this.freezeBlocks(livingEntity.level, livingEntity.blockPosition(), stack, 1.9f);
-        //stack.hurtAndBreak(damage / 3, livingEntity, entity -> CuriosApi.getCuriosHelper().onBrokenCurio(identifier, index, entity));
+        int damage = this.freezeBlocks(livingEntity.level, livingEntity.blockPosition(), stack, 1.9f);
+        stack.hurtAndBreak(damage / 3, livingEntity, entity -> CuriosApi.getCuriosHelper().onBrokenCurio(identifier, index, entity));
     }
 }
