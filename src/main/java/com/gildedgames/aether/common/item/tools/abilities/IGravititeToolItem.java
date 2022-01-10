eb41ceb5648caf789d8d0ca6058e0d68d6e1c77f
quek <55364841+quek04@users.noreply.github.com>
2022-01-09 21:11:31 -0600

    fix: gravitite ability interface

--- src/main/java/com/gildedgames/aether/common/item/tools/abilities/IGravititeToolItem.java
+++ src/main/java/com/gildedgames/aether/common/item/tools/abilities/IGravititeToolItem.java
@@ -1,17 +1,15 @@
 package com.gildedgames.aether.common.item.tools.abilities;
 
 import com.gildedgames.aether.common.entity.block.FloatingBlockEntity;
-
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.common.ForgeHooks;
+import net.minecraft.world.level.block.state.BlockState;
 
 public interface IGravititeToolItem
 {
@@ -22,23 +20,22 @@ public interface IGravititeToolItem
 		BlockState state = world.getBlockState(pos);
 		Player player = context.getPlayer();
 		InteractionHand hand = context.getHand();
-		//TODO: Make work with new effective tool tags.
-//		if (player != null) {
-//			if ((destroySpeed == efficiency || ForgeHooks.isToolEffective(world, pos, heldItem)) && world.isEmptyBlock(pos.above())) {
-//				if (world.getBlockEntity(pos) == null && state.getDestroySpeed(world, pos) != -1.0F) {
-//					if (!world.isClientSide) {
-//						FloatingBlockEntity entity = new FloatingBlockEntity(world, pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5, state);
-//						if (world.getBlockState(pos).is(BlockTags.ANVIL)) {
-//							entity.setHurtsEntities(true);
-//						}
-//						world.removeBlock(pos, false);
-//						world.addFreshEntity(entity);
-//						heldItem.hurtAndBreak(4, player, (p) -> p.broadcastBreakEvent(hand));
-//					}
-//					return InteractionResult.sidedSuccess(world.isClientSide);
-//				}
-//			}
-//		}
+		if (player != null) {
+			if ((destroySpeed == efficiency || heldItem.isCorrectToolForDrops(state)) && world.isEmptyBlock(pos.above())) {
+				if (world.getBlockEntity(pos) == null && state.getDestroySpeed(world, pos) != -1.0F) {
+					if (!world.isClientSide) {
+						FloatingBlockEntity entity = new FloatingBlockEntity(world, pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5, state);
+						if (world.getBlockState(pos).is(BlockTags.ANVIL)) {
+							entity.setHurtsEntities(true);
+						}
+						world.removeBlock(pos, false);
+						world.addFreshEntity(entity);
+						heldItem.hurtAndBreak(4, player, (p) -> p.broadcastBreakEvent(hand));
+					}
+					return InteractionResult.sidedSuccess(world.isClientSide);
+				}
+			}
+		}
 		return InteractionResult.PASS;
 	}
 }
