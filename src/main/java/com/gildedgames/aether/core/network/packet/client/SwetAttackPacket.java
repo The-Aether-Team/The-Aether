9fe1116029d4f5dcd47faf135424b64e4f64bee7
Archie <azwerlein77@gmail.com>
2022-02-02 11:51:59 -0600

    Set the swet's speed through the packet

--- src/main/java/com/gildedgames/aether/core/network/packet/client/SwetAttackPacket.java
+++ src/main/java/com/gildedgames/aether/core/network/packet/client/SwetAttackPacket.java
@@ -1,6 +1,9 @@
 package com.gildedgames.aether.core.network.packet.client;
 
+import com.gildedgames.aether.common.entity.monster.Swet;
 import com.gildedgames.aether.core.network.IAetherPacket.AetherPacket;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
@@ -43,6 +46,8 @@ public class SwetAttackPacket extends AetherPacket {
 
     @Override
     public void execute(Player player) {
-
+        if (Minecraft.getInstance().player != null && Minecraft.getInstance().level != null && Minecraft.getInstance().player.level.getEntity(this.swetID) instanceof Swet swet) {
+            swet.setDeltaMovement(swet.getDeltaMovement().x + (this.xSpeed * 1.5F), swet.getDeltaMovement().y, swet.getDeltaMovement().z + (this.zSpeed * 1.5F));
+        }
     }
 }
