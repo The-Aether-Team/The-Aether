f9c0d28509811f2e4286746f64bcf4444800e640
Archie <azwerlein77@gmail.com>
2022-02-07 18:08:06 -0600

    Reformatted the Aether Time command

--- src/main/java/com/gildedgames/aether/server/command/AetherTimeCommand.java
+++ src/main/java/com/gildedgames/aether/server/command/AetherTimeCommand.java
@@ -13,25 +13,27 @@ import net.minecraft.server.level.ServerLevel;
 // Vanilla copy of TimeCommand
 public class AetherTimeCommand {
     public static void register(CommandDispatcher<CommandSourceStack> pDispatcher) {
-        pDispatcher.register(Commands.literal("aethertime").requires((commandSourceStack) -> {
-            return commandSourceStack.hasPermission(2);
-        }).then(Commands.literal("set").then(Commands.literal("day").executes((context) -> {
-            return setTime(context.getSource(), 3000);
-        })).then(Commands.literal("noon").executes((context) -> {
-            return setTime(context.getSource(), 18000);
-        })).then(Commands.literal("night").executes((context) -> {
-            return setTime(context.getSource(), 39000);
-        })).then(Commands.literal("midnight").executes((context) -> {
-            return setTime(context.getSource(), 54000);
-        })).then(Commands.argument("time", TimeArgument.time()).executes((context) -> {
-            return setTime(context.getSource(), IntegerArgumentType.getInteger(context, "time"));
-        }))).then(Commands.literal("add").then(Commands.argument("time", TimeArgument.time()).executes((context) -> {
-            return addTime(context.getSource(), IntegerArgumentType.getInteger(context, "time"));
-        }))).then(Commands.literal("query").then(Commands.literal("daytime").executes((context) -> {
-            return queryTime(context.getSource(), getDayTime(context.getSource().getLevel()));
-        })).then(Commands.literal("day").executes((context) -> {
-            return queryTime(context.getSource(), (int)(context.getSource().getLevel().getDayTime() / 72000L % 2147483647L));
-        }))));
+        pDispatcher.register(Commands.literal("aether")
+                .then(Commands.literal("time").requires((commandSourceStack) -> {
+                    return commandSourceStack.hasPermission(2);
+                }).then(Commands.literal("set").then(Commands.literal("day").executes((context) -> {
+                    return setTime(context.getSource(), 3000);
+                })).then(Commands.literal("noon").executes((context) -> {
+                    return setTime(context.getSource(), 18000);
+                })).then(Commands.literal("night").executes((context) -> {
+                    return setTime(context.getSource(), 39000);
+                })).then(Commands.literal("midnight").executes((context) -> {
+                    return setTime(context.getSource(), 54000);
+                })).then(Commands.argument("time", TimeArgument.time()).executes((context) -> {
+                    return setTime(context.getSource(), IntegerArgumentType.getInteger(context, "time"));
+                }))).then(Commands.literal("add").then(Commands.argument("time", TimeArgument.time()).executes((context) -> {
+                    return addTime(context.getSource(), IntegerArgumentType.getInteger(context, "time"));
+                }))).then(Commands.literal("query").then(Commands.literal("daytime").executes((context) -> {
+                    return queryTime(context.getSource(), getDayTime(context.getSource().getLevel()));
+                })).then(Commands.literal("day").executes((context) -> {
+                    return queryTime(context.getSource(), (int)(context.getSource().getLevel().getDayTime() / 72000L % 2147483647L));
+                }))))
+        );
     }
 
     /**
