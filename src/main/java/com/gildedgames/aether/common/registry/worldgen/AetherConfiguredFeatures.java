21aaca3e09ac6ac1a3c7100d0d5e0d3f4e8a2401
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-03-20 09:26:03 -0700

    fix: Register placed and configured features to the Aether namespace.

--- src/main/java/com/gildedgames/aether/common/registry/worldgen/AetherConfiguredFeatures.java
+++ src/main/java/com/gildedgames/aether/common/registry/worldgen/AetherConfiguredFeatures.java
@@ -1,5 +1,6 @@
 package com.gildedgames.aether.common.registry.worldgen;
 
+import com.gildedgames.aether.Aether;
 import com.gildedgames.aether.common.block.state.properties.AetherBlockStateProperties;
 import com.gildedgames.aether.common.registry.AetherBlocks;
 import com.gildedgames.aether.common.registry.AetherTags;
@@ -7,8 +8,10 @@ import com.gildedgames.aether.common.world.feature.FeatureBuilders;
 import com.gildedgames.aether.common.world.gen.configuration.AercloudConfiguration;
 import com.gildedgames.aether.common.world.gen.configuration.SimpleDiskConfiguration;
 import net.minecraft.core.Holder;
+import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.util.valueproviders.ConstantInt;
@@ -37,16 +40,16 @@ import java.util.function.Supplier;
 public class AetherConfiguredFeatures {
     public static final RuleTest HOLYSTONE = new TagMatchTest(AetherTags.Blocks.HOLYSTONE);
 
-    public static final Holder<ConfiguredFeature<AercloudConfiguration, ?>> COLD_AERCLOUD = FeatureUtils.register("cold_aercloud", AetherFeaturesForge.AERCLOUD.get(),
+    public static final Holder<ConfiguredFeature<AercloudConfiguration, ?>> COLD_AERCLOUD = register("cold_aercloud", AetherFeaturesForge.AERCLOUD.get(),
             createAercloudConfig(16, AetherBlocks.COLD_AERCLOUD.get().defaultBlockState()));
-    public static final Holder<ConfiguredFeature<AercloudConfiguration, ?>> BLUE_AERCLOUD = FeatureUtils.register("blue_aercloud", AetherFeaturesForge.AERCLOUD.get(),
+    public static final Holder<ConfiguredFeature<AercloudConfiguration, ?>> BLUE_AERCLOUD = register("blue_aercloud", AetherFeaturesForge.AERCLOUD.get(),
             createAercloudConfig(8, AetherBlocks.BLUE_AERCLOUD.get().defaultBlockState()));
-    public static final Holder<ConfiguredFeature<AercloudConfiguration, ?>> GOLDEN_AERCLOUD = FeatureUtils.register("golden_aercloud", AetherFeaturesForge.AERCLOUD.get(),
+    public static final Holder<ConfiguredFeature<AercloudConfiguration, ?>> GOLDEN_AERCLOUD = register("golden_aercloud", AetherFeaturesForge.AERCLOUD.get(),
             createAercloudConfig(4, AetherBlocks.GOLDEN_AERCLOUD.get().defaultBlockState()));
-    public static final Holder<ConfiguredFeature<AercloudConfiguration, ?>> PINK_AERCLOUD = FeatureUtils.register("pink_aercloud", AetherFeaturesForge.AERCLOUD.get(),
+    public static final Holder<ConfiguredFeature<AercloudConfiguration, ?>> PINK_AERCLOUD = register("pink_aercloud", AetherFeaturesForge.AERCLOUD.get(),
             createAercloudConfig(1, AetherBlocks.PINK_AERCLOUD.get().defaultBlockState()));
 
-    public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> SKYROOT_TREE_CONFIGURED_FEATURE = FeatureUtils.register("skyroot_tree", Feature.TREE,
+    public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> SKYROOT_TREE_CONFIGURED_FEATURE = register("skyroot_tree", Feature.TREE,
             new TreeConfiguration.TreeConfigurationBuilder(
                     BlockStateProvider.simple(getDoubleDrops(AetherBlocks.SKYROOT_LOG)),
                     new StraightTrunkPlacer(4, 2, 0),
@@ -54,7 +57,7 @@ public class AetherConfiguredFeatures {
                     new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 3),
                     new TwoLayersFeatureSize(1, 0, 1)).ignoreVines().build());
 
-    public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> GOLDEN_OAK_TREE_CONFIGURED_FEATURE = FeatureUtils.register("golden_oak_tree", Feature.TREE,
+    public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> GOLDEN_OAK_TREE_CONFIGURED_FEATURE = register("golden_oak_tree", Feature.TREE,
             new TreeConfiguration.TreeConfigurationBuilder(
                     BlockStateProvider.simple(getDoubleDrops(AetherBlocks.GOLDEN_OAK_LOG)),
                     new FancyTrunkPlacer(3, 11, 0),
@@ -62,32 +65,32 @@ public class AetherConfiguredFeatures {
                     new FancyFoliagePlacer(ConstantInt.of(2), ConstantInt.of(4), 4),
                     new TwoLayersFeatureSize(0, 0, 0, OptionalInt.of(4))).ignoreVines().build());
 
-    public static final Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> FLOWER_PATCH_CONFIGURED_FEATURE = FeatureUtils.register("flower_patch", Feature.FLOWER,
+    public static final Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> FLOWER_PATCH_CONFIGURED_FEATURE = register("flower_patch", Feature.FLOWER,
             grassPatch(new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder()
                     .add(AetherBlocks.PURPLE_FLOWER.get().defaultBlockState(), 2)
                     .add(AetherBlocks.WHITE_FLOWER.get().defaultBlockState(), 2)
                     .add(getDoubleDrops(AetherBlocks.BERRY_BUSH), 1)), 64));
 
-    public static final Holder<ConfiguredFeature<SimpleDiskConfiguration, ?>> QUICKSOIL_SHELF_CONFIGURED_FEATURE = FeatureUtils.register("quicksoil_shelf", AetherFeaturesForge.SIMPLE_DISK.get(),
+    public static final Holder<ConfiguredFeature<SimpleDiskConfiguration, ?>> QUICKSOIL_SHELF_CONFIGURED_FEATURE = register("quicksoil_shelf", AetherFeaturesForge.SIMPLE_DISK.get(),
             new SimpleDiskConfiguration(
                     UniformFloat.of(Mth.sqrt(12), 5), // sqrt(12) is old static value
                     BlockStateProvider.simple(getDoubleDrops(AetherBlocks.QUICKSOIL)),
                     3));
 
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_AETHER_DIRT_CONFIGURED_FEATURE = FeatureUtils.register("aether_dirt_ore", Feature.ORE,
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_AETHER_DIRT_CONFIGURED_FEATURE = register("aether_dirt_ore", Feature.ORE,
             new OreConfiguration(HOLYSTONE, AetherBlocks.AETHER_DIRT.get().defaultBlockState(), 33));
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_ICESTONE_CONFIGURED_FEATURE = FeatureUtils.register("icestone_ore", Feature.ORE,
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_ICESTONE_CONFIGURED_FEATURE = register("icestone_ore", Feature.ORE,
             new OreConfiguration(HOLYSTONE, AetherBlocks.ICESTONE.get().defaultBlockState(), 16));
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_AMBROSIUM_CONFIGURED_FEATURE = FeatureUtils.register("ambrosium_ore", Feature.ORE,
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_AMBROSIUM_CONFIGURED_FEATURE = register("ambrosium_ore", Feature.ORE,
             new OreConfiguration(HOLYSTONE, getDoubleDrops(AetherBlocks.AMBROSIUM_ORE), 16));
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_ZANITE_CONFIGURED_FEATURE = FeatureUtils.register("zanite_ore", Feature.ORE,
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_ZANITE_CONFIGURED_FEATURE = register("zanite_ore", Feature.ORE,
             new OreConfiguration(HOLYSTONE, AetherBlocks.ZANITE_ORE.get().defaultBlockState(), 8));
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_GRAVITITE_COMMON_CONFIGURED_FEATURE = FeatureUtils.register("gravitite_ore_common", Feature.ORE,
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_GRAVITITE_COMMON_CONFIGURED_FEATURE = register("gravitite_ore_common", Feature.ORE,
             new OreConfiguration(HOLYSTONE, AetherBlocks.GRAVITITE_ORE.get().defaultBlockState(), 6, 0.9F));
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_GRAVITITE_DENSE_CONFIGURED_FEATURE = FeatureUtils.register("gravitite_ore_dense", Feature.ORE,
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_GRAVITITE_DENSE_CONFIGURED_FEATURE = register("gravitite_ore_dense", Feature.ORE,
             new OreConfiguration(HOLYSTONE, AetherBlocks.GRAVITITE_ORE.get().defaultBlockState(), 3, 0.5F));
 
-    public static final Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> TREE_BLEND = FeatureUtils.register("aether_tree_mix", Feature.RANDOM_SELECTOR,
+    public static final Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> TREE_BLEND = register("aether_tree_mix", Feature.RANDOM_SELECTOR,
             new RandomFeatureConfiguration(
                     List.of(new WeightedPlacedFeature(Holder.direct(new PlacedFeature(Holder.hackyErase(AetherConfiguredFeatures.GOLDEN_OAK_TREE_CONFIGURED_FEATURE), List.of(FeatureBuilders.copyBlockSurvivability(AetherBlocks.GOLDEN_OAK_SAPLING.get())))), 0.01f)),
                     Holder.direct(new PlacedFeature(Holder.hackyErase(AetherConfiguredFeatures.SKYROOT_TREE_CONFIGURED_FEATURE), List.of(FeatureBuilders.copyBlockSurvivability(AetherBlocks.SKYROOT_SAPLING.get()))))
@@ -112,5 +115,9 @@ public class AetherConfiguredFeatures {
 
     public static BlockState getDoubleDrops(BlockState blockState) {
         return blockState.setValue(AetherBlockStateProperties.DOUBLE_DROPS, true);
+    }
+
+    public static <FC extends FeatureConfiguration, F extends Feature<FC>> Holder<ConfiguredFeature<FC, ?>> register(String name, F feature, FC configuredFeature) {
+        return BuiltinRegistries.registerExact(BuiltinRegistries.CONFIGURED_FEATURE, new ResourceLocation(Aether.MODID, name).toString(), new ConfiguredFeature<>(feature, configuredFeature));
     }
 }
--- src/main/java/com/gildedgames/aether/common/registry/worldgen/AetherPlacements.java
+++ src/main/java/com/gildedgames/aether/common/registry/worldgen/AetherPlacements.java
@@ -1,49 +1,53 @@
 package com.gildedgames.aether.common.registry.worldgen;
 
+import com.gildedgames.aether.Aether;
 import com.gildedgames.aether.common.registry.AetherBlocks;
 import com.gildedgames.aether.common.registry.AetherTags;
 import com.gildedgames.aether.common.world.gen.placement.ElevationAdjustment;
 import com.gildedgames.aether.common.world.gen.placement.ElevationFilter;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
+import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.placement.*;
 
 import java.util.List;
 
 public class AetherPlacements {
-    public static final Holder<PlacedFeature> COLD_AERCLOUD_PLACED_FEATURE = PlacementUtils.register("cold_aercloud", AetherConfiguredFeatures.COLD_AERCLOUD, createAercloudPlacements(128, 5));
-    public static final Holder<PlacedFeature> BLUE_AERCLOUD_PLACED_FEATURE = PlacementUtils.register("blue_aercloud", AetherConfiguredFeatures.BLUE_AERCLOUD, createAercloudPlacements(96, 5));
-    public static final Holder<PlacedFeature> GOLDEN_AERCLOUD_PLACED_FEATURE = PlacementUtils.register("golden_aercloud", AetherConfiguredFeatures.GOLDEN_AERCLOUD, createAercloudPlacements(160, 5));
-    public static final Holder<PlacedFeature> PINK_AERCLOUD_PLACED_FEATURE = PlacementUtils.register("pink_aercloud", AetherConfiguredFeatures.PINK_AERCLOUD, createAercloudPlacements(160, 7));
+    public static final Holder<PlacedFeature> COLD_AERCLOUD_PLACED_FEATURE = register("cold_aercloud", AetherConfiguredFeatures.COLD_AERCLOUD, createAercloudPlacements(128, 5));
+    public static final Holder<PlacedFeature> BLUE_AERCLOUD_PLACED_FEATURE = register("blue_aercloud", AetherConfiguredFeatures.BLUE_AERCLOUD, createAercloudPlacements(96, 5));
+    public static final Holder<PlacedFeature> GOLDEN_AERCLOUD_PLACED_FEATURE = register("golden_aercloud", AetherConfiguredFeatures.GOLDEN_AERCLOUD, createAercloudPlacements(160, 5));
+    public static final Holder<PlacedFeature> PINK_AERCLOUD_PLACED_FEATURE = register("pink_aercloud", AetherConfiguredFeatures.PINK_AERCLOUD, createAercloudPlacements(160, 7));
 
-    public static final Holder<PlacedFeature> SKYROOT_TREE_PLACED_FEATURE = PlacementUtils.register("skyroot_tree", AetherConfiguredFeatures.SKYROOT_TREE_CONFIGURED_FEATURE,
+    public static final Holder<PlacedFeature> SKYROOT_TREE_PLACED_FEATURE = register("skyroot_tree", AetherConfiguredFeatures.SKYROOT_TREE_CONFIGURED_FEATURE,
             VegetationPlacements.treePlacement(PlacementUtils.countExtra(6, 0.1F, 1), AetherBlocks.SKYROOT_SAPLING.get()));
-    public static final Holder<PlacedFeature> GOLDEN_OAK_TREE_PLACED_FEATURE = PlacementUtils.register("golden_oak_tree", AetherConfiguredFeatures.GOLDEN_OAK_TREE_CONFIGURED_FEATURE,
+    public static final Holder<PlacedFeature> GOLDEN_OAK_TREE_PLACED_FEATURE = register("golden_oak_tree", AetherConfiguredFeatures.GOLDEN_OAK_TREE_CONFIGURED_FEATURE,
             VegetationPlacements.treePlacement(PlacementUtils.countExtra(1, 0.1F, 1), AetherBlocks.GOLDEN_OAK_SAPLING.get()));
 
-    public static final Holder<PlacedFeature> FLOWER_PATCH_PLACED_FEATURE = PlacementUtils.register("flower_patch", AetherConfiguredFeatures.FLOWER_PATCH_CONFIGURED_FEATURE,
+    public static final Holder<PlacedFeature> FLOWER_PATCH_PLACED_FEATURE = register("flower_patch", AetherConfiguredFeatures.FLOWER_PATCH_CONFIGURED_FEATURE,
             InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome());
 
-    public static final Holder<PlacedFeature> QUICKSOIL_SHELF_PLACED_FEATURE = PlacementUtils.register("quicksoil_shelf", AetherConfiguredFeatures.QUICKSOIL_SHELF_CONFIGURED_FEATURE,
+    public static final Holder<PlacedFeature> QUICKSOIL_SHELF_PLACED_FEATURE = register("quicksoil_shelf", AetherConfiguredFeatures.QUICKSOIL_SHELF_CONFIGURED_FEATURE,
             InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP_WORLD_SURFACE, new ElevationAdjustment(UniformInt.of(-4, -2)), new ElevationFilter(47, 70), BlockPredicateFilter.forPredicate(BlockPredicate.anyOf(BlockPredicate.matchesBlock(AetherBlocks.AETHER_DIRT.get(), BlockPos.ZERO), BlockPredicate.matchesTag(AetherTags.Blocks.HOLYSTONE))));
     // FIXME once Terrain can go above 63 again, change 47 -> 63
 
-    public static final Holder<PlacedFeature> ORE_AETHER_DIRT_PLACED_FEATURE = PlacementUtils.register("aether_dirt_ore", AetherConfiguredFeatures.ORE_AETHER_DIRT_CONFIGURED_FEATURE,
+    public static final Holder<PlacedFeature> ORE_AETHER_DIRT_PLACED_FEATURE = register("aether_dirt_ore", AetherConfiguredFeatures.ORE_AETHER_DIRT_CONFIGURED_FEATURE,
             commonOrePlacement(10, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(0), VerticalAnchor.belowTop(0))));
-    public static final Holder<PlacedFeature> ORE_ICESTONE_PLACED_FEATURE = PlacementUtils.register("icestone_ore", AetherConfiguredFeatures.ORE_ICESTONE_CONFIGURED_FEATURE,
+    public static final Holder<PlacedFeature> ORE_ICESTONE_PLACED_FEATURE = register("icestone_ore", AetherConfiguredFeatures.ORE_ICESTONE_CONFIGURED_FEATURE,
             commonOrePlacement(10, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(0), VerticalAnchor.belowTop(0))));
-    public static final Holder<PlacedFeature> ORE_AMBROSIUM_PLACED_FEATURE = PlacementUtils.register("ambrosium_ore", AetherConfiguredFeatures.ORE_AMBROSIUM_CONFIGURED_FEATURE,
+    public static final Holder<PlacedFeature> ORE_AMBROSIUM_PLACED_FEATURE = register("ambrosium_ore", AetherConfiguredFeatures.ORE_AMBROSIUM_CONFIGURED_FEATURE,
             commonOrePlacement(10, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(0), VerticalAnchor.belowTop(0))));
-    public static final Holder<PlacedFeature> ORE_ZANITE_PLACED_FEATURE = PlacementUtils.register("zanite_ore", AetherConfiguredFeatures.ORE_ZANITE_CONFIGURED_FEATURE,
+    public static final Holder<PlacedFeature> ORE_ZANITE_PLACED_FEATURE = register("zanite_ore", AetherConfiguredFeatures.ORE_ZANITE_CONFIGURED_FEATURE,
             commonOrePlacement(10, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(0), VerticalAnchor.belowTop(0))));
-    public static final Holder<PlacedFeature> ORE_GRAVITITE_COMMON_PLACED_FEATURE = PlacementUtils.register("gravitite_ore_common", AetherConfiguredFeatures.ORE_GRAVITITE_COMMON_CONFIGURED_FEATURE,
+    public static final Holder<PlacedFeature> ORE_GRAVITITE_COMMON_PLACED_FEATURE = register("gravitite_ore_common", AetherConfiguredFeatures.ORE_GRAVITITE_COMMON_CONFIGURED_FEATURE,
             commonOrePlacement(7, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(0), VerticalAnchor.belowTop(0))));
-    public static final Holder<PlacedFeature> ORE_GRAVITITE_DENSE_PLACED_FEATURE = PlacementUtils.register("gravitite_ore_dense", AetherConfiguredFeatures.ORE_GRAVITITE_DENSE_CONFIGURED_FEATURE,
+    public static final Holder<PlacedFeature> ORE_GRAVITITE_DENSE_PLACED_FEATURE = register("gravitite_ore_dense", AetherConfiguredFeatures.ORE_GRAVITITE_DENSE_CONFIGURED_FEATURE,
             commonOrePlacement(3, HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(0), VerticalAnchor.belowTop(0))));
 
     // TODO investigate changing this to triangle
@@ -62,5 +66,13 @@ public class AetherPlacements {
 
     public static List<PlacementModifier> rareOrePlacement(int p_195350_, PlacementModifier p_195351_) {
         return orePlacement(RarityFilter.onAverageOnceEvery(p_195350_), p_195351_);
+    }
+
+    public static Holder<PlacedFeature> register(String name, Holder<? extends ConfiguredFeature<?, ?>> configuredFeature, List<PlacementModifier> placementModifiers) {
+        return BuiltinRegistries.register(BuiltinRegistries.PLACED_FEATURE, new ResourceLocation(Aether.MODID, name).toString(), new PlacedFeature(Holder.hackyErase(configuredFeature), List.copyOf(placementModifiers)));
+    }
+
+    public static Holder<PlacedFeature> register(String name, Holder<? extends ConfiguredFeature<?, ?>> configuredFeature, PlacementModifier... placementModifiers) {
+        return register(name, configuredFeature, List.of(placementModifiers));
     }
 }
