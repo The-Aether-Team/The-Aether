5b283aa3abba7cefb342c8c0748afba8f76e857b
Drullkus <failbookftw@gmail.com>
2022-03-21 18:52:27 -0700

    Fix: Re-allow potential Aether addon worlds to use our logic

--- src/main/java/com/gildedgames/aether/core/util/LevelUtil.java
+++ src/main/java/com/gildedgames/aether/core/util/LevelUtil.java
@@ -1,10 +1,14 @@
 package com.gildedgames.aether.core.util;
 
-import com.gildedgames.aether.common.registry.worldgen.AetherDimensions;
+import com.gildedgames.aether.Aether;
 import net.minecraft.world.level.Level;
 
 public final class LevelUtil {
+    // This way if anyone wants to create a new dimension, it will be compatible with as much Aether logic as possible
     public static boolean isLevelAether(Level level) {
-        return level.dimension() == AetherDimensions.AETHER_LEVEL; //TODO: Verify this works.
+        return Aether.MODID.equals(level.dimension().location().getNamespace());
+    }
+
+    private LevelUtil() {
     }
 }
