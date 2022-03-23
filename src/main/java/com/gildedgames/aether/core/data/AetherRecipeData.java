244986445554e37cf9659a29f5e2828dce6d61e7
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-03-22 17:40:55 -0700

    refactor: Removed unused import.

--- src/main/java/com/gildedgames/aether/core/data/AetherRecipeData.java
+++ src/main/java/com/gildedgames/aether/core/data/AetherRecipeData.java
@@ -9,7 +9,6 @@ import net.minecraft.advancements.critereon.EntityPredicate;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.advancements.critereon.MinMaxBounds;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
