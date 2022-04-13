516b23047923276b6d769cc6487de4e7409d6618
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-04-12 19:38:44 -0700

    fix: Aether ores now drop experience.

--- src/main/java/com/gildedgames/aether/common/block/natural/AetherOreBlock.java
+++ src/main/java/com/gildedgames/aether/common/block/natural/AetherOreBlock.java
@@ -1,26 +1,25 @@
 package com.gildedgames.aether.common.block.natural;
 
-import java.util.Random;
-
 import com.gildedgames.aether.common.block.util.IAetherDoubleDropBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.OreBlock;
-import net.minecraft.util.Mth;
 
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class AetherOreBlock extends OreBlock implements IAetherDoubleDropBlock
 {
-	private final int minExpDropped;
-	private final int maxExpDropped;
+	private final UniformInt xpRange;
 	
-	public AetherOreBlock(int minExpDropped, int maxExpDropped, BlockBehaviour.Properties properties) {
+	public AetherOreBlock(UniformInt xpRange, BlockBehaviour.Properties properties) {
 		super(properties);
-		this.minExpDropped = minExpDropped;
-		this.maxExpDropped = maxExpDropped;
+		this.xpRange = xpRange;
 	}
-	
-//	@Override
-//	protected int xpOnDrop(Random rand) {
-//		return Mth.nextInt(rand, minExpDropped, maxExpDropped);
-//	}
+
+	@Override
+	public int getExpDrop(BlockState state, LevelReader reader, BlockPos pos, int fortune, int silkTouch) {
+		return silkTouch == 0 ? this.xpRange.sample(RANDOM) : 0;
+	}
 }
