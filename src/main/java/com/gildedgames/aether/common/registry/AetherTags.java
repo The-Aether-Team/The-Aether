f87c368c4a30626d7baab5405b19e4ece2be7327
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-04-15 12:54:21 -0700

    refactor: Begun reimplementing dimension type tags.

--- src/main/java/com/gildedgames/aether/common/registry/AetherTags.java
+++ src/main/java/com/gildedgames/aether/common/registry/AetherTags.java
@@ -13,6 +13,8 @@ import net.minecraft.tags.*;
 import net.minecraft.resources.ResourceLocation;
 import top.theillusivec4.curios.api.CuriosApi;
 
+import java.util.ArrayList;
+
 public class AetherTags {
 	public static class Blocks {
 		public static final TagKey<Block> AETHER_PORTAL_BLOCKS = tag("aether_portal_blocks");
@@ -120,6 +122,22 @@ public class AetherTags {
 
 		private static TagKey<Fluid> tag(String name) {
 			return TagKey.create(Registry.FLUID_REGISTRY, new ResourceLocation(Aether.MODID, name));
+		}
+	}
+
+	public static class Dimensions {
+		// This should be our most general tag for any worlds that are "Aether-like"
+		// All tags above should reference this tag, for any dimensions added to this tag should apply for all logic above
+		public static final TagKey<DimensionType> HOSTILE_PARADISE = tag("hostile_paradise");
+
+		public static final TagKey<DimensionType> ULTRACOLD = tag("ultracold");
+		public static final TagKey<DimensionType> ETERNAL_DAY = tag("eternal_day");
+		public static final TagKey<DimensionType> NO_WHEAT_SEEDS = tag("no_wheat_seeds");
+		public static final TagKey<DimensionType> FALL_TO_OVERWORLD = tag("fall_to_overworld");
+		public static final TagKey<DimensionType> AETHER_MUSIC = tag("aether_music");
+
+		private static TagKey<DimensionType> tag(String name) {
+			return TagKey.create(Registry.DIMENSION_TYPE_REGISTRY, new ResourceLocation(Aether.MODID, name));
 		}
 	}
 }
--- src/main/java/com/gildedgames/aether/core/util/LevelUtil.java
+++ src/main/java/com/gildedgames/aether/core/util/LevelUtil.java
@@ -5,10 +5,6 @@ import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.common.ForgeConfigSpec;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class LevelUtil {
     public static ResourceKey<Level> destinationDimension() { //Default: aether:the_aether
@@ -17,18 +13,5 @@ public class LevelUtil {
 
     public static ResourceKey<Level> returnDimension() { //Default: minecraft:overworld
         return ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(AetherConfig.COMMON.portalReturnDimensionID.get()));
-    }
-
-    public static boolean contains(ForgeConfigSpec.ConfigValue<List<String>> config, ResourceKey<Level> dimension) {
-        return toResourceKeyArray(config).contains(dimension);
-    }
-
-    private static ArrayList<ResourceKey<Level>> toResourceKeyArray(ForgeConfigSpec.ConfigValue<List<String>> locations) {
-        ArrayList<ResourceKey<Level>> resourceKeys = new ArrayList<>();
-        for (String dimension : locations.get()) {
-            ResourceKey<Level> dimensionKey = ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(dimension));
-            resourceKeys.add(dimensionKey);
-        }
-        return resourceKeys;
     }
 }
