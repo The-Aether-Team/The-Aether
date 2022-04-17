52e5cae11599da939d21166ae24ca6f1d920fc27
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-04-17 13:44:04 -0700

    refactor: Removed leftover logging messages. Also removed orElse(null) in DimensionTypeMixin.

--- src/main/java/com/gildedgames/aether/core/mixin/common/DimensionTypeMixin.java
+++ src/main/java/com/gildedgames/aether/core/mixin/common/DimensionTypeMixin.java
@@ -1,6 +1,5 @@
 package com.gildedgames.aether.core.mixin.common;
 
-import com.gildedgames.aether.Aether;
 import com.gildedgames.aether.common.event.hooks.CapabilityHooks;
 import com.gildedgames.aether.common.registry.AetherTags;
 import com.gildedgames.aether.core.capability.time.AetherTime;
@@ -22,12 +21,12 @@ public class DimensionTypeMixin {
     private void timeOfDay(long dayTime, CallbackInfoReturnable<Float> cir) {
         Level level = CapabilityHooks.AetherTimeHooks.world;
         if (level != null && LevelUtil.inTag(level, AetherTags.Dimensions.ETERNAL_DAY)) {
-            Aether.LOGGER.info(1);
-            AetherTime aetherTime = AetherTime.get(level).orElse(null);
-            double time = aetherTime.getDayTime();
-            double d0 = Mth.frac(time / 72000.0D - 0.25D);
-            double d1 = 0.5D - Math.cos(d0 * Math.PI) / 2.0D;
-            cir.setReturnValue((float)(d0 * 2.0D + d1) / 3.0F);
+            AetherTime.get(level).ifPresent(aetherTime -> {
+                double time = aetherTime.getDayTime();
+                double d0 = Mth.frac(time / 72000.0D - 0.25D);
+                double d1 = 0.5D - Math.cos(d0 * Math.PI) / 2.0D;
+                cir.setReturnValue((float)(d0 * 2.0D + d1) / 3.0F);
+            });
         }
     }
 
@@ -35,10 +34,10 @@ public class DimensionTypeMixin {
     private void moonPhase(long dayTime, CallbackInfoReturnable<Integer> cir) {
         Level level = CapabilityHooks.AetherTimeHooks.world;
         if (level != null && LevelUtil.inTag(level, AetherTags.Dimensions.ETERNAL_DAY)) {
-            Aether.LOGGER.info(2);
-            AetherTime aetherTime = AetherTime.get(level).orElse(null);
-            long time = aetherTime.getDayTime();
-            cir.setReturnValue((int) (time / 72000L % 8L + 8L) % 8);
+            AetherTime.get(level).ifPresent(aetherTime -> {
+                long time = aetherTime.getDayTime();
+                cir.setReturnValue((int) (time / 72000L % 8L + 8L) % 8);
+            });
         }
     }
 }
