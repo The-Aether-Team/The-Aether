cf718da2c7359a75263e0e721b4b1216cabe4fdb
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-04-19 17:23:12 -0700

    refactor: More exploit avoidance.

--- src/main/java/com/gildedgames/aether/common/item/accessories/abilities/SlowFallAccessory.java
+++ src/main/java/com/gildedgames/aether/common/item/accessories/abilities/SlowFallAccessory.java
@@ -1,11 +1,15 @@
 package com.gildedgames.aether.common.item.accessories.abilities;
 
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 
 public interface SlowFallAccessory {
     default void handleSlowFall(LivingEntity livingEntity) {
-        if (livingEntity.getDeltaMovement().y() <= 0.0 && !livingEntity.isOnGround() && !livingEntity.isFallFlying() && !livingEntity.isInWater() && !livingEntity.isInLava() && !livingEntity.isShiftKeyDown()) {
-            livingEntity.setDeltaMovement(livingEntity.getDeltaMovement().multiply(1.0, 0.6, 1.0));
+        AttributeInstance gravity = livingEntity.getAttribute(net.minecraftforge.common.ForgeMod.ENTITY_GRAVITY.get());
+        if (gravity != null) {
+            if (livingEntity.getDeltaMovement().y() <= 0.0 && !livingEntity.isOnGround() && !livingEntity.isFallFlying() && !livingEntity.isInWater() && !livingEntity.isInLava() && !livingEntity.isShiftKeyDown() && gravity.getValue() > 0.0075) {
+                livingEntity.setDeltaMovement(livingEntity.getDeltaMovement().multiply(1.0, 0.6, 1.0));
+            }
         }
         livingEntity.resetFallDistance();
     }
