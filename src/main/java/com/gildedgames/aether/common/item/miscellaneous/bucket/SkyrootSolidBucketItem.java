487ceae2b0ba0369b210ccb4823a296d427d4858
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-05-17 15:51:03 -0700

    fix: Okay now Skyroot Powder Snow buckets actually work properly in the creative inventory this time.

--- src/main/java/com/gildedgames/aether/common/item/miscellaneous/bucket/SkyrootSolidBucketItem.java
+++ src/main/java/com/gildedgames/aether/common/item/miscellaneous/bucket/SkyrootSolidBucketItem.java
@@ -1,11 +1,14 @@
 package com.gildedgames.aether.common.item.miscellaneous.bucket;
 
 import com.gildedgames.aether.common.registry.AetherItems;
+import net.minecraft.core.NonNullList;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SolidBucketItem;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
@@ -28,6 +31,12 @@ public class SkyrootSolidBucketItem extends SolidBucketItem {
             player.setItemInHand(interactionhand, AetherItems.SKYROOT_BUCKET.get().getDefaultInstance());
         }
         return interactionresult;
+    }
+
+    public void fillItemCategory(@Nonnull CreativeModeTab category, @Nonnull NonNullList<ItemStack> items) {
+        if (this.allowdedIn(category)) {
+            items.add(new ItemStack(this));
+        }
     }
 
     public void registerBlocks(@Nonnull Map<Block, Item> blockToItemMap, @Nonnull Item item) { }
