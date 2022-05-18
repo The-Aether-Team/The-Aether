f666397e86543c282fabb7be1d7c02805368398a
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-05-18 13:55:23 -0700

    feat: Skyroot Boat

--- src/main/java/com/gildedgames/aether/client/registry/AetherRenderers.java
+++ src/main/java/com/gildedgames/aether/client/registry/AetherRenderers.java
@@ -116,6 +116,7 @@ public class AetherRenderers {
 //        event.registerEntityRenderer(AetherEntityTypes.VALKYRIE_QUEEN.get(), ValkyrieQueenRenderer::new);
 //        event.registerEntityRenderer(AetherEntityTypes.SUN_SPIRIT.get(), SunSpiritRenderer::new);
 
+        event.registerEntityRenderer(AetherEntityTypes.SKYROOT_BOAT.get(), SkyrootBoatRenderer::new);
         event.registerEntityRenderer(AetherEntityTypes.CLOUD_MINION.get(), CloudMinionRenderer::new);
         event.registerEntityRenderer(AetherEntityTypes.COLD_PARACHUTE.get(), (context) -> new ParachuteRenderer(context, AetherBlocks.COLD_AERCLOUD));
         event.registerEntityRenderer(AetherEntityTypes.GOLDEN_PARACHUTE.get(), (context) -> new ParachuteRenderer(context, AetherBlocks.GOLDEN_AERCLOUD));
@@ -171,6 +172,8 @@ public class AetherRenderers {
 //        event.registerLayerDefinition(AetherModelLayers.SLIDER, SliderModel::createBodyLayer);
 //        event.registerLayerDefinition(AetherModelLayers.VALKYRIE_QUEEN, ValkyrieQueenModel:createBodyLayer);
         event.registerLayerDefinition(AetherModelLayers.SUN_SPIRIT, SunSpiritModel::createBodyLayer);
+
+        event.registerLayerDefinition(AetherModelLayers.SKYROOT_BOAT, BoatModel::createBodyModel);
 
         event.registerLayerDefinition(AetherModelLayers.CLOUD_MINION, CloudMinionModel::createBodyLayer);
 
