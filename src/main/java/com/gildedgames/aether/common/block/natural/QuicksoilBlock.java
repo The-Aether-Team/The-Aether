d9ddcb1312a0368942f80f0a3553d64a9b1bee4c
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-05-19 17:38:24 -0700

    fix: Speed cap to Quicksoil.

---
+++ src/main/java/com/gildedgames/aether/common/block/natural/QuicksoilBlock.java
@@ -0,0 +1,28 @@
+package com.gildedgames.aether.common.block.natural;
+
+import com.gildedgames.aether.common.block.util.AetherDoubleDropBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+
+public class QuicksoilBlock extends AetherDoubleDropBlock {
+    public QuicksoilBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public float getFriction(BlockState state, LevelReader level, BlockPos pos, @Nullable Entity entity) {
+        if (entity != null) {
+            if (entity.getLevel().isClientSide()) {
+                Vec3 motion = entity.getDeltaMovement();
+                if (Math.abs(motion.x()) > 1.0 || Math.abs(motion.z()) > 1.0) {
+                    return 0.9F;
+                }
+            }
+        }
+        return this.getFriction();
+    }
+}
