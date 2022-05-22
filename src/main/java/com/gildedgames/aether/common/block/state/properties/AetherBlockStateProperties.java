f73bc2c5546b33e4251d0832c761109814f14622
Kevin Merrill <47490946+Kelvin285@users.noreply.github.com>
2022-05-22 01:54:15 -0400

    emptied repo

--- src/main/java/com/gildedgames/aether/common/block/state/properties/AetherBlockStateProperties.java
+++
@@ -1,8 +0,0 @@
-package com.gildedgames.aether.common.block.state.properties;
-
-import net.minecraft.state.BooleanProperty;
-
-public class AetherBlockStateProperties
-{
-	public static final BooleanProperty DOUBLE_DROPS = BooleanProperty.create("double_drops");
-}
