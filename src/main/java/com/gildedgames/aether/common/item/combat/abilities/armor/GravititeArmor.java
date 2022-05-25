df2575ff09d065100a990d5f368450d587f3d0a1
bconlon <aidan90803@icloud.com>
2022-05-25 13:35:32 -0700

    feat: Gravitite Jump now uses keybind (pending testing and runData).

--- src/main/java/com/gildedgames/aether/common/item/combat/abilities/armor/GravititeArmor.java
+++ src/main/java/com/gildedgames/aether/common/item/combat/abilities/armor/GravititeArmor.java
@@ -1,5 +1,6 @@
 package com.gildedgames.aether.common.item.combat.abilities.armor;
 
+import com.gildedgames.aether.client.registry.AetherKeys;
 import com.gildedgames.aether.core.util.EquipmentUtil;
 import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
 import net.minecraft.server.level.ServerPlayer;
@@ -10,7 +11,7 @@ public interface GravititeArmor {
     static void boostedJump(LivingEntity entity) {
         if (EquipmentUtil.hasFullGravititeSet(entity)) {
             if (entity instanceof Player player) {
-                if (!player.isShiftKeyDown()) {
+                if (AetherKeys.gravititeJumpAbility.isDown()) {
                     player.push(0.0, 1.0, 0.0);
                     if (player instanceof ServerPlayer serverPlayer) {
                         serverPlayer.connection.send(new ClientboundSetEntityMotionPacket(serverPlayer));
