f42940b3f022e36bab0a6eb6f836b2516ff65d10
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-06-08 21:42:50 -0700

    fix: Menu fixes. (#661)
    * fix: Splash persists properly between screens.
    
    * fix: Call stopPlaying() for AetherMusicManager on quick load.
    
    * refactor: World preview is now third person-based.

--- src/main/java/com/gildedgames/aether/client/event/listeners/LevelClientListener.java
+++ src/main/java/com/gildedgames/aether/client/event/listeners/LevelClientListener.java
@@ -3,6 +3,7 @@ package com.gildedgames.aether.client.event.listeners;
 import com.gildedgames.aether.client.event.hooks.LevelClientHooks;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -13,5 +14,13 @@ public class LevelClientListener {
     @SubscribeEvent
     public static void onRenderLevelLast(RenderLevelLastEvent event) {
         LevelClientHooks.renderMenuWithWorld(Minecraft.getInstance());
+    }
+
+    @SubscribeEvent
+    public static void onRenderPlayer(RenderPlayerEvent.Pre event) {
+        LevelClientHooks.adjustShadow(event.getRenderer());
+        if (LevelClientHooks.shouldRenderPlayer()) {
+            event.setCanceled(true);
+        }
     }
 }
