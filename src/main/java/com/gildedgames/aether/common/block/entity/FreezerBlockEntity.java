907b9b76f1b8044c844147840e698a8ba7059c3d
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-06-17 16:39:59 -0700

    fix: Tag fixes.

--- src/main/java/com/gildedgames/aether/common/block/entity/FreezerBlockEntity.java
+++ src/main/java/com/gildedgames/aether/common/block/entity/FreezerBlockEntity.java
@@ -7,15 +7,16 @@ import com.gildedgames.aether.common.inventory.container.FreezerMenu;
 import com.gildedgames.aether.common.registry.AetherBlockEntityTypes;
 import com.google.common.collect.Maps;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.tags.Tag;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
@@ -44,9 +45,9 @@ public class FreezerBlockEntity extends AbstractAetherFurnaceBlockEntity
 		return freezingMap;
 	}
 
-	private static void addItemTagFreezingTime(Tag<Item> itemTag, int burnTime) {
-		for (Item item : itemTag.getValues()) {
-			freezingMap.put(item, burnTime);
+	private static void addItemTagFreezingTime(TagKey<Item> itemTag, int burnTime) {
+		for(Holder<Item> holder : Registry.ITEM.getTagOrEmpty(itemTag)) {
+			freezingMap.put(holder.value(), burnTime);
 		}
 	}
 
