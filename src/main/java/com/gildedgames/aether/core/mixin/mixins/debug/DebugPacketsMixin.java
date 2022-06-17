88dd11eb52ee9c5b274996d0c90ec74ea0114325
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-06-16 19:16:27 -0700

    refactor: More commenting.

--- src/main/java/com/gildedgames/aether/core/mixin/mixins/debug/DebugPacketsMixin.java
+++ src/main/java/com/gildedgames/aether/core/mixin/mixins/debug/DebugPacketsMixin.java
@@ -1,25 +1,25 @@
 package com.gildedgames.aether.core.mixin.mixins.debug;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import io.netty.buffer.Unpooled;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
-import net.minecraft.network.protocol.game.DebugPackets;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.pathfinder.Path;
-import net.minecraft.world.level.pathfinder.Target;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.HashSet;
-
+//import com.mojang.blaze3d.vertex.PoseStack;
+//import io.netty.buffer.Unpooled;
+//import net.minecraft.client.renderer.MultiBufferSource;
+//import net.minecraft.network.FriendlyByteBuf;
+//import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
+//import net.minecraft.network.protocol.game.DebugPackets;
+//import net.minecraft.resources.ResourceLocation;
+//import net.minecraft.server.level.ServerLevel;
+//import net.minecraft.world.entity.Mob;
+//import net.minecraft.world.level.Level;
+//import net.minecraft.world.level.pathfinder.Path;
+//import net.minecraft.world.level.pathfinder.Target;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.Shadow;
+//import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//
+//import java.util.HashSet;
+//
 //@Mixin(DebugPackets.class)
 //public class DebugPacketsMixin {
 //    @Shadow
--- src/main/java/com/gildedgames/aether/core/mixin/mixins/debug/DebugRendererMixin.java
+++ src/main/java/com/gildedgames/aether/core/mixin/mixins/debug/DebugRendererMixin.java
@@ -1,16 +1,16 @@
 package com.gildedgames.aether.core.mixin.mixins.debug;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.debug.DebugRenderer;
-import net.minecraft.client.renderer.debug.PathfindingRenderer;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
+//import com.mojang.blaze3d.vertex.PoseStack;
+//import net.minecraft.client.renderer.MultiBufferSource;
+//import net.minecraft.client.renderer.debug.DebugRenderer;
+//import net.minecraft.client.renderer.debug.PathfindingRenderer;
+//import org.spongepowered.asm.mixin.Final;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.Shadow;
+//import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//
 //@Mixin(DebugRenderer.class)
 //public class DebugRendererMixin {
 //    @Shadow
