13f0acbc21fa278e168aa48eff8c0f181a8153f1
bconlon <67203206+bconlon1@users.noreply.github.com>
2022-09-01 22:17:11 -0700

    more removal

--- src/main/java/com/gildedgames/aether/Aether.java
+++ src/main/java/com/gildedgames/aether/Aether.java
@@ -39,7 +39,6 @@ import com.gildedgames.aether.world.processor.AetherStructureProcessors;
 import com.gildedgames.aether.world.structure.AetherStructureTypes;
 import com.gildedgames.aether.world.structurepiece.AetherStructurePieceTypes;
 import com.gildedgames.aether.world.treedecorator.AetherTreeDecoratorTypes;
-import com.mojang.logging.LogUtils;
 import net.minecraft.SharedConstants;
 import net.minecraft.core.Registry;
 import net.minecraft.core.cauldron.CauldronInteraction;
@@ -74,7 +73,8 @@ import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.resource.PathPackResources;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import top.theillusivec4.curios.api.SlotTypeMessage;
 
 import java.nio.file.Path;
@@ -85,7 +85,7 @@ import java.util.List;
 public class Aether
 {
     public static final String MODID = "aether";
-    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
     public static final Path DIRECTORY = FMLPaths.CONFIGDIR.get().resolve("aether");
 
     public static TriviaGenerator TRIVIA_READER;
