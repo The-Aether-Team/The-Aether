085e1ade59441b1645146828f2750c315112b282
bconlon <bconlongit@gmail.com>
2022-09-19 12:02:33 -0700

    improv: cleanup Vampire Blade

--- src/main/java/com/gildedgames/aether/item/combat/loot/VampireBladeItem.java
+++ src/main/java/com/gildedgames/aether/item/combat/loot/VampireBladeItem.java
@@ -1,29 +1,33 @@
 package com.gildedgames.aether.item.combat.loot;
 
+import com.gildedgames.aether.capability.player.AetherPlayer;
 import com.gildedgames.aether.item.AetherItemGroups;
 import com.gildedgames.aether.item.combat.AetherItemTiers;
 import com.gildedgames.aether.item.AetherItems;
 import net.minecraft.world.entity.LivingEntity;
 
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 
-public class VampireBladeItem extends SwordItem
-{
+import javax.annotation.Nonnull;
+
+public class VampireBladeItem extends SwordItem {
     public VampireBladeItem() {
-        super(AetherItemTiers.VAMPIRE, 3, -2.4f, new Item.Properties().rarity(AetherItems.AETHER_LOOT).tab(AetherItemGroups.AETHER_WEAPONS));
+        super(AetherItemTiers.VAMPIRE, 3, -2.4F, new Item.Properties().rarity(AetherItems.AETHER_LOOT).tab(AetherItemGroups.AETHER_WEAPONS));
     }
 
     @Override
-    public boolean isValidRepairItem(ItemStack toRepair, ItemStack repair) {
-        return false;
-    }
-
-    @Override
-    public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        if (attacker.getHealth() < attacker.getMaxHealth()) {
-            attacker.heal(1.0F);
+    public boolean hurtEnemy(@Nonnull ItemStack stack, @Nonnull LivingEntity target, @Nonnull LivingEntity attacker) {
+        if ((attacker instanceof Player player && player.getAttackStrengthScale(1.0F) == 1.0F) || !(attacker instanceof Player)) {
+            if (attacker.getHealth() < attacker.getMaxHealth()) {
+                if (attacker instanceof Player player) {
+                    AetherPlayer.get(player).ifPresent(aetherPlayer -> aetherPlayer.setVampireHealing(true));
+                } else {
+                    attacker.heal(1.0F);
+                }
+            }
         }
         return super.hurtEnemy(stack, target, attacker);
     }
