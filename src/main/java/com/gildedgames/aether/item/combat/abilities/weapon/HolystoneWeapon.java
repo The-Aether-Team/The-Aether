0696ff3bf99c03f6d17f16fa5f408feeb13ab388
bconlon <bconlongit@gmail.com>
2022-09-18 19:57:06 -0700

    refactor: removed unnecessary !isClientSide() check

--- src/main/java/com/gildedgames/aether/item/combat/abilities/weapon/HolystoneWeapon.java
+++ src/main/java/com/gildedgames/aether/item/combat/abilities/weapon/HolystoneWeapon.java
@@ -7,12 +7,12 @@ import net.minecraft.world.entity.player.Player;
 
 public interface HolystoneWeapon {
     /**
-     * Drops ambrosium at a target's position if it can drop ambrosium, if the call isn't clientside, and if the attacker attacked with full attack strength if they're a player, with a 1/25 chance.
+     * Drops ambrosium at a target's position if it can drop ambrosium, and if the attacker attacked with full attack strength if they're a player, with a 1/25 chance.
      * @param target The hurt entity.
      * @param attacker The attacking entity.
      */
     default void dropAmbrosium(LivingEntity target, LivingEntity attacker) {
-        if (!target.getType().is(AetherTags.Entities.NO_AMBROSIUM_DROPS) && !attacker.getLevel().isClientSide() && target.getLevel().getRandom().nextInt(25) == 0
+        if (!target.getType().is(AetherTags.Entities.NO_AMBROSIUM_DROPS) && target.getLevel().getRandom().nextInt(25) == 0
                 && ((attacker instanceof Player player && player.getAttackStrengthScale(1.0F) == 1.0F) || !(attacker instanceof Player))) {
             target.spawnAtLocation(AetherItems.AMBROSIUM_SHARD.get());
         }
