0563d4cc130c34dae3f92c385c60cfee9f51704e
bconlon <bconlongit@gmail.com>
2022-09-18 20:10:03 -0700

    refactor: Holy Sword cleanup

--- src/main/java/com/gildedgames/aether/item/combat/loot/HolySwordItem.java
+++ src/main/java/com/gildedgames/aether/item/combat/loot/HolySwordItem.java
@@ -3,7 +3,6 @@ package com.gildedgames.aether.item.combat.loot;
 import com.gildedgames.aether.item.AetherItemGroups;
 import com.gildedgames.aether.item.combat.AetherItemTiers;
 import com.gildedgames.aether.item.AetherItems;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.LivingEntity;
@@ -14,26 +13,29 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 
-public class HolySwordItem extends SwordItem
-{
+import javax.annotation.Nonnull;
+
+public class HolySwordItem extends SwordItem {
     public HolySwordItem() {
         super(AetherItemTiers.HOLY, 3, -2.4f, new Item.Properties().rarity(AetherItems.AETHER_LOOT).tab(AetherItemGroups.AETHER_WEAPONS));
     }
 
+    /**
+     * Deals 15 damage to undead mobs or mobs that treat healing and harming effects as inverted, with an extra 2.5 damage for every level of smite the item has. This reduces the item's durability by 10.
+     * @param stack The stack used to hurt the target
+     * @param target The hurt entity.
+     * @param attacker The attacking entity.
+     * @return Whether the enemy was hurt or not.
+     */
     @Override
-    public boolean isValidRepairItem(ItemStack toRepair, ItemStack repair) {
-        return false;
-    }
-
-    @Override
-    public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+    public boolean hurtEnemy(@Nonnull ItemStack stack, LivingEntity target, @Nonnull LivingEntity attacker) {
         if (target.getMobType() == MobType.UNDEAD || target.isInvertedHealAndHarm()) {
             float damageAmount = 15.0F;
-            int smiteModifier = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SMITE, stack);
+            int smiteModifier = stack.getEnchantmentLevel(Enchantments.SMITE);
             if (smiteModifier > 0) {
                 damageAmount += (smiteModifier * 2.5);
             }
-            target.hurt(DamageSource.DROWN, damageAmount);
+            target.hurt(DamageSource.GENERIC, damageAmount);
             stack.hurtAndBreak(10, attacker, (entity) -> entity.broadcastBreakEvent(EquipmentSlot.MAINHAND));
         }
         return super.hurtEnemy(stack, target, attacker);
