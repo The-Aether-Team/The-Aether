90f10faa68e48c958d418f9edfe738ad2d5a9b18
bconlon <aidan90803@icloud.com>
2022-09-29 17:14:05 -0700

    refactor(item): Clean up consumable buckets

--- src/main/java/com/gildedgames/aether/item/miscellaneous/bucket/SkyrootPoisonBucketItem.java
+++ src/main/java/com/gildedgames/aether/item/miscellaneous/bucket/SkyrootPoisonBucketItem.java
@@ -2,6 +2,7 @@ package com.gildedgames.aether.item.miscellaneous.bucket;
 
 import com.gildedgames.aether.item.AetherItems;
 import com.gildedgames.aether.effect.AetherEffects;
+import com.gildedgames.aether.item.miscellaneous.ConsumableItem;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -13,18 +14,24 @@ import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.level.Level;
 
-public class SkyrootPoisonBucketItem extends Item
-{
+public class SkyrootPoisonBucketItem extends Item implements ConsumableItem {
     public SkyrootPoisonBucketItem(Properties properties) {
         super(properties);
     }
 
+    /**
+     * Inflicts Inebriation for 500 ticks and consumes the item using {@link ConsumableItem#consume(Item, ItemStack, LivingEntity)}.
+     * @param stack The {@link ItemStack} in use.
+     * @param level The {@link Level} of the user.
+     * @param user The {@link LivingEntity} using the stack.
+     * @return The {@link ItemStack} after using, which is a Skyroot Bucket if successful.
+     */
     @Override
-    public ItemStack finishUsingItem(ItemStack stack, Level worldIn, LivingEntity entityLiving) {
-        if (!worldIn.isClientSide) entityLiving.addEffect(new MobEffectInstance(AetherEffects.INEBRIATION.get(), 500, 0, false, false));
-        if (entityLiving instanceof Player && !((Player) entityLiving).getAbilities().instabuild) {
-            stack.shrink(1);
+    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity user) {
+        if (!level.isClientSide()) {
+            user.addEffect(new MobEffectInstance(AetherEffects.INEBRIATION.get(), 500, 0, false, false));
         }
+        this.consume(this, stack, user);
         return stack.isEmpty() ? new ItemStack(AetherItems.SKYROOT_BUCKET.get()) : stack;
     }
 
