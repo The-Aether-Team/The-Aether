44a1869dd9f4c6c768243216e3a4bf48be820a54
bconlon <aidan90803@icloud.com>
2022-11-08 16:40:06 -0800

    fix(block): FrictionCapped now runs for both sides

--- src/main/java/com/gildedgames/aether/block/FrictionCapped.java
+++ src/main/java/com/gildedgames/aether/block/FrictionCapped.java
@@ -7,11 +7,9 @@ import org.jetbrains.annotations.Nullable;
 public interface FrictionCapped {
     default float getFriction(@Nullable Entity entity, float defaultFriction) {
         if (entity != null) {
-            if (entity.getLevel().isClientSide()) {
-                Vec3 motion = entity.getDeltaMovement();
-                if (Math.abs(motion.x()) > 1.0 || Math.abs(motion.z()) > 1.0) {
-                    return 0.99F;
-                }
+            Vec3 motion = entity.getDeltaMovement();
+            if (Math.abs(motion.x()) > 1.0 || Math.abs(motion.z()) > 1.0) {
+                return 0.99F;
             }
         }
         return defaultFriction;
