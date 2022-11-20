5cde72cabb26c9d081f8d537d5716073f9a60d27
bconlon <bconlongit@gmail.com>
2022-11-19 21:50:30 -0800

    refactor(block): Clean up FloatingBlock and UnstableObsidianBlock

--- src/main/java/com/gildedgames/aether/block/miscellaneous/FloatingBlock.java
+++ src/main/java/com/gildedgames/aether/block/miscellaneous/FloatingBlock.java
@@ -12,42 +12,55 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 
-import javax.annotation.Nonnull;
-
-public class FloatingBlock extends Block implements Floatable
-{
+/**
+ * Based on {@link net.minecraft.world.level.block.FallingBlock}.
+ */
+public class FloatingBlock extends Block implements Floatable {
 	private final boolean powered;
 	
-	public FloatingBlock(boolean powered, BlockBehaviour.Properties properties) {
+	public FloatingBlock(boolean powered, Properties properties) {
 		super(properties);
 		this.powered = powered;
 	}
 
+	/**
+	 * Warning for "deprecation" is suppressed because the method is fine to override.
+	 */
+	@SuppressWarnings("deprecation")
 	@Override
-	public void onPlace(@Nonnull BlockState state, @Nonnull Level level, @Nonnull BlockPos pos, @Nonnull BlockState oldState, boolean isMoving) {
-		super.onPlace(state, level, pos, oldState, isMoving);
+	public void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean isMoving) {
 		level.scheduleTick(pos, this, this.getDelayAfterPlace());
 	}
 
-	@Nonnull
+	/**
+	 * Warning for "deprecation" is suppressed because the method is fine to override.
+	 */
+	@SuppressWarnings("deprecation")
 	@Override
-	public BlockState updateShape(@Nonnull BlockState state, @Nonnull Direction direction, @Nonnull BlockState facingState, @Nonnull LevelAccessor level, @Nonnull BlockPos pos, @Nonnull BlockPos facingPos) {
+	public BlockState updateShape(BlockState state, Direction direction, BlockState facingState, LevelAccessor level, BlockPos pos, BlockPos facingPos) {
 		level.scheduleTick(pos, this, this.getDelayAfterPlace());
 		return super.updateShape(state, direction, facingState, level, pos, facingPos);
 	}
 
-	public void tick(@Nonnull BlockState state, @Nonnull ServerLevel level, @Nonnull BlockPos pos, @Nonnull RandomSource random) {
+	/**
+	 * Warning for "deprecation" is suppressed because the method is fine to override.
+	 */
+	@SuppressWarnings("deprecation")
+	@Override
+	public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
 		super.tick(state, level, pos, random);
 		if ((this.powered && level.hasNeighborSignal(pos)) || (!this.powered && isFree(level.getBlockState(pos.above())) && pos.getY() <= level.getMaxBuildHeight())) {
 			FloatingBlockEntity floatingBlockEntity = new FloatingBlockEntity(level, (double) pos.getX() + 0.5D, pos.getY(), (double) pos.getZ() + 0.5D, level.getBlockState(pos));
 			level.addFreshEntity(floatingBlockEntity);
+			this.floating(floatingBlockEntity);
 		} else {
 			level.scheduleTick(pos, this, this.getDelayAfterPlace());
 		}
 	}
+
+	protected void floating(FloatingBlockEntity entity) { }
 
 	public static boolean isFree(BlockState state) {
 		Material material = state.getMaterial();
