1824734567676014da97ac6fb4eee5bef1efc195
bconlon <bconlongit@gmail.com>
2022-11-27 13:23:46 -0800

    refactor: Remove commented out code

--- src/main/java/com/gildedgames/aether/world/placementmodifier/DungeonBlacklistFilter.java
+++ src/main/java/com/gildedgames/aether/world/placementmodifier/DungeonBlacklistFilter.java
@@ -22,18 +22,14 @@ public class DungeonBlacklistFilter extends PlacementFilter {
         if (!(context.getLevel() instanceof WorldGenRegion)) {
             return false;
         }
-
-        Registry<Structure> configuredStructureFeatureRegistry = context.getLevel().registryAccess().registryOrThrow(Registry.STRUCTURE_REGISTRY);
         StructureManager structureManager = ((WorldGenRegionAccessor)context.getLevel()).getStructureManager();
-
+        Registry<Structure> configuredStructureFeatureRegistry = context.getLevel().registryAccess().registryOrThrow(Registry.STRUCTURE_REGISTRY);
         for (Holder<Structure> structure : configuredStructureFeatureRegistry.getOrCreateTag(AetherTags.Structures.DUNGEONS)) {
             if (structureManager.getStructureAt(pos, structure.value()).isValid()) {
                 return false;
             }
         }
         return true;
-
-        //return !((WorldGenRegionAccessor) context.getLevel()).getStructureManager().getStructureWithPieceAt(pos, AetherTags.Structures.DUNGEONS).isValid();
     }
 
     @Override
