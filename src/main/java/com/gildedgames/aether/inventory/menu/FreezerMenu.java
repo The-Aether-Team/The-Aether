127f00da66b1a2eef187ccb3ecc2e0744f2e0bdb
bconlon <aidan90803@icloud.com>
2022-12-01 17:56:35 -0800

    refactor(inventory): AetherMenuTypes,  FreezerMenu, and AltarMenu cleanup

--- src/main/java/com/gildedgames/aether/inventory/menu/FreezerMenu.java
+++ src/main/java/com/gildedgames/aether/inventory/menu/FreezerMenu.java
@@ -6,23 +6,20 @@ import com.gildedgames.aether.recipe.AetherRecipeTypes;
 
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.Container;
-import net.minecraft.world.inventory.AbstractFurnaceMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.inventory.ContainerData;
-
-import javax.annotation.Nonnull;
 
 public class FreezerMenu extends AbstractAetherFurnaceMenu {
 	public FreezerMenu(int windowId, Inventory playerInventory) {
 		super(AetherMenuTypes.FREEZER.get(), AetherRecipeTypes.FREEZING.get(), AetherRecipeBookTypes.FREEZER, windowId, playerInventory);
 	}
 
-	public FreezerMenu(int windowId, Inventory playerInventory, Container freezingInventory, ContainerData furnaceData) {
-		super(AetherMenuTypes.FREEZER.get(), AetherRecipeTypes.FREEZING.get(), AetherRecipeBookTypes.FREEZER, windowId, playerInventory, freezingInventory, furnaceData);
+	public FreezerMenu(int windowId, Inventory playerInventory, Container freezerContainer, ContainerData data) {
+		super(AetherMenuTypes.FREEZER.get(), AetherRecipeTypes.FREEZING.get(), AetherRecipeBookTypes.FREEZER, windowId, playerInventory, freezerContainer, data);
 	}
 
 	@Override
-	public boolean isFuel(@Nonnull ItemStack stack) {
+	public boolean isFuel(ItemStack stack) {
 		return FreezerBlockEntity.getFreezingMap().containsKey(stack.getItem());
 	}
 }
