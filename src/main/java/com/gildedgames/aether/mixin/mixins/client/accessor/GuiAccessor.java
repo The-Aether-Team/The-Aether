8c015b0e7bd4d1c089cb7b1fc9786171e0425451
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2022-12-02 16:51:29 -0700

    fix: fixed silver heart rendering in certain scenarios

--- src/main/java/com/gildedgames/aether/mixin/mixins/client/accessor/GuiAccessor.java
+++ src/main/java/com/gildedgames/aether/mixin/mixins/client/accessor/GuiAccessor.java
@@ -2,12 +2,16 @@ package com.gildedgames.aether.mixin.mixins.client.accessor;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.util.RandomSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(Gui.class)
 public interface GuiAccessor {
+    @Accessor
+    RandomSource getRandom();
+
     @Accessor
     long getLastHealthTime();
 
