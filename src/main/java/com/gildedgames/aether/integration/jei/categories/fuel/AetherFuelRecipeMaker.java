9cb63ff48c5005a012944b81d1d92d6a7b101dbe
bconlon <67203206+bconlon1@users.noreply.github.com>
2023-01-17 13:05:07 -0800

    feat: 1.19.3 JEI integration (#909)
    Uncomment the JEI integration now that it's released on 1.19.3
    
    reviewed by Burning-Cactus
    
    merged from #909

--- src/main/java/com/gildedgames/aether/integration/jei/categories/fuel/AetherFuelRecipeMaker.java
+++ src/main/java/com/gildedgames/aether/integration/jei/categories/fuel/AetherFuelRecipeMaker.java
@@ -1,29 +1,29 @@
-//package com.gildedgames.aether.integration.jei.categories.fuel;
-//
-//import com.gildedgames.aether.block.AetherBlocks;
-//import com.gildedgames.aether.blockentity.AltarBlockEntity;
-//import com.gildedgames.aether.blockentity.FreezerBlockEntity;
-//import com.gildedgames.aether.blockentity.IncubatorBlockEntity;
-//import net.minecraft.world.item.ItemStack;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public final class AetherFuelRecipeMaker {
-//
-//    private AetherFuelRecipeMaker() {
-//    }
-//
-//    public static List<AetherFuelRecipe> getFuelRecipes() {
-//        List<AetherFuelRecipe> fuelRecipes = new ArrayList<>();
-//
-//        AltarBlockEntity.getEnchantingMap().forEach((item, burnTime) ->
-//                fuelRecipes.add(new AetherFuelRecipe(List.of(new ItemStack(item)), burnTime, AetherBlocks.ALTAR.get())));
-//        FreezerBlockEntity.getFreezingMap().forEach((item, burnTime) ->
-//                fuelRecipes.add(new AetherFuelRecipe(List.of(new ItemStack(item)), burnTime, AetherBlocks.FREEZER.get())));
-//        IncubatorBlockEntity.getIncubatingMap().forEach((item, burnTime) ->
-//                fuelRecipes.add(new AetherFuelRecipe(List.of(new ItemStack(item)), burnTime, AetherBlocks.INCUBATOR.get())));
-//
-//        return fuelRecipes;
-//    }
-//}
+package com.gildedgames.aether.integration.jei.categories.fuel;
+
+import com.gildedgames.aether.block.AetherBlocks;
+import com.gildedgames.aether.blockentity.AltarBlockEntity;
+import com.gildedgames.aether.blockentity.FreezerBlockEntity;
+import com.gildedgames.aether.blockentity.IncubatorBlockEntity;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class AetherFuelRecipeMaker {
+
+    private AetherFuelRecipeMaker() {
+    }
+
+    public static List<AetherFuelRecipe> getFuelRecipes() {
+        List<AetherFuelRecipe> fuelRecipes = new ArrayList<>();
+
+        AltarBlockEntity.getEnchantingMap().forEach((item, burnTime) ->
+                fuelRecipes.add(new AetherFuelRecipe(List.of(new ItemStack(item)), burnTime, AetherBlocks.ALTAR.get())));
+        FreezerBlockEntity.getFreezingMap().forEach((item, burnTime) ->
+                fuelRecipes.add(new AetherFuelRecipe(List.of(new ItemStack(item)), burnTime, AetherBlocks.FREEZER.get())));
+        IncubatorBlockEntity.getIncubatingMap().forEach((item, burnTime) ->
+                fuelRecipes.add(new AetherFuelRecipe(List.of(new ItemStack(item)), burnTime, AetherBlocks.INCUBATOR.get())));
+
+        return fuelRecipes;
+    }
+}
