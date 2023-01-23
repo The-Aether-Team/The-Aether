fd803bfcdcc4f98cfeee6d5420d938b36ba1d474
FlashFyre <33395370+FlashFyreMods@users.noreply.github.com>
2023-01-23 19:32:13 +0000

    feat: World gen improvements (#913)
    - Adds density function and noise registry
    - Adjusts the terrain gen to make it similar to the original terrain
    - Moves silver and gold dungeons into a shared structure set
    - Fixes and adds to the biome map for better biome distribution
    
    reviewed by bconlon1
    
    merged from #913

--- src/main/java/com/gildedgames/aether/data/resources/registries/AetherStructureSets.java
+++ src/main/java/com/gildedgames/aether/data/resources/registries/AetherStructureSets.java
@@ -1,6 +1,9 @@
 package com.gildedgames.aether.data.resources.registries;
 
+import java.util.List;
+
 import com.gildedgames.aether.Aether;
+
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
@@ -13,9 +16,8 @@ import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadType;
 
 public class AetherStructureSets {
     public static final ResourceKey<StructureSet> LARGE_AERCLOUD = createKey("large_aercloud");
-    public static final ResourceKey<StructureSet> BRONZE_DUNGEON = createKey("bronze_dungeon");
-    public static final ResourceKey<StructureSet> SILVER_DUNGEON = createKey("silver_dungeon");
-    public static final ResourceKey<StructureSet> GOLD_DUNGEON = createKey("gold_dungeon");
+    public static final ResourceKey<StructureSet> BRONZE_DUNGEON = createKey("bronze_dungeon");    
+    public static final ResourceKey<StructureSet> SILVER_AND_GOLD_DUNGEONS = createKey("silver_and_gold_dungeons");
 
     private static ResourceKey<StructureSet> createKey(String name) {
         return ResourceKey.create(Registries.STRUCTURE_SET, new ResourceLocation(Aether.MODID, name));
@@ -24,8 +26,10 @@ public class AetherStructureSets {
     public static void bootstrap(BootstapContext<StructureSet> context) {
         HolderGetter<Structure> structures = context.lookup(Registries.STRUCTURE);
         context.register(LARGE_AERCLOUD, new StructureSet(structures.getOrThrow(AetherStructures.LARGE_AERCLOUD), new RandomSpreadStructurePlacement(4, 2, RandomSpreadType.LINEAR, 15536586)));
-        context.register(BRONZE_DUNGEON, new StructureSet(structures.getOrThrow(AetherStructures.BRONZE_DUNGEON), new RandomSpreadStructurePlacement(12, 10, RandomSpreadType.LINEAR, 32146754)));
-        context.register(SILVER_DUNGEON, new StructureSet(structures.getOrThrow(AetherStructures.SILVER_DUNGEON), new RandomSpreadStructurePlacement(16, 6, RandomSpreadType.LINEAR, 4325806)));
-        context.register(GOLD_DUNGEON, new StructureSet(structures.getOrThrow(AetherStructures.GOLD_DUNGEON), new RandomSpreadStructurePlacement(20, 6, RandomSpreadType.LINEAR, 15436785)));
+        context.register(BRONZE_DUNGEON, new StructureSet(structures.getOrThrow(AetherStructures.BRONZE_DUNGEON), new RandomSpreadStructurePlacement(12, 10, RandomSpreadType.LINEAR, 32146754)));        
+        context.register(SILVER_AND_GOLD_DUNGEONS, new StructureSet(List.of(
+        		StructureSet.entry(structures.getOrThrow(AetherStructures.SILVER_DUNGEON), 3), 
+        		StructureSet.entry(structures.getOrThrow(AetherStructures.GOLD_DUNGEON), 2)), 
+        	new RandomSpreadStructurePlacement(10, 6, RandomSpreadType.LINEAR, 4325806)));
     }
 }
