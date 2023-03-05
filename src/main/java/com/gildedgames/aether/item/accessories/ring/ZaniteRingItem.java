6c82d8ce49c340c0f2d36a375abc28236640d45b
bconlon <bconlongit@gmail.com>
2023-03-04 19:06:13 -0800

    refactor: Change how zanite accessory durability works

--- src/main/java/com/gildedgames/aether/item/accessories/ring/ZaniteRingItem.java
+++ src/main/java/com/gildedgames/aether/item/accessories/ring/ZaniteRingItem.java
@@ -4,9 +4,7 @@ import com.gildedgames.aether.AetherTags;
 import com.gildedgames.aether.client.AetherSoundEvents;
 import com.gildedgames.aether.item.accessories.abilities.ZaniteAccessory;
 import net.minecraft.world.item.ItemStack;
-
 import net.minecraftforge.event.entity.player.PlayerEvent;
-import top.theillusivec4.curios.api.SlotContext;
 
 /**
  * Zanite ring mining speed boost behavior is called by {@link com.gildedgames.aether.event.listeners.abilities.AccessoryAbilityListener#onMiningSpeed(PlayerEvent.BreakSpeed)}
@@ -19,10 +17,5 @@ public class ZaniteRingItem extends RingItem implements ZaniteAccessory {
     @Override
     public boolean isValidRepairItem(ItemStack repairItem, ItemStack repairMaterial) {
         return repairMaterial.is(AetherTags.Items.ZANITE_REPAIRING);
-    }
-
-    @Override
-    public void curioTick(SlotContext slotContext, ItemStack stack) {
-        this.damageZaniteAccessory(slotContext, stack);
     }
 }
