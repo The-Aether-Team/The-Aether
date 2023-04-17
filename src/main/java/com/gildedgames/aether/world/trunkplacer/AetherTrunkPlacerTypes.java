ad49fbf2bb5a064782a4db5c82c8cb4474d258ae
bconlon <bconlon.gml@gmail.com>
2023-04-16 20:40:11 -0700

    improv: Crystal Tree Trunk Placer

--- src/main/java/com/gildedgames/aether/world/trunkplacer/AetherTrunkPlacerTypes.java
+++ src/main/java/com/gildedgames/aether/world/trunkplacer/AetherTrunkPlacerTypes.java
@@ -10,5 +10,6 @@ public class AetherTrunkPlacerTypes {
     public static final DeferredRegister<TrunkPlacerType<?>> TRUNK_PLACERS = DeferredRegister.create(Registries.TRUNK_PLACER_TYPE, Aether.MODID);
 
     public static final RegistryObject<TrunkPlacerType<GoldenOakTrunkPlacer>> GOLDEN_OAK_TRUNK_PLACER = TRUNK_PLACERS.register("golden_oak_trunk_placer", () -> new TrunkPlacerType<>(GoldenOakTrunkPlacer.CODEC));
+    public static final RegistryObject<TrunkPlacerType<CrystalTreeTrunkPlacer>> CRYSTAL_TREE_TRUNK_PLACER = TRUNK_PLACERS.register("crystal_tree_trunk_placer", () -> new TrunkPlacerType<>(CrystalTreeTrunkPlacer.CODEC));
 
 }
