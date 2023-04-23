b9a15345c0f19e919c92d4415757ee6e2e6a0918
bconlon <67203206+bconlon1@users.noreply.github.com>
2023-04-23 08:07:36 -0700

    refactor: Tweak movement speed and upwards movement for armor abilities (#1176)
    closes #1172

--- src/main/java/com/aetherteam/aether/item/combat/abilities/armor/NeptuneArmor.java
+++ src/main/java/com/aetherteam/aether/item/combat/abilities/armor/NeptuneArmor.java
@@ -3,6 +3,7 @@ package com.aetherteam.aether.item.combat.abilities.armor;
 import com.aetherteam.aether.capability.player.AetherPlayer;
 import com.aetherteam.aether.util.EquipmentUtil;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.phys.Vec3;
@@ -10,7 +11,7 @@ import net.minecraftforge.event.entity.living.LivingEvent;
 
 public interface NeptuneArmor {
     /**
-     * Boosts the entity's movement in water or bubble columns if wearing a full set of Neptune Armor. The default boost is a multiplier of 1.55, but is modified based on duration in water and whether the boots have Depth Strider.
+     * Boosts the entity's movement in water or bubble columns if wearing a full set of Neptune Armor. The default boost is modified based on duration in water and whether the boots have Depth Strider.
      * @param entity The {@link LivingEntity} wearing the armor.
      * @see com.aetherteam.aether.event.listeners.abilities.ArmorAbilityListener#onEntityUpdate(LivingEvent.LivingTickEvent)
      */
@@ -23,10 +24,16 @@ public interface NeptuneArmor {
                         aetherPlayer.setNeptuneSubmergeLength(Math.min(aetherPlayer.getNeptuneSubmergeLength() + 0.1, 1.0));
                         defaultBoost *= aetherPlayer.getNeptuneSubmergeLength();
                         entity.moveRelative(0.04F * defaultBoost, new Vec3(entity.xxa, entity.yya, entity.zza));
+                        if (entity.getDeltaMovement().y() > 0 || entity.isCrouching()) {
+                            entity.move(MoverType.SELF, entity.getDeltaMovement().multiply(0.0, defaultBoost * 1.5, 0.0));
+                        }
                     });
                 } else {
                     float defaultBoost = boostWithDepthStrider(entity);
                     entity.moveRelative(0.04F * defaultBoost, new Vec3(entity.xxa, entity.yya, entity.zza));
+                    if (entity.getDeltaMovement().y() > 0 || entity.isCrouching()) {
+                        entity.move(MoverType.SELF, entity.getDeltaMovement().multiply(0.0, defaultBoost * 1.5, 0.0));
+                    }
                 }
             }
         }
@@ -38,15 +45,15 @@ public interface NeptuneArmor {
     }
 
     /**
-     * Adds an extra 1.05 to the boost for every Depth Strider level up to Depth Strider 3.
+     * Adds an extra 0.15 to the boost for every Depth Strider level up to Depth Strider 3.
      * @param entity The {@link LivingEntity} wearing the armor.
      * @return The modified boost as a {@link Float}.
      */
     private static float boostWithDepthStrider(LivingEntity entity) {
-        float defaultBoost = 1.05F;
+        float defaultBoost = 0.4F;
         float depthStriderModifier = Math.min(EnchantmentHelper.getDepthStrider(entity), 3.0F);
         if (depthStriderModifier > 0.0F) {
-            defaultBoost += depthStriderModifier * 1.05F;
+            defaultBoost += depthStriderModifier * 0.4F;
         }
         return defaultBoost;
     }
