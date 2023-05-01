9d9b40bef2093454f455591a4542875d565b447d
bconlon <bconlon.gml@gmail.com>
2023-04-30 21:33:40 -0700

    fix: Better ground checks for Valkyrie Armor

--- src/main/java/com/aetherteam/aether/item/combat/abilities/armor/ValkyrieArmor.java
+++ src/main/java/com/aetherteam/aether/item/combat/abilities/armor/ValkyrieArmor.java
@@ -2,6 +2,7 @@ package com.aetherteam.aether.item.combat.abilities.armor;
 
 import com.aetherteam.aether.capability.player.AetherPlayer;
 import com.aetherteam.aether.mixin.mixins.common.accessor.ServerGamePacketListenerImplAccessor;
+import com.aetherteam.aether.util.EntityUtil;
 import com.aetherteam.aether.util.EquipmentUtil;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
@@ -21,7 +22,7 @@ public interface ValkyrieArmor {
                 AetherPlayer.get(player).ifPresent(aetherPlayer -> {
                     Vec3 deltaMovement = player.getDeltaMovement();
                     if (player.getLevel().isClientSide()) { // Updates the flight modifier and timer values.
-                        if (aetherPlayer.isJumping() && !player.isOnGround()) { // Checks if the player is off the ground and holding the jump key (space bar by default).
+                        if (aetherPlayer.isJumping() && !EntityUtil.betterGroundCheck(player)) { // Checks if the player is off the ground and holding the jump key (space bar by default).
                             if (aetherPlayer.getFlightModifier() >= aetherPlayer.getFlightModifierMax()) { // Limits the flight modifier to a maximum value.
                                 aetherPlayer.setFlightModifier(aetherPlayer.getFlightModifierMax());
                             }
@@ -37,13 +38,13 @@ public interface ValkyrieArmor {
                             // Resets only the modifier if the player stops holding the jump key midair. The timer doesn't reset though and remains frozen, and will continue where it left off when the key is held again, preventing infinite flight.
                             aetherPlayer.setFlightModifier(1.0F);
                         }
-                        if (player.isOnGround()) { // Resets both timer and modifier if the player is on the ground.
+                        if (EntityUtil.betterGroundCheck(player)) { // Resets both timer and modifier if the player is on the ground.
                             aetherPlayer.setFlightTimer(0);
                             aetherPlayer.setFlightModifier(1.0F);
                         }
                     }
                     // Modifies the player's upwards movement based on the set flight modifier and timer values.
-                    if (aetherPlayer.isJumping() && !player.isOnGround() && aetherPlayer.getFlightTimer() > 2 && aetherPlayer.getFlightTimer() < aetherPlayer.getFlightTimerMax() && aetherPlayer.getFlightModifier() > 1.0F) {
+                    if (aetherPlayer.isJumping() && !EntityUtil.betterGroundCheck(player) && aetherPlayer.getFlightTimer() > 2 && aetherPlayer.getFlightTimer() < aetherPlayer.getFlightTimerMax() && aetherPlayer.getFlightModifier() > 1.0F) {
                         player.setDeltaMovement(deltaMovement.x(), 0.025F * aetherPlayer.getFlightModifier(), deltaMovement.z());
                     }
                     if (player instanceof ServerPlayer serverPlayer) { // Prevents the player from being kicked for flying.
