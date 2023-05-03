6baa9d1973d8e04251b0b9f0b160cb52a958572a
Oscar Payn <oscar@gildedgames.com>
2023-05-04 00:03:49 +0100

    feat: spawn costs for aechor plants
    stupid plants

--- src/main/java/com/aetherteam/aether/data/resources/AetherMobCategory.java
+++ src/main/java/com/aetherteam/aether/data/resources/AetherMobCategory.java
@@ -4,6 +4,6 @@ import net.minecraft.world.entity.MobCategory;
 
 public class AetherMobCategory {
     public static final MobCategory AETHER_SURFACE_MONSTER = MobCategory.create("AETHER_SURFACE_MONSTER", "aether_surface_monster", 15, false, false, 128);
-    public static final MobCategory AETHER_SKY_MONSTER = MobCategory.create("AETHER_SKY_MONSTER", "aether_sky_monster", 2, false, false, 128);
-    public static final MobCategory AETHER_AERWHALE = MobCategory.create("AETHER_AERWHALE", "aether_aerwhale", 2, true, false, 128);
+    public static final MobCategory AETHER_SKY_MONSTER = MobCategory.create("AETHER_SKY_MONSTER", "aether_sky_monster", 1, false, false, 128);
+    public static final MobCategory AETHER_AERWHALE = MobCategory.create("AETHER_AERWHALE", "aether_aerwhale", 1, true, false, 128);
 }
