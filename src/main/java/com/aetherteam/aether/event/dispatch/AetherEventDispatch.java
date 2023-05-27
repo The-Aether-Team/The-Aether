3ed549d539fe89f39fe6a367a1788fc397328fd9
bconlon <67203206+bconlon1@users.noreply.github.com>
2023-05-27 15:06:54 -0700

    feat: Moa Egg event (#1443)
    closes #1440

--- src/main/java/com/aetherteam/aether/event/dispatch/AetherEventDispatch.java
+++ src/main/java/com/aetherteam/aether/event/dispatch/AetherEventDispatch.java
@@ -4,7 +4,10 @@ import javax.annotation.Nullable;
 
 import com.aetherteam.aether.event.events.*;
 
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.entity.player.Player;
@@ -15,6 +18,12 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraftforge.common.MinecraftForge;
 
 public class AetherEventDispatch {
+	public static EggLayEvent onLayEgg(Entity entity, SoundEvent sound, float volume, float pitch, Item item) {
+		EggLayEvent event = new EggLayEvent(entity, sound, volume, pitch, item);
+		MinecraftForge.EVENT_BUS.post(event);
+		return event;
+	}
+
 	public static PlacementBanEvent.SpawnParticles onPlacementSpawnParticles(LevelAccessor world, BlockPos pos, @Nullable Direction face, @Nullable ItemStack stack, @Nullable BlockState state) {
 		PlacementBanEvent.SpawnParticles event = new PlacementBanEvent.SpawnParticles(world, pos, face, stack, state);
 		MinecraftForge.EVENT_BUS.post(event);
