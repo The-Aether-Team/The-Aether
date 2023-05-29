8522dc86a15d0330cb51879bdade116db5bba773
bconlon <bconlon.gml@gmail.com>
2023-05-28 18:39:45 -0700

    fix: Remove extra durability damage call in Slider hurt method

--- src/main/java/com/aetherteam/aether/entity/monster/dungeon/boss/slider/Slider.java
+++ src/main/java/com/aetherteam/aether/entity/monster/dungeon/boss/slider/Slider.java
@@ -49,7 +49,6 @@ import net.minecraftforge.network.NetworkHooks;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Scanner;
 
 public class Slider extends PathfinderMob implements BossMob<Slider>, Enemy, IEntityAdditionalSpawnData {
     public static final EntityDataAccessor<Boolean> DATA_AWAKE_ID = SynchedEntityData.defineId(Slider.class, EntityDataSerializers.BOOLEAN);
@@ -166,8 +165,6 @@ public class Slider extends PathfinderMob implements BossMob<Slider>, Enemy, IEn
                             }
                         }
                         this.setHurtAngle(0.7F - (this.getHealth() / 875.0F));
-
-                        attacker.getMainHandItem().hurtAndBreak(1, attacker, (entity) -> entity.broadcastBreakEvent(EquipmentSlot.MAINHAND));
 
                         SliderAi.wasHurtBy(this, attacker, amount);
 
