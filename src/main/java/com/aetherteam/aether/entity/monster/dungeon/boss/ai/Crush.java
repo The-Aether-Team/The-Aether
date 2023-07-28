37c5e24322d9d9446ab6777bb423005f377f1268
bconlon <bconlon.gml@gmail.com>
2023-07-28 16:38:26 -0700

    fix: Break any blocks between Slider and target, such as trapdoors

--- src/main/java/com/aetherteam/aether/entity/monster/dungeon/boss/ai/Crush.java
+++ src/main/java/com/aetherteam/aether/entity/monster/dungeon/boss/ai/Crush.java
@@ -10,11 +10,17 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.util.Unit;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.behavior.Behavior;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.ForgeEventFactory;
+
+import java.util.Optional;
 
 public class Crush extends Behavior<Slider> {
     public Crush() {
@@ -23,7 +29,7 @@ public class Crush extends Behavior<Slider> {
 
     @Override
     protected boolean checkExtraStartConditions(ServerLevel level, Slider slider) {
-        return slider.isAwake() && !slider.isDeadOrDying() && (slider.horizontalCollision || slider.verticalCollision);
+        return slider.isAwake() && !slider.isDeadOrDying() && (slider.horizontalCollision || slider.verticalCollision || this.blocksBetween(slider));
     }
 
     @Override
@@ -31,14 +37,12 @@ public class Crush extends Behavior<Slider> {
         boolean crushed = false;
         if (ForgeEventFactory.getMobGriefingEvent(level, slider)) {
             AABB crushBox = slider.getBoundingBox().inflate(0.2);
-            for(BlockPos pos : BlockPos.betweenClosed(Mth.floor(crushBox.minX), Mth.floor(crushBox.minY), Mth.floor(crushBox.minZ), Mth.floor(crushBox.maxX), Mth.floor(crushBox.maxY), Mth.floor(crushBox.maxZ))) {
+            for (BlockPos pos : BlockPos.betweenClosed(Mth.floor(crushBox.minX), Mth.floor(crushBox.minY), Mth.floor(crushBox.minZ), Mth.floor(crushBox.maxX), Mth.floor(crushBox.maxY), Mth.floor(crushBox.maxZ))) {
                 if (slider.getDungeon() == null || slider.getDungeon().roomBounds().contains(pos.getCenter())) {
                     BlockState blockState = slider.level.getBlockState(pos);
-                    if (!blockState.isAir()) {
-                        if (!blockState.is(AetherTags.Blocks.SLIDER_UNBREAKABLE)) {
-                            crushed = slider.level.destroyBlock(pos, true, slider) || crushed;
-                            slider.blockDestroySmoke(pos);
-                        }
+                    if (this.isBreakable(blockState)) {
+                        crushed = slider.level.destroyBlock(pos, true, slider) || crushed;
+                        slider.blockDestroySmoke(pos);
                     }
                 }
             }
@@ -49,5 +53,15 @@ public class Crush extends Behavior<Slider> {
             slider.getBrain().setMemoryWithExpiry(AetherMemoryModuleTypes.MOVE_DELAY.get(), Unit.INSTANCE, slider.calculateMoveDelay());
             slider.setDeltaMovement(Vec3.ZERO);
         }
+    }
+
+    private boolean blocksBetween(Slider slider) {
+        Brain<?> brain = slider.getBrain();
+        Optional<LivingEntity> attackTarget = brain.getMemory(MemoryModuleType.ATTACK_TARGET);
+        return attackTarget.filter(livingEntity -> slider.getLevel().getBlockStates(AABB.of(BoundingBox.fromCorners(livingEntity.blockPosition(), slider.blockPosition()))).anyMatch(this::isBreakable)).isPresent();
+    }
+
+    private boolean isBreakable(BlockState blockState) {
+        return !blockState.isAir() && !blockState.is(AetherTags.Blocks.SLIDER_UNBREAKABLE);
     }
 }
