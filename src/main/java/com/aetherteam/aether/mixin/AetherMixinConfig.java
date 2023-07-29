2be32fc290a45a9c677e01751b542942d4b33b05
Jonathan Colmenares <me@jonathing.me>
2023-07-28 22:03:41 -0500

    improv: Hard-lock mixin config loading to pre-defined entries

--- src/main/java/com/aetherteam/aether/mixin/AetherMixinConfig.java
+++ src/main/java/com/aetherteam/aether/mixin/AetherMixinConfig.java
@@ -6,6 +6,8 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.minecraftforge.fml.loading.FMLPaths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileReader;
@@ -15,11 +17,13 @@ import java.nio.file.Path;
 import java.util.Map;
 
 public class AetherMixinConfig {
-    private static final String MIXIN_PACKAGE_ROOT = "com.aetherteam.aether.core.mixin.";
+    private static final Logger LOGGER = LoggerFactory.getLogger(AetherMixinConfig.class); // avoid calling LogUtils to avoid loading mojang/mc class early
+
+    private static final String MIXIN_PACKAGE_ROOT = "com.aetherteam.aether.mixin.mixins.";
     private static final Path DIRECTORY = FMLPaths.CONFIGDIR.get().resolve("aether");
     private static final File MIXIN_CONFIG = new File(DIRECTORY.toString(), "mixin_config.json");
 
-    private static final ImmutableList<Entry> configEntries = ImmutableList.of(
+    private static final ImmutableList<Entry> CONFIG_ENTRIES = ImmutableList.of(
             new Entry(MIXIN_PACKAGE_ROOT + "client.AdvancementToastMixin", true),
             new Entry(MIXIN_PACKAGE_ROOT + "client.TippableArrowRendererMixin", true)
     );
@@ -33,7 +37,7 @@ public class AetherMixinConfig {
                 Gson gson = new GsonBuilder().setPrettyPrinting().create();
                 JsonObject jsonObject = new JsonObject();
                 jsonObject.addProperty("comment", "Disabling these is not advised unless necessary");
-                for (Entry entry : configEntries) {
+                for (Entry entry : CONFIG_ENTRIES) {
                     jsonObject.addProperty(entry.path(), entry.defaultValue());
                 }
                 gson.toJson(jsonObject, fileWriter);
@@ -50,11 +54,17 @@ public class AetherMixinConfig {
                 JsonObject jsonObject = gson.fromJson(fileReader, JsonObject.class);
                 for (Map.Entry<String, JsonElement> property : jsonObject.entrySet()) {
                     if (property.getKey().equals(mixinClassName)) {
-                        return new Entry(property.getKey(), property.getValue().getAsBoolean());
+                        var entry = new Entry(property.getKey(), property.getValue().getAsBoolean());
+                        for (Entry configEntry : CONFIG_ENTRIES) {
+                            if (configEntry.path.equals(entry.path)) {
+                                return entry;
+                            }
+                        }
+                        LOGGER.error("Attempted to alter application of mixin {} from config, when it is not part of the config!", entry.path);
                     }
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                LOGGER.error("Failed to read mixin config!", e);
             }
         }
         return null;
