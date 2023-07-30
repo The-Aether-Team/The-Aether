f08893d0ccbbfb3448186bf8ece1d5d491a35e71
bconlon <bconlon.gml@gmail.com>
2023-07-29 22:18:43 -0700

    refactor: Clean up `world.foliageplacer`

--- src/main/java/com/aetherteam/aether/world/foliageplacer/AetherFoliagePlacerTypes.java
+++ src/main/java/com/aetherteam/aether/world/foliageplacer/AetherFoliagePlacerTypes.java
@@ -11,6 +11,5 @@ public class AetherFoliagePlacerTypes {
 
     public static final RegistryObject<FoliagePlacerType<CrystalFoliagePlacer>> CRYSTAL_FOLIAGE_PLACER = FOLIAGE_PLACERS.register("crystal_foliage_placer", () -> new FoliagePlacerType<>(CrystalFoliagePlacer.CODEC));
     public static final RegistryObject<FoliagePlacerType<HolidayFoliagePlacer>> HOLIDAY_FOLIAGE_PLACER = FOLIAGE_PLACERS.register("holiday_foliage_placer", () -> new FoliagePlacerType<>(HolidayFoliagePlacer.CODEC));
-
     public static final RegistryObject<FoliagePlacerType<GoldenOakFoliagePlacer>> GOLDEN_OAK_FOLIAGE_PLACER = FOLIAGE_PLACERS.register("golden_oak_foliage_placer", () -> new FoliagePlacerType<>(GoldenOakFoliagePlacer.CODEC));
 }
