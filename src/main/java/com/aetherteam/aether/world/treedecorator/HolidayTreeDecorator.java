f19502883ec2d50c54526f6b0734c8e6ba67e061
bconlon <bconlon.gml@gmail.com>
2023-07-29 20:26:07 -0700

    refactor: Clean up `world.treedecorator`

--- src/main/java/com/aetherteam/aether/world/treedecorator/HolidayTreeDecorator.java
+++ src/main/java/com/aetherteam/aether/world/treedecorator/HolidayTreeDecorator.java
@@ -4,6 +4,7 @@ import com.aetherteam.aether.AetherTags;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelSimulatedReader;
 import net.minecraft.world.level.block.Blocks;
@@ -15,10 +16,11 @@ import net.minecraft.world.level.levelgen.feature.treedecorators.TreeDecoratorTy
 
 import java.util.List;
 
+/**
+ * Decorates blocks around Holiday Trees.
+ */
 public class HolidayTreeDecorator extends TreeDecorator {
-    public static final Codec<HolidayTreeDecorator> CODEC = BlockStateProvider.CODEC.fieldOf("provider").xmap(HolidayTreeDecorator::new, (instance) -> {
-        return instance.provider;
-    }).codec();
+    public static final Codec<HolidayTreeDecorator> CODEC = BlockStateProvider.CODEC.fieldOf("provider").xmap(HolidayTreeDecorator::new, (instance) -> instance.provider).codec();
     private final BlockStateProvider provider;
 
     public HolidayTreeDecorator(BlockStateProvider provider) {
@@ -34,49 +36,64 @@ public class HolidayTreeDecorator extends TreeDecorator {
         List<BlockPos> logPositions = context.logs();
         if (!logPositions.isEmpty()) {
             int i = logPositions.get(0).getY();
-            logPositions.stream().filter((logs) -> logs.getY() == i).forEach((logPos) -> {
-                this.placeCircle(context, logPos);
-            });
+            logPositions.stream().filter((logs) -> logs.getY() == i).forEach((logPos) -> this.placeCircle(context, logPos));
         }
     }
 
-    private void placeCircle(Context context, BlockPos pPos) {
+    /**
+     * Places blocks in a radius around the tree.
+     * @param context The {@link TreeDecorator.Context}.
+     * @param pos The initial {@link BlockPos} for placement.
+     */
+    private void placeCircle(Context context, BlockPos pos) {
         LevelSimulatedReader level = context.level();
         RandomSource random = context.random();
-        this.placeBlockAt(context, level, random, pPos, 0.0F);
+        this.placeBlockAt(context, level, random, pos, 0.0F);
         int radius = 10;
 
         for (int z = 1; z < radius; z++) {
             for (int x = 0; x < radius; x++) {
-                if (x * x + z * z > radius*radius) continue;
-                float distance = (float) Math.sqrt(x*x + z*z) / (radius*radius);
-                this.placeBlockAt(context, level, random, pPos.offset(x, 0, z), distance);
-                this.placeBlockAt(context, level, random, pPos.offset(-x, 0, -z), distance);
-                this.placeBlockAt(context, level, random, pPos.offset(-z, 0, x), distance);
-                this.placeBlockAt(context, level, random, pPos.offset(z, 0, -x), distance);
+                if (Mth.square(x) + Mth.square(z) > Mth.square(radius)) continue;
+                float distance = (float) Math.sqrt(Mth.square(x) + Mth.square(z)) / Mth.square(radius);
+                this.placeBlockAt(context, level, random, pos.offset(x, 0, z), distance);
+                this.placeBlockAt(context, level, random, pos.offset(-x, 0, -z), distance);
+                this.placeBlockAt(context, level, random, pos.offset(-z, 0, x), distance);
+                this.placeBlockAt(context, level, random, pos.offset(z, 0, -x), distance);
             }
         }
     }
 
-    private void placeBlockAt(Context context, LevelSimulatedReader pLevel, RandomSource pRandom, BlockPos pPos, float distance) {
-        for(int i = 9; i >= -4; i--) {
-            BlockPos blockpos = pPos.above(i);
-            if (context.isAir(blockpos.above())) {
-                if ((pLevel.isStateAtPosition(blockpos, HolidayTreeDecorator::isAetherGrass) || pLevel.isStateAtPosition(blockpos, HolidayTreeDecorator::isLeaves) || Feature.isGrassOrDirt(pLevel, blockpos)) && context.isAir(blockpos.above(4))) {
-                    if (distance <= pRandom.nextFloat() / 2 * (1 - distance)) {
-                        if (pLevel.isStateAtPosition(blockpos, HolidayTreeDecorator::isLeaves)) {
-                            context.setBlock(blockpos.above(), Blocks.SNOW.defaultBlockState());
+    /**
+     * Loops through a column and randomly places blocks as long as there is enough air above the position.
+     * If the block is a leaf, snow will be placed above.
+     * If the block is dirt or grass, a block from the provider will be placed above.
+     * @param context The {@link TreeDecorator.Context}.
+     * @param level The {@link LevelSimulatedReader} to place the block in.
+     * @param random The  {@link RandomSource}.
+     * @param pos The initial {@link BlockPos} for placement.
+     * @param distance The {@link Float} distance to place at.
+     */
+    private void placeBlockAt(Context context, LevelSimulatedReader level, RandomSource random, BlockPos pos, float distance) {
+        for (int i = 9; i >= -4; i--) {
+            BlockPos blockPos = pos.above(i);
+            if (context.isAir(blockPos.above())) {
+                if ((level.isStateAtPosition(blockPos, HolidayTreeDecorator::isAetherGrass) || level.isStateAtPosition(blockPos, HolidayTreeDecorator::isLeaves) || Feature.isGrassOrDirt(level, blockPos)) && context.isAir(blockPos.above(4))) {
+                    if (distance <= random.nextFloat() / 2 * (1 - distance)) {
+                        if (level.isStateAtPosition(blockPos, HolidayTreeDecorator::isLeaves)) {
+                            context.setBlock(blockPos.above(), Blocks.SNOW.defaultBlockState());
                         } else {
-                            context.setBlock(blockpos.above(), this.provider.getState(pRandom, blockpos));
+                            context.setBlock(blockPos.above(), this.provider.getState(random, blockPos));
                         }
                     }
                 }
             }
         }
     }
+
     private static boolean isAetherGrass(BlockState state) {
         return state.is(AetherTags.Blocks.AETHER_DIRT);
     }
+
     private static boolean isLeaves(BlockState state) {
         return state.is(BlockTags.LEAVES);
     }
