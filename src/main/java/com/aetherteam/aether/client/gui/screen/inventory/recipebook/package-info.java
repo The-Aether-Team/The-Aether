4a0ddb9037b1ed62aa86e4c69d99d4d6096b1ceb
bconlon <bconlon.gml@gmail.com>
2023-07-31 16:04:22 -0700

    chore: Delete a package-info

--- src/main/java/com/aetherteam/aether/client/gui/screen/inventory/recipebook/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package is for classes that extend RecipeBookComponent. These components are used to display the recipe book
- * on the Aether's menu screens.
- *
- * @since 3/31/22
- */
-package com.aetherteam.aether.client.gui.screen.inventory.recipebook;
