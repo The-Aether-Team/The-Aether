f34c1047e061d8ed967ce3d719101b03dc711ff0
bconlon <bconlon.gml@gmail.com>
2023-08-03 17:24:34 -0700

    refactor: Clean up TippableArrowRendererMixin

--- src/main/java/com/aetherteam/aether/mixin/mixins/client/TippableArrowRendererMixin.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/client/TippableArrowRendererMixin.java
@@ -16,11 +16,17 @@ public class TippableArrowRendererMixin {
     @Unique
     private static final ResourceLocation FLAMING_ARROW_LOCATION = new ResourceLocation(Aether.MODID, "textures/entity/projectile/flaming_arrow.png");
 
-    @Inject(at = @At("HEAD"), method = "getTextureLocation*", cancellable = true)
+    /**
+     * Changes the texture for any arrows if they are marked as Phoenix Arrows by {@link com.aetherteam.aether.capability.arrow.PhoenixArrowCapability}.
+     * @param entity The {@link Arrow} entity.
+     * @param cir The {@link ResourceLocation} {@link CallbackInfoReturnable} used for the method's return value.
+     */
+    @Inject(at = @At("HEAD"), method = "getTextureLocation(Lnet/minecraft/world/entity/projectile/Arrow;)Lnet/minecraft/resources/ResourceLocation;", cancellable = true)
     private void getTextureLocation(Arrow entity, CallbackInfoReturnable<ResourceLocation> cir) {
-        PhoenixArrow.get(entity).ifPresent(phoenixArrow -> {
+        PhoenixArrow.get(entity).ifPresent((phoenixArrow) -> {
             if (phoenixArrow.isPhoenixArrow()) {
-                if (entity.getColor() <= 0) {
+                Arrow arrow = (Arrow) phoenixArrow.getArrow();
+                if (arrow.getColor() <= 0) {
                     cir.setReturnValue(FLAMING_ARROW_LOCATION);
                 }
             }
