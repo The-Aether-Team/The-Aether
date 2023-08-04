b2fb99715a09182c15cd8b079eb3363e633e1aa3
bconlon <bconlon.gml@gmail.com>
2023-08-03 18:44:53 -0700

    refactor: Remove unused accessors from TitleScreenAccessor

--- src/main/java/com/aetherteam/aether/mixin/mixins/client/accessor/TitleScreenAccessor.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/client/accessor/TitleScreenAccessor.java
@@ -1,6 +1,5 @@
 package com.aetherteam.aether.mixin.mixins.client.accessor;
 
-import net.minecraft.client.gui.components.LogoRenderer;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -10,12 +9,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 public interface TitleScreenAccessor {
     @Accessor("splash")
     String aether$getSplash();
-
-    @Accessor("splash")
-    void aether$setSplash(String splash);
-
-    @Accessor("logoRenderer")
-    LogoRenderer aether$getLogoRenderer();
 
     @Accessor("fading")
     boolean aether$isFading();
