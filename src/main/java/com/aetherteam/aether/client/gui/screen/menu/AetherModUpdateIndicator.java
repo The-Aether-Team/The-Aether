d3e24e3d1942e581f70e0a426f95e263ac80ffce
bconlon <bconlon.gml@gmail.com>
2023-08-05 21:01:12 -0700

    refactor: Clean up AetherModUpdateIndicator

--- src/main/java/com/aetherteam/aether/client/gui/screen/menu/AetherModUpdateIndicator.java
+++ src/main/java/com/aetherteam/aether/client/gui/screen/menu/AetherModUpdateIndicator.java
@@ -3,6 +3,7 @@ package com.aetherteam.aether.client.gui.screen.menu;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.gui.TitleScreenModUpdateIndicator;
 import net.minecraftforge.client.loading.ClientModLoader;
@@ -14,35 +15,42 @@ import net.minecraftforge.versions.forge.ForgeVersion;
 
 public class AetherModUpdateIndicator extends TitleScreenModUpdateIndicator {
 	private static final ResourceLocation VERSION_CHECK_ICONS = new ResourceLocation(ForgeVersion.MOD_ID, "textures/gui/version_check_icons.png");
-	private final AetherTitleScreen menu;
+	private final AetherTitleScreen screen;
 	private VersionChecker.Status showNotification = null;
 	private boolean hasCheckedForUpdates = false;
 	
-	public AetherModUpdateIndicator(AetherTitleScreen menu) {
+	public AetherModUpdateIndicator(AetherTitleScreen screen) {
 		super(null);
-		this.menu = menu;
+		this.screen = screen;
 	}
 
+	/**
+	 * Renders Forge's mod update indicator next to the "# mods loaded" text on the title screen.
+	 * @param poseStack The rendering {@link PoseStack}.
+	 * @param mouseX The {@link Integer} for the mouse's x-position.
+	 * @param mouseY The {@link Integer} for the mouse's y-position.
+	 * @param partialTicks The {@link Float} for the game's partial ticks.
+	 */
 	@Override
-	public void render(PoseStack mStack, int mouseX, int mouseY, float partialTicks) {
+	public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
 		if (this.showNotification != null && this.showNotification.shouldDraw() && FMLConfig.runVersionCheck() && Minecraft.getInstance().screen != null) {
 			RenderSystem.setShaderTexture(0, VERSION_CHECK_ICONS);
 
 			this.width = Minecraft.getInstance().screen.width;
 			this.height = Minecraft.getInstance().screen.height;
 			this.font = Minecraft.getInstance().font;
-			int tModCount = ModList.get().size();
-			String modLine = ForgeI18n.parseMessage("fml.menu.loadingmods", tModCount);
+			int modCount = ModList.get().size();
+			String modText = ForgeI18n.parseMessage("fml.menu.loadingmods", modCount);
 
-			int x = width - font.width(modLine) - 11;
-			int y = height - font.lineHeight - 11;
+			int x = this.width - this.font.width(modText) - 11;
+			int y = this.height - this.font.lineHeight - 11;
 
-			if (!this.menu.isAlignedLeft()) {
-				x = font.width(modLine) + 4;
-				y = height - font.lineHeight - 1;
+			if (!this.screen.isAlignedLeft()) {
+				x = this.font.width(modText) + 4;
+				y = this.height - this.font.lineHeight - 1;
 			}
 
-			blit(mStack, x, y, this.showNotification.getSheetOffset() * 8, (this.showNotification.isAnimated() && ((System.currentTimeMillis() / 800 & 1) == 1)) ? 8 : 0, 8, 8, 64, 16);
+			GuiComponent.blit(poseStack, x, y, this.showNotification.getSheetOffset() * 8, (this.showNotification.isAnimated() && ((System.currentTimeMillis() / 800 & 1) == 1)) ? 8 : 0, 8, 8, 64, 16);
 		}
 	}
 
