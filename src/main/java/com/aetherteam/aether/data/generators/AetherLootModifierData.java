77367a316eefa22536c23e75b9a61731feb89c22
bconlon <67203206+bconlon1@users.noreply.github.com>
2023-09-04 19:56:27 -0700

    feat: GLM for randomly replacing armor with gloves in vanilla loot (#1675)
    closes #1674

--- src/main/java/com/aetherteam/aether/data/generators/AetherLootModifierData.java
+++ src/main/java/com/aetherteam/aether/data/generators/AetherLootModifierData.java
@@ -3,20 +3,17 @@ package com.aetherteam.aether.data.generators;
 import com.aetherteam.aether.Aether;
 import com.aetherteam.aether.block.AetherBlocks;
 import com.aetherteam.aether.item.AetherItems;
-import com.aetherteam.aether.loot.modifiers.DoubleDropsModifier;
-import com.aetherteam.aether.loot.modifiers.EnchantedGrassModifier;
-import com.aetherteam.aether.loot.modifiers.PigDropsModifier;
-import com.aetherteam.aether.loot.modifiers.RemoveSeedsModifier;
+import com.aetherteam.aether.loot.AetherLoot;
+import com.aetherteam.aether.loot.modifiers.*;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.storage.loot.predicates.AlternativeLootItemCondition;
-import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
-import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
-import net.minecraft.world.level.storage.loot.predicates.MatchTool;
+import net.minecraft.world.level.storage.loot.predicates.*;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
+import net.minecraftforge.common.loot.LootTableIdCondition;
 
 public class AetherLootModifierData extends GlobalLootModifierProvider {
     public AetherLootModifierData(PackOutput output) {
@@ -37,5 +34,13 @@ public class AetherLootModifierData extends GlobalLootModifierProvider {
                 }, new ItemStack(AetherItems.BLUE_BERRY.get())));
         this.add("double_drops", new DoubleDropsModifier(new LootItemCondition[]{ }));
         this.add("pig_drops", new PigDropsModifier(new LootItemCondition[]{ }));
+        this.add("gloves_loot_leather", new GlovesLootModifier(new LootItemCondition[]{ }, new ItemStack(AetherItems.LEATHER_GLOVES.get()), ArmorMaterials.LEATHER));
+        this.add("gloves_loot_chain", new GlovesLootModifier(new LootItemCondition[]{
+                InvertedLootItemCondition.invert(LootTableIdCondition.builder(AetherLoot.GOLD_DUNGEON_REWARD)).build()
+        }, new ItemStack(AetherItems.CHAINMAIL_GLOVES.get()), ArmorMaterials.CHAIN));
+        this.add("gloves_loot_iron", new GlovesLootModifier(new LootItemCondition[]{ }, new ItemStack(AetherItems.IRON_GLOVES.get()), ArmorMaterials.IRON));
+        this.add("gloves_loot_gold", new GlovesLootModifier(new LootItemCondition[]{ }, new ItemStack(AetherItems.GOLDEN_GLOVES.get()), ArmorMaterials.GOLD));
+        this.add("gloves_loot_diamond", new GlovesLootModifier(new LootItemCondition[]{ }, new ItemStack(AetherItems.DIAMOND_GLOVES.get()), ArmorMaterials.DIAMOND));
+        this.add("gloves_loot_netherite", new GlovesLootModifier(new LootItemCondition[]{ }, new ItemStack(AetherItems.NETHERITE_GLOVES.get()), ArmorMaterials.NETHERITE));
     }
 }
