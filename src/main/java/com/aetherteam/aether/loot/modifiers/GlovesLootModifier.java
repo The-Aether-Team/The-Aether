e2e16daeaf00882945b14cd4a8fa477af787d628
bconlon <bconlon.gml@gmail.com>
2023-09-10 14:05:47 -0700

    fix: loot works again

--- src/main/java/com/aetherteam/aether/loot/modifiers/GlovesLootModifier.java
+++ src/main/java/com/aetherteam/aether/loot/modifiers/GlovesLootModifier.java
@@ -54,7 +54,7 @@ public class GlovesLootModifier extends LootModifier {
             BlockPos pos = new BlockPos(vec3);
             BlockEntity blockEntity = context.getLevel().getBlockEntity(pos);
             if (blockEntity instanceof BaseContainerBlockEntity) {
-                ArmorMaterial armorMaterial = ArmorMaterials.valueOf(this.armorMaterial);
+                ArmorMaterial armorMaterial = ArmorMaterials.valueOf(this.armorMaterial.toUpperCase());
                 List<ItemStack> armorItems = lootStacks.stream().filter((itemStack) -> itemStack.getItem() instanceof ArmorItem armorItem && armorItem.getMaterial().equals(armorMaterial)).toList();
                 for (ItemStack armorStack : armorItems) {
                     if (randomSource.nextInt(4) < 1) {
