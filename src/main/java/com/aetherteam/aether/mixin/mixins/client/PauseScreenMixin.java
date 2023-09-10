b30b4c3a65e47acffaaadc79c18a7fb4d33e65db
bconlon <bconlon.gml@gmail.com>
2023-09-09 20:40:54 -0700

    refactor: Reimplement PauseScreenMixin

--- src/main/java/com/aetherteam/aether/mixin/mixins/client/PauseScreenMixin.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/client/PauseScreenMixin.java
@@ -1,28 +1,35 @@
 package com.aetherteam.aether.mixin.mixins.client;
 
+import com.aetherteam.aether.AetherConfig;
+import com.aetherteam.aether.client.WorldDisplayHelper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.PauseScreen;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PauseScreen.class)
-public class PauseScreenMixin { //todo need to figure out how to mixin into the disconnect button lambda. examine bytecode
-//    /**
-//     * Used by the world preview system.<br>
-//     * Stops the level disconnect behavior when the world preview is active, and instead
-//     * sets the {@link net.minecraft.client.gui.screens.TitleScreen} back up so there is no load time to return to the menu.
-//     * @param ci The {@link CallbackInfo} for the void method return.
-//     * @see WorldDisplayHelper#setActive()
-//     * @see WorldDisplayHelper#setupLevelForDisplay()
-//     */
-//    @Inject(at = @At(value = "HEAD"), method = "onDisconnect()V", cancellable = true)
-//    public void onDisconnect(CallbackInfo ci) {
-//        if (AetherConfig.CLIENT.enable_world_preview.get() && Minecraft.getInstance().getSingleplayerServer() != null) {
-//            WorldDisplayHelper.setActive();
-//            WorldDisplayHelper.setupLevelForDisplay();
-//            Player player = Minecraft.getInstance().player;
-//            if (player != null) {
-//                player.setXRot(0);
-//            }
-//            ci.cancel();
-//        }
-//    }
+public class PauseScreenMixin {
+    /**
+     * Used by the world preview system.<br>
+     * Stops the level disconnect behavior when the world preview is active, and instead
+     * sets the {@link net.minecraft.client.gui.screens.TitleScreen} back up so there is no load time to return to the menu.
+     * @param ci The {@link CallbackInfo} for the void method return.
+     * @see WorldDisplayHelper#setActive()
+     * @see WorldDisplayHelper#setupLevelForDisplay()
+     */
+    @Inject(at = @At(value = "HEAD"), method = "lambda$createPauseMenu$10(Lnet/minecraft/client/gui/components/Button;)V", cancellable = true)
+    public void onDisconnect(CallbackInfo ci) {
+        if (AetherConfig.CLIENT.enable_world_preview.get() && Minecraft.getInstance().getSingleplayerServer() != null) {
+            WorldDisplayHelper.setActive();
+            WorldDisplayHelper.setupLevelForDisplay();
+            Player player = Minecraft.getInstance().player;
+            if (player != null) {
+                player.setXRot(0);
+            }
+            ci.cancel();
+        }
+    }
 }
