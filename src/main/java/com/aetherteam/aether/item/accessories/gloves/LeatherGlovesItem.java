7dd135659955e7e0bfed114af8526bcc2ea48231
bconlon <67203206+bconlon1@users.noreply.github.com>
2023-09-14 18:12:21 -0700

    improv: Gloves now use ArmorMaterials (#1702)
    closes #1699

--- src/main/java/com/aetherteam/aether/item/accessories/gloves/LeatherGlovesItem.java
+++ src/main/java/com/aetherteam/aether/item/accessories/gloves/LeatherGlovesItem.java
@@ -1,10 +1,11 @@
 package com.aetherteam.aether.item.accessories.gloves;
 
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.DyeableLeatherItem;
 
 public class LeatherGlovesItem extends GlovesItem implements DyeableLeatherItem {
     public LeatherGlovesItem(double punchDamage, Properties properties) {
-        super(punchDamage, "leather_gloves", () -> SoundEvents.ARMOR_EQUIP_LEATHER, properties);
+        super(ArmorMaterials.LEATHER, punchDamage, "leather_gloves", () -> SoundEvents.ARMOR_EQUIP_LEATHER, properties);
     }
 }
