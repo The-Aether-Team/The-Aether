b282ce520ce8901a43873f4a46028f6f4dd9b24a
Oscar Payn <oscar@gildedgames.com>
2023-09-15 16:29:33 +0100

    refactor: Moa UV map expanded
    changed the Moa/cockatrice UV map to separate duplicated elements and allow asymmetrical designs

--- src/main/java/com/aetherteam/aether/client/renderer/entity/model/BipedBirdModel.java
+++ src/main/java/com/aetherteam/aether/client/renderer/entity/model/BipedBirdModel.java
@@ -43,15 +43,15 @@ public abstract class BipedBirdModel<T extends Entity & WingedBird & NotGrounded
         PartDefinition partDefinition = meshDefinition.getRoot();
         PartDefinition head = partDefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 13).addBox(-2.0F, -4.0F, -6.0F, 4.0F, 4.0F, 8.0F, CubeDeformation.NONE, 0.5F, 0.5F), PartPose.offset(0.0F, 8.0F, -4.0F));
         head.addOrReplaceChild("jaw", CubeListBuilder.create().texOffs(24, 13).addBox(-2.0F, -1.0F, -6.0F, 4.0F, 1.0F, 8.0F, new CubeDeformation(-0.1F), 0.5F, 0.5F), PartPose.ZERO);
-        head.addOrReplaceChild("neck", CubeListBuilder.create().texOffs( 44, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 6.0F, 2.0F, CubeDeformation.NONE, 0.5F, 0.5F), PartPose.ZERO);
+        head.addOrReplaceChild("neck", CubeListBuilder.create().texOffs( 22, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 6.0F, 2.0F, CubeDeformation.NONE, 0.5F, 0.5F), PartPose.ZERO);
         PartDefinition body = partDefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -3.0F, 0.0F, 6.0F, 8.0F, 5.0F, cube, 0.5F, 0.5F), PartPose.offsetAndRotation(0.0F, 16.0F, 0.0F, Mth.HALF_PI, 0.0F, 0.0F));
-        partDefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(22, 0).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 9.0F, 2.0F, CubeDeformation.NONE, 0.5F, 0.5F), PartPose.offset(-2.0F, 16.0F, 1.0F));
-        partDefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(22, 0).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 9.0F, 2.0F, CubeDeformation.NONE, 0.5F, 0.5F), PartPose.offset(2.0F, 16.0F, 1.0F));
-        body.addOrReplaceChild("right_wing", CubeListBuilder.create().texOffs(52, 0).addBox(-1.0F, 0.0F, -2.0F, 1.0F, 8.0F, 4.0F, CubeDeformation.NONE, 0.5F, 0.5F), PartPose.offset(-3.001F, -3.0F, 3.0F));
-        body.addOrReplaceChild("left_wing", CubeListBuilder.create().texOffs(52, 0).addBox(0.0F, 0.0F, -2.0F, 1.0F, 8.0F, 4.0F, CubeDeformation.NONE, 0.5F, 0.5F), PartPose.offset(3.001F, -3.0F, 3.0F));
-        partDefinition.addOrReplaceChild("right_tail_feather", CubeListBuilder.create().texOffs(30, 0).addBox(-1.0F, -5.0F, 5.0F, 2.0F, 1.0F, 5.0F, new CubeDeformation(-0.3F), 0.5F, 0.5F), PartPose.offsetAndRotation(0.0F, 17.5F, 1.0F, 0.25F, -0.375F, 0.0F));
-        partDefinition.addOrReplaceChild("middle_tail_feather", CubeListBuilder.create().texOffs(30, 0).addBox(-1.0F, -5.0F, 5.0F, 2.0F, 1.0F, 5.0F, new CubeDeformation(-0.3F), 0.5F, 0.5F), PartPose.offsetAndRotation(0.0F, 17.5F, 1.0F, 0.25F, 0.0F, 0.0F));
-        partDefinition.addOrReplaceChild("left_tail_feather", CubeListBuilder.create().texOffs(30, 0).addBox(-1.0F, -5.0F, 5.0F, 2.0F, 1.0F, 5.0F, new CubeDeformation(-0.3F), 0.5F, 0.5F), PartPose.offsetAndRotation(0.0F, 17.5F, 1.0F, 0.25F, 0.375F, 0.0F));
+        partDefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(54, 21).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 9.0F, 2.0F, CubeDeformation.NONE, 0.5F, 0.5F), PartPose.offset(-2.0F, 16.0F, 1.0F));
+        partDefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(46, 21).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 9.0F, 2.0F, CubeDeformation.NONE, 0.5F, 0.5F), PartPose.offset(2.0F, 16.0F, 1.0F));
+        body.addOrReplaceChild("right_wing", CubeListBuilder.create().texOffs(40, 0).addBox(-1.0F, 0.0F, -2.0F, 1.0F, 8.0F, 4.0F, CubeDeformation.NONE, 0.5F, 0.5F), PartPose.offset(-3.001F, -3.0F, 3.0F));
+        body.addOrReplaceChild("left_wing", CubeListBuilder.create().texOffs(30, 0).addBox(0.0F, 0.0F, -2.0F, 1.0F, 8.0F, 4.0F, CubeDeformation.NONE, 0.5F, 0.5F), PartPose.offset(3.001F, -3.0F, 3.0F));
+        partDefinition.addOrReplaceChild("right_tail_feather", CubeListBuilder.create().texOffs(0, 26).addBox(-1.0F, -5.0F, 5.0F, 2.0F, 1.0F, 5.0F, new CubeDeformation(-0.3F), 0.5F, 0.5F), PartPose.offsetAndRotation(0.0F, 17.5F, 1.0F, 0.25F, -0.375F, 0.0F));
+        partDefinition.addOrReplaceChild("middle_tail_feather", CubeListBuilder.create().texOffs(14, 26).addBox(-1.0F, -5.0F, 5.0F, 2.0F, 1.0F, 5.0F, new CubeDeformation(-0.3F), 0.5F, 0.5F), PartPose.offsetAndRotation(0.0F, 17.5F, 1.0F, 0.25F, 0.0F, 0.0F));
+        partDefinition.addOrReplaceChild("left_tail_feather", CubeListBuilder.create().texOffs(28, 26).addBox(-1.0F, -5.0F, 5.0F, 2.0F, 1.0F, 5.0F, new CubeDeformation(-0.3F), 0.5F, 0.5F), PartPose.offsetAndRotation(0.0F, 17.5F, 1.0F, 0.25F, 0.375F, 0.0F));
         return LayerDefinition.create(meshDefinition, 128, 64);
     }
 
