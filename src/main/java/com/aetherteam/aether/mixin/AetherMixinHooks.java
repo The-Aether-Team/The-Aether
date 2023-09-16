e590f74ec45f0d3d187d2d8e7fe016611f06c3ad
bconlon <bconlon.gml@gmail.com>
2023-09-15 18:34:33 -0700

    improv: Dispeners can equip accessories to armor stands

--- src/main/java/com/aetherteam/aether/mixin/AetherMixinHooks.java
+++ src/main/java/com/aetherteam/aether/mixin/AetherMixinHooks.java
@@ -103,19 +103,19 @@ public class AetherMixinHooks {
         return false;
     }
 
-    public static String getIdentifierForItem(Mob mob, ItemStack stack) {
+    public static String getIdentifierForItem(LivingEntity livingEntity, ItemStack stack) {
         if (AetherConfig.COMMON.use_curios_menu.get()) {
             TagKey<Item> glovesTag = TagKey.create(Registries.ITEM, new ResourceLocation(Curios.MODID, "hands"));
             TagKey<Item> pendantTag = TagKey.create(Registries.ITEM, new ResourceLocation(Curios.MODID, "necklace"));
             if (stack.is(glovesTag)) {
                 return "hands";
-            } else if (stack.is(pendantTag) && (mob.getType() == EntityType.PIGLIN || mob.getType() == EntityType.ZOMBIFIED_PIGLIN)) {
+            } else if (stack.is(pendantTag) && (livingEntity.getType() == EntityType.PIGLIN || livingEntity.getType() == EntityType.ZOMBIFIED_PIGLIN)) {
                 return "necklace";
             }
         } else {
             if (stack.is(AetherTags.Items.AETHER_GLOVES)) {
                 return "aether_gloves";
-            } else if (stack.is(AetherTags.Items.AETHER_PENDANT) && (mob.getType() == EntityType.PIGLIN || mob.getType() == EntityType.ZOMBIFIED_PIGLIN)) {
+            } else if (stack.is(AetherTags.Items.AETHER_PENDANT) && (livingEntity.getType() == EntityType.PIGLIN || livingEntity.getType() == EntityType.ZOMBIFIED_PIGLIN)) {
                 return "aether_pendant";
             }
         }
---
+++ src/main/java/com/aetherteam/aether/mixin/mixins/common/ArmorStandMixin.java
@@ -0,0 +1,38 @@
+package com.aetherteam.aether.mixin.mixins.common;
+
+import com.aetherteam.aether.AetherConfig;
+import com.aetherteam.aether.item.accessories.cape.CapeItem;
+import com.aetherteam.aether.item.accessories.gloves.GlovesItem;
+import com.aetherteam.aether.item.accessories.miscellaneous.ShieldOfRepulsionItem;
+import com.aetherteam.aether.item.accessories.pendant.PendantItem;
+import com.aetherteam.aether.mixin.AetherMixinHooks;
+import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ArmorStand.class)
+public class ArmorStandMixin {
+    @Inject(at = @At(value = "HEAD"), method = "canTakeItem(Lnet/minecraft/world/item/ItemStack;)Z", cancellable = true)
+    private void canTakeItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        ArmorStand armorStand = (ArmorStand) (Object) this;
+        String identifier = "";
+        if (stack.getItem() instanceof GlovesItem) {
+            identifier = AetherConfig.COMMON.use_curios_menu.get() ? "hands" : "aether_gloves";
+        } else if (stack.getItem() instanceof PendantItem) {
+            identifier = AetherConfig.COMMON.use_curios_menu.get() ? "necklace" : "aether_pendant";
+        } else if (stack.getItem() instanceof CapeItem) {
+            identifier = AetherConfig.COMMON.use_curios_menu.get() ? "back" : "aether_cape";
+        } else if (stack.getItem() instanceof ShieldOfRepulsionItem) {
+            identifier = AetherConfig.COMMON.use_curios_menu.get() ? "body" : "aether_shield";
+        }
+        if (!identifier.isEmpty()) {
+            ItemStack accessory = AetherMixinHooks.getItemByIdentifier(armorStand, identifier);
+            if (accessory.isEmpty()) {
+                cir.setReturnValue(true);
+            }
+        }
+    }
+}
