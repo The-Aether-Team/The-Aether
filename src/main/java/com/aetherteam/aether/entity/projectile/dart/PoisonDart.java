c3b850eff71d3f00016001f701c96013111bdacb
Oscar Payn <oscar@gildedgames.com>
2023-09-19 16:56:08 +0100

    feat: dart shooter balance experiment
    reduced firing speed
    slightly reduced projectile speed
    reduced damage
    reduced power enchantment modifier

--- src/main/java/com/aetherteam/aether/entity/projectile/dart/PoisonDart.java
+++ src/main/java/com/aetherteam/aether/entity/projectile/dart/PoisonDart.java
@@ -11,12 +11,12 @@ import net.minecraft.world.level.Level;
 public class PoisonDart extends AbstractDart {
     public PoisonDart(EntityType<? extends PoisonDart> type, Level level) {
         super(type, level, AetherItems.POISON_DART);
-        this.setBaseDamage(2.0);
+        this.setBaseDamage(0.5);
     }
 
     public PoisonDart(Level level) {
         super(AetherEntityTypes.POISON_DART.get(), level, AetherItems.POISON_DART);
-        this.setBaseDamage(2.0);
+        this.setBaseDamage(0.5);
     }
 
     /**
@@ -26,6 +26,6 @@ public class PoisonDart extends AbstractDart {
     @Override
     protected void doPostHurtEffects(LivingEntity living) {
         super.doPostHurtEffects(living);
-        living.addEffect(new MobEffectInstance(AetherEffects.INEBRIATION.get(), 500, 0));
+        living.addEffect(new MobEffectInstance(AetherEffects.INEBRIATION.get(), 200, 0));
     }
 }
