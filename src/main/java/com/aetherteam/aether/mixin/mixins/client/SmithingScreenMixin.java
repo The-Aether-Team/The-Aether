24d16c200572f0aa175b642617a8a2f702f3d4ae
bconlon <67203206+bconlon1@users.noreply.github.com>
2023-09-23 17:29:19 -0700

    refactor: Clean up SmithingScreenMixin (#1760)
    closes #1755

--- src/main/java/com/aetherteam/aether/mixin/mixins/client/SmithingScreenMixin.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/client/SmithingScreenMixin.java
@@ -13,16 +13,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.type.ISlotType;
 import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
-import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
-import top.theillusivec4.curios.api.type.inventory.IDynamicStackHandler;
-import top.theillusivec4.curios.common.inventory.CurioStacksHandler;
 
 import javax.annotation.Nullable;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 @Mixin(SmithingScreen.class)
 public class SmithingScreenMixin {
@@ -42,20 +35,13 @@ public class SmithingScreenMixin {
             LazyOptional<ICuriosItemHandler> lazyHandler = CuriosApi.getCuriosInventory(this.armorStandPreview);
             if (lazyHandler.isPresent() && lazyHandler.resolve().isPresent()) {
                 ICuriosItemHandler handler = lazyHandler.resolve().get();
-                ISlotType slot = CuriosApi.getEntitySlots(this.armorStandPreview.getType()).get(identifier); // Temporary workaround to Curios not registering slots for client-only entities.
-                handler.setCurios(new HashMap<>(Map.of(identifier, new CurioStacksHandler(handler, slot.getIdentifier(), slot.getSize(), slot.useNativeGui(), slot.hasCosmetic(), slot.canToggleRendering(), slot.getDropRule()))));
-                Optional<ICurioStacksHandler> stacksHandler = handler.getStacksHandler(identifier);
-                if (stacksHandler.isPresent()) {
-                    IDynamicStackHandler stackHandler = stacksHandler.get().getCosmeticStacks();
-                    if (0 < stackHandler.getSlots()) {
-                        stackHandler.setStackInSlot(0, ItemStack.EMPTY);
-                        if (!stack.isEmpty()) {
-                            Item item = stack.getItem();
-                            if (item instanceof GlovesItem) {
-                                stackHandler.setStackInSlot(0, stack.copy());
-                                ci.cancel();
-                            }
-                        }
+                handler.setEquippedCurio(identifier, 0, ItemStack.EMPTY);
+                if (!stack.isEmpty()) {
+                    ItemStack itemStack = stack.copy();
+                    Item item = stack.getItem();
+                    if (item instanceof GlovesItem) {
+                        handler.setEquippedCurio(identifier, 0, itemStack);
+                        ci.cancel();
                     }
                 }
             }
