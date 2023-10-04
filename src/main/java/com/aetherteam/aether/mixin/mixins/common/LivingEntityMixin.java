2715253bb07e6fc9ce3350bb30bafd7cbc85bec3
bconlon <67203206+bconlon1@users.noreply.github.com>
2023-10-04 10:17:34 -0700

    improv: Fix extended reach (#1791)
    #1781

--- src/main/java/com/aetherteam/aether/mixin/mixins/common/LivingEntityMixin.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/common/LivingEntityMixin.java
@@ -1,17 +1,7 @@
 package com.aetherteam.aether.mixin.mixins.common;
 
-import com.aetherteam.aether.event.hooks.AbilityHooks;
-import com.aetherteam.aether.event.listeners.abilities.ToolAbilityListener;
 import com.aetherteam.aether.item.combat.abilities.armor.PhoenixArmor;
-import com.aetherteam.aether.item.tools.abilities.ValkyrieTool;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeInstance;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.common.ForgeMod;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -28,30 +18,5 @@ public class LivingEntityMixin {
     private void travel(CallbackInfo ci) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
         PhoenixArmor.boostVerticalLavaSwimming(livingEntity);
-    }
-
-    /**
-     * Reduces the player's offhand reach to its normal distance when holding a Valkyrie Tool in the mainhand.
-     * This ensures that the offhand reach is not extended by the mainhand attribute modifier given by the Valkyrie Tool.
-     * @param reach The {@link Double} for the original reach attribute.
-     * @param attribute The reach {@link Attribute}.
-     * @return The {@link Double} for the new reach attribute.
-     */
-    @ModifyExpressionValue(at = @At(value = "INVOKE", target = "net/minecraft/world/entity/ai/attributes/AttributeMap.getValue(Lnet/minecraft/world/entity/ai/attributes/Attribute;)D"), method = "getAttributeValue(Lnet/minecraft/world/entity/ai/attributes/Attribute;)D")
-    private double modifyReachAttribute(double reach, Attribute attribute) {
-        LivingEntity livingEntity = (LivingEntity) (Object) this;
-        if (livingEntity instanceof Player player && (attribute == ForgeMod.ENTITY_REACH.get() || attribute == ForgeMod.BLOCK_REACH.get())) {
-            InteractionHand hand = ToolAbilityListener.INTERACTION_HAND;
-            if (hand == InteractionHand.OFF_HAND && AbilityHooks.ToolHooks.hasValkyrieItemInMainHandOnly(player)) {
-                AttributeInstance reachDistance = player.getAttribute(attribute);
-                if (reachDistance != null) {
-                    AttributeModifier valkyrieModifier = reachDistance.getModifier(ValkyrieTool.REACH_DISTANCE_MODIFIER_UUID);
-                    if (valkyrieModifier != null) {
-                        return reach - valkyrieModifier.getAmount();
-                    }
-                }
-            }
-        }
-        return reach;
     }
 }
