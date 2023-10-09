ecd036219e27d97fa65287af3fe4b6156c93ac75
Oscar Payn <oscar@gildedgames.com>
2023-10-09 21:37:59 +0100

    feat: new moa skins and model tweak

--- src/main/java/com/aetherteam/aether/client/renderer/entity/model/BipedBirdModel.java
+++ src/main/java/com/aetherteam/aether/client/renderer/entity/model/BipedBirdModel.java
@@ -45,8 +45,8 @@ public abstract class BipedBirdModel<T extends Entity & WingedBird & NotGrounded
         head.addOrReplaceChild("jaw", CubeListBuilder.create().texOffs(24, 13).addBox(-2.0F, -1.0F, -6.0F, 4.0F, 1.0F, 8.0F, new CubeDeformation(-0.1F), 0.5F, 0.5F), PartPose.ZERO);
         head.addOrReplaceChild("neck", CubeListBuilder.create().texOffs( 22, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 6.0F, 2.0F, cube, 0.5F, 0.5F), PartPose.ZERO);
         PartDefinition body = partDefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -3.0F, 0.0F, 6.0F, 8.0F, 5.0F, cube, 0.5F, 0.5F), PartPose.offsetAndRotation(0.0F, 16.0F, 0.0F, Mth.HALF_PI, 0.0F, 0.0F));
-        partDefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(54, 21).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 9.0F, 2.0F, cube, 0.5F, 0.5F), PartPose.offset(-2.0F, 16.0F, 1.0F));
-        partDefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(46, 21).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 9.0F, 2.0F, cube, 0.5F, 0.5F), PartPose.offset(2.0F, 16.0F, 1.0F));
+        partDefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(54, 21).addBox(-0.99F, -1.0F, -1.0F, 2.0F, 9.0F, 2.0F, cube, 0.5F, 0.5F), PartPose.offset(-2.0F, 16.0F, 1.0F));
+        partDefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(46, 21).addBox(-1.01F, -1.0F, -1.0F, 2.0F, 9.0F, 2.0F, cube, 0.5F, 0.5F), PartPose.offset(2.0F, 16.0F, 1.0F));
         body.addOrReplaceChild("right_wing", CubeListBuilder.create().texOffs(40, 0).addBox(-1.0F, 0.0F, -2.0F, 1.0F, 8.0F, 4.0F, cube, 0.5F, 0.5F), PartPose.offset(-3.001F, -3.0F, 3.0F));
         body.addOrReplaceChild("left_wing", CubeListBuilder.create().texOffs(30, 0).addBox(0.0F, 0.0F, -2.0F, 1.0F, 8.0F, 4.0F, cube, 0.5F, 0.5F), PartPose.offset(3.001F, -3.0F, 3.0F));
         partDefinition.addOrReplaceChild("right_tail_feather", CubeListBuilder.create().texOffs(0, 26).addBox(-1.0F, -5.0F, 5.0F, 2.0F, 1.0F, 5.0F, new CubeDeformation(-0.3F), 0.5F, 0.5F), PartPose.offsetAndRotation(0.0F, 17.5F, 1.0F, 0.25F, -0.375F, 0.0F));
