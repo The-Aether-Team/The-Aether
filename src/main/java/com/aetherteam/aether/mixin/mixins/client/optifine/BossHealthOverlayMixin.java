893167639f0297370099143cc374c58ee0b4e374
iris <60985521+KaiAF@users.noreply.github.com>
2023-10-13 14:33:38 -0500

    fix: crash with OptiFine (#1809)

---
+++ src/main/java/com/aetherteam/aether/mixin/mixins/client/optifine/BossHealthOverlayMixin.java
@@ -0,0 +1,26 @@
+package com.aetherteam.aether.mixin.mixins.client.optifine;
+
+import com.aetherteam.aether.client.event.hooks.GuiHooks;
+import net.minecraft.client.gui.components.BossHealthOverlay;
+import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(BossHealthOverlay.class)
+public class BossHealthOverlayMixin {
+    /**
+     * Cancels the {@link CustomizeGuiOverlayEvent.BossEventProgress} GUI event after the event hook has been called for it.
+     * Made as a workaround for Jade's boss bar pushdown.<br>
+     * This modifies the assignment of the {@link CustomizeGuiOverlayEvent.BossEventProgress} event variable.
+     * @param event The original {@link CustomizeGuiOverlayEvent.BossEventProgress} parameter value.
+     * @return The modified {@link CustomizeGuiOverlayEvent.BossEventProgress} parameter value.
+     */
+    @ModifyVariable(at = @At(value = "STORE"), method = "render(Lnet/minecraft/client/gui/GuiGraphics;)V", index = 9)
+    @SuppressWarnings({"MixinAnnotationTarget", "InvalidInjectorMethodSignature"})
+    private Object event(Object event) {
+        CustomizeGuiOverlayEvent.BossEventProgress e = (CustomizeGuiOverlayEvent.BossEventProgress)event;
+        e.setCanceled(GuiHooks.BOSS_EVENTS.containsKey(e.getBossEvent().getId()));
+        return event;
+    }
+}
