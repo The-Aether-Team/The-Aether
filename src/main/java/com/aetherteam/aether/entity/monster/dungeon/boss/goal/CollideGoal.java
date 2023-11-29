7e28c04bf2f606c0a500cfc7bb0d517415edcfad
bconlon <bconlon.gml@gmail.com>
2023-11-28 21:40:04 -0800

    refactor: The slider uses the goal selector now.

---
+++ src/main/java/com/aetherteam/aether/entity/monster/dungeon/boss/goal/CollideGoal.java
@@ -0,0 +1,59 @@
+package com.aetherteam.aether.entity.monster.dungeon.boss.goal;
+
+import com.aetherteam.aether.data.resources.AetherDamageTypes;
+import com.aetherteam.aether.entity.monster.dungeon.boss.Slider;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+
+public class CollideGoal extends Goal {
+    private final Slider slider;
+
+    public CollideGoal(Slider slider) {
+        this.slider = slider;
+    }
+
+    @Override
+    public boolean canUse() {
+        if (!this.slider.isAwake() || this.slider.isDeadOrDying()) {
+            return false;
+        }
+        return this.slider.attackCooldown() <= 0 || this.slider.getDeltaMovement().length() > 0.08;
+    }
+
+    @Override
+    public void tick() {
+        Vec3 min = new Vec3(slider.getBoundingBox().minX - 0.1, slider.getBoundingBox().minY - 0.1, slider.getBoundingBox().minZ - 0.1);
+        Vec3 max = new Vec3(slider.getBoundingBox().maxX + 0.1, slider.getBoundingBox().maxY + 0.1, slider.getBoundingBox().maxZ + 0.1);
+        AABB collisionBounds = new AABB(min, max);
+        for (Entity entity : this.slider.getLevel().getEntities(slider, collisionBounds)) {
+            if (entity instanceof LivingEntity livingEntity && entity.hurt(AetherDamageTypes.crush(this.slider), 6)) {
+                if (livingEntity instanceof Player player && player.getUseItem().is(Items.SHIELD) && player.isBlocking()) { // Disables the player's Shield if one is being used.
+                    player.getCooldowns().addCooldown(Items.SHIELD, 100);
+                    player.stopUsingItem();
+                    this.slider.getLevel().broadcastEntityEvent(player, (byte) 30);
+                }
+                entity.setDeltaMovement(entity.getDeltaMovement().multiply(4.0, 1.0, 4.0).add(0.0, 0.25, 0.0));
+
+                this.slider.setMoveDelay(this.slider.calculateMoveDelay());
+                this.slider.setAttackCooldown(20);
+                this.slider.setMoveDirection(null);
+
+                // Stop the Slider's movement.
+                this.slider.playSound(this.slider.getCollideSound(), 2.5F, 1.0F / (this.slider.getRandom().nextFloat() * 0.2F + 0.9F));
+                this.slider.setDeltaMovement(Vec3.ZERO);
+            } else if (!(entity instanceof Player player && player.isCreative()) && !(entity instanceof Slider)) {
+                entity.setDeltaMovement(this.slider.getDeltaMovement().multiply(4.0, 1.0, 4.0).add(0.0, 0.25, 0.0));
+            }
+        }
+    }
+
+    @Override
+    public boolean requiresUpdateEveryTick() {
+        return true;
+    }
+}
---
+++ src/main/java/com/aetherteam/aether/entity/monster/dungeon/boss/goal/CrushGoal.java
@@ -0,0 +1,80 @@
+package com.aetherteam.aether.entity.monster.dungeon.boss.goal;
+
+import com.aetherteam.aether.AetherTags;
+import com.aetherteam.aether.entity.EntityUtil;
+import com.aetherteam.aether.entity.monster.dungeon.boss.Slider;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.event.ForgeEventFactory;
+
+public class CrushGoal extends Goal {
+    private final Slider slider;
+
+    public CrushGoal(Slider slider) {
+        this.slider = slider;
+    }
+    @Override
+    public boolean canUse() {
+        return this.slider.isAwake() && !this.slider.isDeadOrDying() && (this.slider.horizontalCollision || this.slider.verticalCollision || this.blocksBetween(this.slider));
+    }
+
+    @Override
+    public boolean canContinueToUse() {
+        return false;
+    }
+
+    @Override
+    public void start() {
+        boolean crushed = false;
+        if (ForgeEventFactory.getMobGriefingEvent(this.slider.getLevel(), this.slider)) {
+            AABB crushBox = this.slider.getBoundingBox().inflate(0.2);
+            for (BlockPos pos : BlockPos.betweenClosed(Mth.floor(crushBox.minX), Mth.floor(crushBox.minY), Mth.floor(crushBox.minZ), Mth.floor(crushBox.maxX), Mth.floor(crushBox.maxY), Mth.floor(crushBox.maxZ))) {
+                if (this.slider.getDungeon() == null || this.slider.getDungeon().roomBounds().contains(Vec3.atCenterOf(pos))) {
+                    BlockState blockState = this.slider.getLevel().getBlockState(pos);
+                    if (this.isBreakable(blockState)) {
+                        crushed = this.slider.getLevel().destroyBlock(pos, true, this.slider) || crushed;
+                        EntityUtil.spawnRemovalParticles(this.slider.getLevel(), pos);
+                    }
+                }
+            }
+        }
+        if (crushed) {
+            this.slider.getLevel().playSound(null, this.slider.blockPosition(), SoundEvents.GENERIC_EXPLODE, SoundSource.BLOCKS, 3.0F, (0.625F + (this.slider.getRandom().nextFloat() - this.slider.getRandom().nextFloat()) * 0.2F) * 0.7F);
+            this.slider.playSound(this.slider.getCollideSound(), 2.5F, 1.0F / (this.slider.getRandom().nextFloat() * 0.2F + 0.9F));
+            this.slider.setMoveDelay(this.slider.calculateMoveDelay());
+            this.slider.setDeltaMovement(Vec3.ZERO);
+        }
+    }
+
+    /**
+     * Checks if there are blocks between a target and the Slider.
+     * @param slider The {@link Slider} that the brain belongs to.
+     * @return Whether there are blocks, as a {@link Boolean}.
+     */
+    private boolean blocksBetween(Slider slider) {
+        LivingEntity target = slider.getTarget();
+        if (target == null) {
+            return false;
+        }
+        return slider.getLevel().getBlockStates(
+                AABB.of(BoundingBox.fromCorners(target.blockPosition(), slider.blockPosition()))
+        ).anyMatch(this::isBreakable);
+    }
+
+    private boolean isBreakable(BlockState blockState) {
+        return !blockState.isAir() && !blockState.is(AetherTags.Blocks.SLIDER_UNBREAKABLE);
+    }
+
+    @Override
+    public boolean requiresUpdateEveryTick() {
+        return true;
+    }
+}
