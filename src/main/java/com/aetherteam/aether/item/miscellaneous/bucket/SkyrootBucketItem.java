a7b0457bcb90164898ad71d18da133c49aedf8a8
bconlon <bconlon.gml@gmail.com>
2024-01-01 13:40:54 -0800

    fix(item): Skyroot Buckets Now Burn In Furnances

--- src/main/java/com/aetherteam/aether/item/miscellaneous/bucket/SkyrootBucketItem.java
+++ src/main/java/com/aetherteam/aether/item/miscellaneous/bucket/SkyrootBucketItem.java
@@ -15,6 +15,7 @@ import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BucketPickup;
@@ -137,6 +138,11 @@ public class SkyrootBucketItem extends BucketItem {
     @Override
     public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable CompoundTag tag) {
         return null;
+    }
+
+    @Override
+    public int getBurnTime(ItemStack itemStack, RecipeType<?> recipeType) {
+        return 200;
     }
 
     /**
