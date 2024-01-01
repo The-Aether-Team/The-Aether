151c04335d56d3a6a7e1595a7886f79390b79dd6
bconlon <bconlon.gml@gmail.com>
2024-01-01 13:06:11 -0800

    fix: no more z-fighting on animal wings model

--- src/main/java/com/aetherteam/aether/client/renderer/entity/model/QuadrupedWingsModel.java
+++ src/main/java/com/aetherteam/aether/client/renderer/entity/model/QuadrupedWingsModel.java
@@ -30,9 +30,9 @@ public class QuadrupedWingsModel<T extends WingedAnimal> extends EntityModel<T> 
         MeshDefinition meshDefinition = new MeshDefinition();
         PartDefinition partDefinition = meshDefinition.getRoot();
         PartDefinition leftWingInnerDef = partDefinition.addOrReplaceChild("left_wing_inner", CubeListBuilder.create().texOffs(0, 0).mirror().addBox(-1.0F, -16.0F, 0.0F, 2.0F, 16.0F, 8.0F), PartPose.offset(-4.0F, 2.0F + offset, -4.0F));
-        leftWingInnerDef.addOrReplaceChild("left_wing_outer", CubeListBuilder.create().texOffs(20, 0).mirror().addBox(-1.0F, -16.0F, 0.0F, 2.0F, 16.0F, 8.0F), PartPose.offset(0.0F, -16.0F, 0.0F));
+        leftWingInnerDef.addOrReplaceChild("left_wing_outer", CubeListBuilder.create().texOffs(20, 0).mirror().addBox(-1.0F, -16.0F, 0.0005F, 2.0F, 16.0F, 7.999F), PartPose.offset(0.0F, -16.0F, 0.0F));
         PartDefinition rightWingInnerDef = partDefinition.addOrReplaceChild("right_wing_inner", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, -16.0F, 0.0F, 2.0F, 16.0F, 8.0F), PartPose.offset(4.0F, 2.0F + offset, -4.0F));
-        rightWingInnerDef.addOrReplaceChild("right_wing_outer", CubeListBuilder.create().texOffs(20, 0).addBox(-1.0F, -16.0F, 0.0F, 2.0F, 16.0F, 8.0F), PartPose.offset(0.0F, -16.0F, 0.0F));
+        rightWingInnerDef.addOrReplaceChild("right_wing_outer", CubeListBuilder.create().texOffs(20, 0).addBox(-1.0F, -16.0F, 0.0005F, 2.0F, 16.0F, 7.999F), PartPose.offset(0.0F, -16.0F, 0.0F));
         return LayerDefinition.create(meshDefinition, 64, 32);
     }
 
