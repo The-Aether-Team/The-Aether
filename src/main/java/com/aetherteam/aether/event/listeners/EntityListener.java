8e1e2400ce3a27f43cb329dd4cee85b6fe8479c3
bconlon <67203206+bconlon1@users.noreply.github.com>
2024-01-15 19:46:42 -0800

    fix: Prevent Thunder Crystal lightning from damaging items (#1971)
    #1965

--- src/main/java/com/aetherteam/aether/event/listeners/EntityListener.java
+++ src/main/java/com/aetherteam/aether/event/listeners/EntityListener.java
@@ -7,6 +7,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
@@ -111,7 +112,13 @@ public class EntityListener {
     @SubscribeEvent
     public static void onLightningStrike(EntityStruckByLightningEvent event) {
         Entity entity = event.getEntity();
-        event.setCanceled(EntityHooks.lightningHitKeys(entity));
+        LightningBolt lightningBolt = event.getLightning();
+        if (EntityHooks.lightningHitKeys(entity)) {
+            event.setCanceled(true);
+        }
+        if (EntityHooks.thunderCrystalHitItems(entity, lightningBolt)) {
+            event.setCanceled(true);
+        }
     }
 
     /**
