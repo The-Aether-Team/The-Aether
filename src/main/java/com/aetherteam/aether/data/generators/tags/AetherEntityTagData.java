245e898f03ab3d8de9dd07f3fb750f1ab74a3f1d
bconlon <67203206+bconlon1@users.noreply.github.com>
2024-01-24 13:54:02 -0800

    improv: dungeon fixes (#1983)
    #1979

--- src/main/java/com/aetherteam/aether/data/generators/tags/AetherEntityTagData.java
+++ src/main/java/com/aetherteam/aether/data/generators/tags/AetherEntityTagData.java
@@ -3,6 +3,7 @@ package com.aetherteam.aether.data.generators.tags;
 import com.aetherteam.aether.Aether;
 import com.aetherteam.aether.AetherTags;
 import com.aetherteam.aether.entity.AetherEntityTypes;
+import com.aetherteam.aether.item.AetherItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
@@ -71,6 +72,16 @@ public class AetherEntityTagData extends EntityTypeTagsProvider {
                 AetherEntityTypes.SUN_SPIRIT.get());
         this.tag(AetherTags.Entities.TREATED_AS_AETHER_ENTITY);
         this.tag(AetherTags.Entities.TREATED_AS_VANILLA_ENTITY);
+        this.tag(AetherTags.Entities.DUNGEON_ENTITIES).add(
+                AetherEntityTypes.SENTRY.get(),
+                AetherEntityTypes.SLIDER.get(),
+                AetherEntityTypes.VALKYRIE.get(),
+                AetherEntityTypes.VALKYRIE_QUEEN.get(),
+                AetherEntityTypes.THUNDER_CRYSTAL.get(),
+                AetherEntityTypes.SUN_SPIRIT.get(),
+                AetherEntityTypes.FIRE_MINION.get(),
+                AetherEntityTypes.FIRE_CRYSTAL.get(),
+                AetherEntityTypes.ICE_CRYSTAL.get());
 
         // Forge
         this.tag(Tags.EntityTypes.BOSSES).add(
