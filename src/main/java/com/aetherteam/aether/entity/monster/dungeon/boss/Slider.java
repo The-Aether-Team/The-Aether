5f8f665196b3c4f5e86b4a0d9a31092a41877ef6
bconlon <bconlon.gml@gmail.com>
2024-01-27 20:52:02 -0800

    feat: AddPlayer and RemovePlayer BossFight events

--- src/main/java/com/aetherteam/aether/entity/monster/dungeon/boss/Slider.java
+++ src/main/java/com/aetherteam/aether/entity/monster/dungeon/boss/Slider.java
@@ -367,6 +367,7 @@ public class Slider extends PathfinderMob implements AetherBossMob<Slider>, Enem
         PacketRelay.sendToPlayer(AetherPacketHandler.INSTANCE, new BossInfoPacket.Display(this.bossFight.getId(), this.getId()), player);
         if (this.getDungeon() == null || this.getDungeon().isPlayerTracked(player)) {
             this.bossFight.addPlayer(player);
+            AetherEventDispatch.onBossFightPlayerAdd(this, this.getDungeon(), player);
         }
     }
 
@@ -379,6 +380,7 @@ public class Slider extends PathfinderMob implements AetherBossMob<Slider>, Enem
         super.stopSeenByPlayer(player);
         PacketRelay.sendToPlayer(AetherPacketHandler.INSTANCE, new BossInfoPacket.Remove(this.bossFight.getId(), this.getId()), player);
         this.bossFight.removePlayer(player);
+        AetherEventDispatch.onBossFightPlayerRemove(this, this.getDungeon(), player);
     }
 
     /**
@@ -389,6 +391,7 @@ public class Slider extends PathfinderMob implements AetherBossMob<Slider>, Enem
     public void onDungeonPlayerAdded(@Nullable Player player) {
         if (player instanceof ServerPlayer serverPlayer) {
             this.bossFight.addPlayer(serverPlayer);
+            AetherEventDispatch.onBossFightPlayerAdd(this, this.getDungeon(), serverPlayer);
         }
     }
 
@@ -400,6 +403,7 @@ public class Slider extends PathfinderMob implements AetherBossMob<Slider>, Enem
     public void onDungeonPlayerRemoved(@Nullable Player player) {
         if (player instanceof ServerPlayer serverPlayer) {
             this.bossFight.removePlayer(serverPlayer);
+            AetherEventDispatch.onBossFightPlayerRemove(this, this.getDungeon(), serverPlayer);
         }
     }
 
