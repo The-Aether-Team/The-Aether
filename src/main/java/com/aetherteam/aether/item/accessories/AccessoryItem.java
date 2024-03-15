b080a31e375c7f80c4649402d1bc2a8bb795aee6
bconlon <bconlon.gml@gmail.com>
2024-03-15 15:19:13 -0700

    refactor: Minor adjustment to AccessoryItem and variants

--- src/main/java/com/aetherteam/aether/item/accessories/AccessoryItem.java
+++ src/main/java/com/aetherteam/aether/item/accessories/AccessoryItem.java
@@ -2,6 +2,7 @@ package com.aetherteam.aether.item.accessories;
 
 import com.aetherteam.aether.block.dispenser.AetherDispenseBehaviors;
 import com.aetherteam.aether.client.AetherSoundEvents;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Vanishable;
@@ -12,9 +13,18 @@ import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
+import java.util.function.Supplier;
+
 public class AccessoryItem extends Item implements ICurioItem, Vanishable {
+    private final Supplier<? extends SoundEvent> soundEventSupplier;
+
     public AccessoryItem(Properties properties) {
+        this(AetherSoundEvents.ITEM_ACCESSORY_EQUIP_GENERIC, properties);
+    }
+
+    public AccessoryItem(Supplier<? extends SoundEvent> soundEventSupplier, Properties properties) {
         super(properties);
+        this.soundEventSupplier = soundEventSupplier;
         DispenserBlock.registerBehavior(this, AetherDispenseBehaviors.DISPENSE_ACCESSORY_BEHAVIOR); // Behavior to allow accessories to be equipped from a Dispenser.
     }
 
@@ -25,7 +35,7 @@ public class AccessoryItem extends Item implements ICurioItem, Vanishable {
 
     @Override
     public ICurio.SoundInfo getEquipSound(SlotContext slotContext, ItemStack stack) {
-        return new ICurio.SoundInfo(AetherSoundEvents.ITEM_ACCESSORY_EQUIP_GENERIC.get(), 1.0F, 1.0F);
+        return new ICurio.SoundInfo(this.soundEventSupplier.get(), 1.0F, 1.0F);
     }
 
     @Override
