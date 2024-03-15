a058a5c66a9f7f039ee4413d282c0de4de0ce70f
bconlon <bconlon.gml@gmail.com>
2024-03-15 15:15:09 -0700

    refactor: Minor adjustment to AccessoryItem and variants

--- src/main/java/com/aetherteam/aether/item/accessories/cape/CapeItem.java
+++ src/main/java/com/aetherteam/aether/item/accessories/cape/CapeItem.java
@@ -4,26 +4,17 @@ import com.aetherteam.aether.Aether;
 import com.aetherteam.aether.client.AetherSoundEvents;
 import com.aetherteam.aether.item.accessories.AccessoryItem;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
-import top.theillusivec4.curios.api.SlotContext;
-import top.theillusivec4.curios.api.type.capability.ICurio;
 
 public class CapeItem extends AccessoryItem {
     protected ResourceLocation CAPE_LOCATION;
 
     public CapeItem(String capeLocation, Properties properties) {
-        super(properties);
-        this.setRenderTexture(Aether.MODID, capeLocation);
+        this(new ResourceLocation(Aether.MODID, capeLocation), properties);
     }
 
     public CapeItem(ResourceLocation capeLocation, Properties properties) {
-        super(properties);
+        super(AetherSoundEvents.ITEM_ACCESSORY_EQUIP_CAPE, properties);
         this.setRenderTexture(capeLocation.getNamespace(), capeLocation.getPath());
-    }
-
-    @Override
-    public ICurio.SoundInfo getEquipSound(SlotContext slotContext, ItemStack stack) {
-        return new ICurio.SoundInfo(AetherSoundEvents.ITEM_ACCESSORY_EQUIP_CAPE.get(), 1.0F, 1.0F);
     }
 
     public void setRenderTexture(String modId, String registryName) {
--- src/main/java/com/aetherteam/aether/item/accessories/ring/RingItem.java
+++ src/main/java/com/aetherteam/aether/item/accessories/ring/RingItem.java
@@ -2,22 +2,11 @@ package com.aetherteam.aether.item.accessories.ring;
 
 import com.aetherteam.aether.item.accessories.AccessoryItem;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.item.ItemStack;
-import top.theillusivec4.curios.api.SlotContext;
-import top.theillusivec4.curios.api.type.capability.ICurio;
 
 import java.util.function.Supplier;
 
 public class RingItem extends AccessoryItem {
-    protected final Supplier<? extends SoundEvent> equipSound;
-
     public RingItem(Supplier<? extends SoundEvent> ringSound, Properties properties) {
-        super(properties);
-        this.equipSound = ringSound;
-    }
-
-    @Override
-    public ICurio.SoundInfo getEquipSound(SlotContext slotContext, ItemStack stack) {
-        return new ICurio.SoundInfo(this.equipSound.get(), 1.0F, 1.0F);
+        super(ringSound, properties);
     }
 }
