875d41aedd5044cc1e1be9e0cdf951aa6a9832ba
bconlon <bconlon.gml@gmail.com>
2024-03-15 15:08:45 -0700

    refactor: Minor adjustment to AccessoryItem and variants

--- src/main/java/com/aetherteam/aether/item/accessories/pendant/PendantItem.java
+++ src/main/java/com/aetherteam/aether/item/accessories/pendant/PendantItem.java
@@ -4,31 +4,19 @@ import com.aetherteam.aether.Aether;
 import com.aetherteam.aether.item.accessories.AccessoryItem;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.item.ItemStack;
-import top.theillusivec4.curios.api.SlotContext;
-import top.theillusivec4.curios.api.type.capability.ICurio;
 
 import java.util.function.Supplier;
 
 public class PendantItem extends AccessoryItem {
     protected ResourceLocation PENDANT_LOCATION;
-    protected final Supplier<? extends SoundEvent> equipSound;
 
     public PendantItem(String pendantLocation, Supplier<? extends SoundEvent> pendantSound, Properties properties) {
-        super(properties);
-        this.setRenderTexture(Aether.MODID, pendantLocation);
-        this.equipSound = pendantSound;
+        this(new ResourceLocation(Aether.MODID, pendantLocation), pendantSound, properties);
     }
 
     public PendantItem(ResourceLocation pendantLocation, Supplier<? extends SoundEvent> pendantSound, Properties properties) {
-        super(properties);
+        super(pendantSound, properties);
         this.setRenderTexture(pendantLocation.getNamespace(), pendantLocation.getPath());
-        this.equipSound = pendantSound;
-    }
-
-    @Override
-    public ICurio.SoundInfo getEquipSound(SlotContext slotContext, ItemStack stack) {
-        return new ICurio.SoundInfo(this.equipSound.get(), 1.0f, 1.0f);
     }
 
     public void setRenderTexture(String modId, String registryName) {
