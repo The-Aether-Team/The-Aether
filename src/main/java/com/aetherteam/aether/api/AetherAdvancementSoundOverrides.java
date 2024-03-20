2be536f8c2989612a112e654996e4eca789b005c
bconlon <bconlon.gml@gmail.com>
2024-03-19 17:19:37 -0700

    fix: missing import

--- src/main/java/com/aetherteam/aether/api/AetherAdvancementSoundOverrides.java
+++ src/main/java/com/aetherteam/aether/api/AetherAdvancementSoundOverrides.java
@@ -14,6 +14,7 @@ import net.minecraftforge.registries.RegistryBuilder;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class AetherAdvancementSoundOverrides {
