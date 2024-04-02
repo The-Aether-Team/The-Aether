720eb7934fa80077ffedf013fc84659e45266019
bconlon <bconlon.gml@gmail.com>
2024-04-01 17:46:23 -0700

    Revert "refactor: merge pro tip and trivia lines"
    This reverts commit 861d757b4731bd0a1e33558454a23a34c725285f.

--- src/main/java/com/aetherteam/aether/client/TriviaGenerator.java
+++ src/main/java/com/aetherteam/aether/client/TriviaGenerator.java
@@ -38,10 +38,21 @@ public class TriviaGenerator {
     }
 
     /**
-     * @return A {@link Component} for the trivia line at the current index.
+     * @return A constructed trivia line {@link Component} appended with "Pro Tip:", for display on the loading screen.
      */
     @Nullable
     public Component getTriviaLine() {
+        if (this.getTriviaComponent() != null) {
+            return Component.translatable("gui.aether.pro_tip").append(Component.literal(" ").append(this.getTriviaComponent()));
+        }
+        return null;
+    }
+
+    /**
+     * @return A {@link Component} for the trivia line at the current index.
+     */
+    @Nullable
+    private Component getTriviaComponent() {
         if (!this.getTrivia().isEmpty()) {
             return this.getTrivia().get(this.index);
         }
