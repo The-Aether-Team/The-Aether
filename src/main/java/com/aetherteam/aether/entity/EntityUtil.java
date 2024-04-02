20d5fad795191452ef464ddcbff5af3ad8036195
bconlon <bconlon.gml@gmail.com>
2024-04-01 19:07:01 -0700

    fix: lerp entity rotation copying

--- src/main/java/com/aetherteam/aether/entity/EntityUtil.java
+++ src/main/java/com/aetherteam/aether/entity/EntityUtil.java
@@ -5,6 +5,7 @@ import com.aetherteam.aether.mixin.mixins.common.accessor.EntityAccessor;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -19,10 +20,10 @@ public final class EntityUtil {
      * @param source The {@link Entity} to copy rotations from.
      */
     public static void copyRotations(Entity entity, Entity source) {
-        entity.setYRot(source.getYRot() % 360.0F);
-        entity.setXRot(source.getXRot() % 360.0F);
-        entity.setYBodyRot(source.getYRot());
-        entity.setYHeadRot(source.getYRot());
+        entity.setYRot((float) Mth.rotLerp(1.0 / 3.0, source.getYRot(), source.yRotO));
+        entity.setXRot((float) Mth.rotLerp(1.0 / 3.0, source.getXRot(), source.xRotO));
+        entity.setYBodyRot((float) Mth.rotLerp(1.0 / 3.0, source.getYRot(), source.yRotO));
+        entity.setYHeadRot((float) Mth.rotLerp(1.0 / 3.0, source.getYRot(), source.yRotO));
     }
 
     /**
