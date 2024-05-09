e24ad0d4864ef81fa1b28b67993fb556539700b4
Alexandra-Myers <78748498+Alexandra-Myers@users.noreply.github.com>
2024-05-09 12:13:02 -0400

    Revert FeatureMixin to not conflict with #2141

--- src/main/java/com/aetherteam/aether/mixin/mixins/common/FeatureMixin.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/common/FeatureMixin.java
@@ -1,30 +1,29 @@
 package com.aetherteam.aether.mixin.mixins.common;
 
 import com.aetherteam.aether.AetherTags;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelSimulatedReader;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Feature.class)
 public class FeatureMixin {
     /**
      * Prevents Aether Dirt from being replaced by Podzol.<br><br>
-     * Marked for deprecation as it will be replaced by a NeoForge event in 1.20.<br><br>
-     * Note from Alexandra: Improved despite deprecation because it's still not an event in this version.
+     * Marked for deprecation as it will be replaced by a NeoForge event in 1.20.
      *
-     * @param original Whether Podzol should replace this, before checking if it is Aether Dirt.
      * @param level The {@link LevelSimulatedReader} that is being checked in.
      * @param pos   The {@link BlockPos} to check for the block.
-     * @return Whether Podzol should replace this.
+     * @param cir   The {@link Boolean} {@link CallbackInfoReturnable} used for the method's return value.
      */
     @Deprecated(forRemoval = true)
-    @ModifyReturnValue(at = @At(value = "RETURN"), method = "isGrassOrDirt(Lnet/minecraft/world/level/LevelSimulatedReader;Lnet/minecraft/core/BlockPos;)Z")
-    private static boolean isGrassOrDirt(boolean original, @Local(ordinal = 0, argsOnly = true) LevelSimulatedReader level, @Local(ordinal = 0, argsOnly = true) BlockPos pos) {
-        if (level.isStateAtPosition(pos, state -> state.is(AetherTags.Blocks.AETHER_DIRT))) return false;
-        return original;
+    @Inject(at = @At(value = "HEAD"), method = "isGrassOrDirt(Lnet/minecraft/world/level/LevelSimulatedReader;Lnet/minecraft/core/BlockPos;)Z", cancellable = true)
+    private static void isGrassOrDirt(LevelSimulatedReader level, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (level.isStateAtPosition(pos, state -> state.is(AetherTags.Blocks.AETHER_DIRT))) {
+            cir.setReturnValue(false);
+        }
     }
 }
