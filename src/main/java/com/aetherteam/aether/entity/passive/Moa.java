2eec807f7f96b26934a9551cd177c7e447b9d3d0
bconlon <bconlon.gml@gmail.com>
2024-06-22 16:58:31 -0700

    refactor: moa checks isSaddled more

--- src/main/java/com/aetherteam/aether/entity/passive/Moa.java
+++ src/main/java/com/aetherteam/aether/entity/passive/Moa.java
@@ -321,7 +321,7 @@ public class Moa extends MountableAnimal implements WingedBird {
 		} else {
 			if (this.isAlive()) {
 				LivingEntity entity = this.getControllingPassenger();
-				if (this.isVehicle() && entity != null) {
+				if (this.isVehicle() && this.isSaddled() && entity != null) {
 					EntityUtil.copyRotations(this, entity);
 					if (this.isControlledByLocalInstance()) {
 						this.travelWithInput(new Vec3(0, vector.y(), 0));
@@ -730,7 +730,7 @@ public class Moa extends MountableAnimal implements WingedBird {
 	 */
 	@Override
 	public float getSpeed() {
-		if (this.isVehicle()) {
+		if (this.isVehicle() && this.isSaddled()) {
 			return this.getSteeringSpeed();
 		} else {
 			MoaType moaType = this.getMoaType();
@@ -773,7 +773,7 @@ public class Moa extends MountableAnimal implements WingedBird {
 	 */
 	@Override
 	public float getFlyingSpeed() {
-		if (this.isVehicle()) {
+		if (this.isVehicle() && this.isSaddled()) {
 			return this.getSteeringSpeed() * 0.45F;
 		} else {
 			return this.getSteeringSpeed() * 0.025F;
