193e32b48e96d12cfe1a9810300216bcab749a1d
bconlon <bconlon.gml@gmail.com>
2024-06-22 17:06:21 -0700

    fix: sun altar config should work maybe

--- src/main/java/com/aetherteam/aether/client/gui/component/inventory/SunAltarSlider.java
+++ src/main/java/com/aetherteam/aether/client/gui/component/inventory/SunAltarSlider.java
@@ -1,7 +1,5 @@
 package com.aetherteam.aether.client.gui.component.inventory;
 
-import com.aetherteam.aether.attachment.AetherDataAttachments;
-import com.aetherteam.aether.data.resources.registries.AetherDimensions;
 import com.aetherteam.aether.network.packet.serverbound.SunAltarUpdatePacket;
 import com.aetherteam.nitrogen.network.PacketRelay;
 import net.minecraft.client.gui.components.AbstractSliderButton;
@@ -10,18 +8,18 @@ import net.minecraft.world.level.Level;
 
 public class SunAltarSlider extends AbstractSliderButton {
     private final Level level;
+    private final int timeScale;
 
-    public SunAltarSlider(Level level, int x, int y, int width, int height, Component title, double value) {
+    public SunAltarSlider(Level level, int x, int y, int width, int height, Component title, double value, int timeScale) {
         super(x, y, width, height, title, value);
         this.level = level;
+        this.timeScale = timeScale;
     }
 
     @Override
     protected void applyValue() {
-        long time = (long) (this.value * AetherDimensions.AETHER_TICKS_PER_DAY);
-        if (this.level.hasData(AetherDataAttachments.AETHER_TIME)) {
-            PacketRelay.sendToServer(new SunAltarUpdatePacket(time));
-        }
+        long time = (long) (this.value * this.timeScale);
+        PacketRelay.sendToServer(new SunAltarUpdatePacket(time, this.timeScale));
     }
 
     @Override
--- src/main/java/com/aetherteam/aether/client/gui/screen/inventory/SunAltarScreen.java
+++ src/main/java/com/aetherteam/aether/client/gui/screen/inventory/SunAltarScreen.java
@@ -17,12 +17,12 @@ public class SunAltarScreen extends Screen {
         this.timeScale = timeScale;
     }
 
-    @Override
+    @Overrideltar
     public void init() {
         super.init();
         if (this.getMinecraft().level != null) {
             double sliderValue = (this.getMinecraft().level.getDayTime() % (long) this.timeScale) / (double) this.timeScale; // What position the slider bar should be at.
-            this.addRenderableWidget(new SunAltarSlider(this.getMinecraft().level, this.width / 2 - 75, this.height / 2, 150, 20, Component.translatable("gui.aether.sun_altar.time"), sliderValue));
+            this.addRenderableWidget(new SunAltarSlider(this.getMinecraft().level, this.width / 2 - 75, this.height / 2, 150, 20, Component.translatable("gui.aether.sun_altar.time"), sliderValue, this.timeScale));
         }
     }
 
