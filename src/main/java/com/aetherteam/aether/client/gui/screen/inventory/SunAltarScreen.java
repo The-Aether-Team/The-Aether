ee09f975c851e439e01531a1d519a91106207f73
bconlon <bconlon.gml@gmail.com>
2024-06-24 11:07:55 -0700

    feat: configurable sun altar dimension list

--- src/main/java/com/aetherteam/aether/client/gui/screen/inventory/SunAltarScreen.java
+++ src/main/java/com/aetherteam/aether/client/gui/screen/inventory/SunAltarScreen.java
@@ -2,7 +2,6 @@ package com.aetherteam.aether.client.gui.screen.inventory;
 
 import com.aetherteam.aether.Aether;
 import com.aetherteam.aether.client.gui.component.inventory.SunAltarSlider;
-import com.aetherteam.aether.data.resources.registries.AetherDimensions;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -11,17 +10,19 @@ import net.minecraft.util.FormattedCharSequence;
 
 public class SunAltarScreen extends Screen {
     private static final ResourceLocation TEXTURE = new ResourceLocation(Aether.MODID, "textures/gui/menu/sun_altar.png");
+    private final int timeScale;
 
-    public SunAltarScreen(Component title) {
+    public SunAltarScreen(Component title, int timeScale) {
         super(title);
+        this.timeScale = timeScale;
     }
 
     @Override
     public void init() {
         super.init();
         if (this.getMinecraft().level != null) {
-            double sliderValue = (this.getMinecraft().level.getDayTime() % (long) AetherDimensions.AETHER_TICKS_PER_DAY) / (double) AetherDimensions.AETHER_TICKS_PER_DAY; // What position the slider bar should be at.
-            this.addRenderableWidget(new SunAltarSlider(this.getMinecraft().level, this.width / 2 - 75, this.height / 2, 150, 20, Component.translatable("gui.aether.sun_altar.time"), sliderValue));
+            double sliderValue = (this.getMinecraft().level.getDayTime() % (long) this.timeScale) / (double) this.timeScale; // What position the slider bar should be at.
+            this.addRenderableWidget(new SunAltarSlider(this.getMinecraft().level, this.width / 2 - 75, this.height / 2, 150, 20, Component.translatable("gui.aether.sun_altar.time"), sliderValue, this.timeScale));
         }
     }
 
