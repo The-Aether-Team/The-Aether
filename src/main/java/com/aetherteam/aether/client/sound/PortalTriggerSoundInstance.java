eb78165efc010bc7f38217f3839e0dfbbff9fb91
bconlon <bconlon.gml@gmail.com>
2024-06-25 13:03:12 -0700

    improv: portal trigger fade handling

--- src/main/java/com/aetherteam/aether/client/sound/PortalTriggerSoundInstance.java
+++ src/main/java/com/aetherteam/aether/client/sound/PortalTriggerSoundInstance.java
@@ -1,17 +1,20 @@
 package com.aetherteam.aether.client.sound;
 
+import com.aetherteam.aether.attachment.AetherDataAttachments;
 import net.minecraft.client.resources.sounds.AbstractTickableSoundInstance;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.player.Player;
 
-public class FadeOutSoundInstance extends AbstractTickableSoundInstance {
+public class PortalTriggerSoundInstance extends AbstractTickableSoundInstance {
+    private final Player player;
     private final float startingVolume;
-    private boolean fadeOut;
     private int fade;
 
-    public FadeOutSoundInstance(
+    public PortalTriggerSoundInstance(
+        Player player,
         SoundEvent event,
         SoundSource source,
         float volume,
@@ -26,6 +29,7 @@ public class FadeOutSoundInstance extends AbstractTickableSoundInstance {
         boolean relative
     ) {
         super(event, source, random);
+        this.player = player;
         this.volume = volume;
         this.startingVolume = volume;
         this.pitch = pitch;
@@ -38,18 +42,33 @@ public class FadeOutSoundInstance extends AbstractTickableSoundInstance {
         this.relative = relative;
     }
 
+    public static PortalTriggerSoundInstance forLocalAmbience(Player player, SoundEvent pSound, float pVolume, float pPitch) {
+        return new PortalTriggerSoundInstance(
+            player,
+            pSound,
+            SoundSource.AMBIENT,
+            pPitch,
+            pVolume,
+            SoundInstance.createUnseededRandom(),
+            false,
+            0,
+            SoundInstance.Attenuation.NONE,
+            0.0,
+            0.0,
+            0.0,
+            true
+        );
+    }
+
     @Override
     public void tick() {
-        if (this.fadeOut) {
+        var data = this.player.getData(AetherDataAttachments.AETHER_PLAYER);
+        if (!data.isInPortal()) {
             this.fade++;
-            this.volume = (float) Math.exp(-(this.fade / (50 / 3.0))) - this.startingVolume;
-            if (this.fade >= 50) {
+            this.volume = (float) Math.exp(-(this.fade / (75 / 3.0))) - this.startingVolume;
+            if (this.fade >= 75) {
                 this.stop();
             }
         }
-    }
-
-    public void fadeOut() {
-        this.fadeOut = true;
     }
 }
