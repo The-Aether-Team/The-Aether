a894c6316877ded58f2adc697df6cd95a1a207d5
bconlon <bconlon.gml@gmail.com>
2024-08-31 10:34:04 -0700

    fix: apply gravity to darts in liquid

--- src/main/java/com/aetherteam/aether/entity/projectile/dart/AbstractDart.java
+++ src/main/java/com/aetherteam/aether/entity/projectile/dart/AbstractDart.java
@@ -55,6 +55,9 @@ public abstract class AbstractDart extends AbstractArrow {
                 this.discard();
             }
         }
+        if (!this.getLevel().getFluidState(this.blockPosition()).isEmpty()) {
+            this.setNoGravity(false);
+        }
     }
 
     /**
