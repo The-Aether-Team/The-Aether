71e200ddf9b180de086d769a8e27b174cd149613
bconlon <bconlon.gml@gmail.com>
2024-10-03 16:52:49 -0700

    feat: disc item

--- src/main/java/com/aetherteam/aether/client/AetherSoundEvents.java
+++ src/main/java/com/aetherteam/aether/client/AetherSoundEvents.java
@@ -58,6 +58,7 @@ public class AetherSoundEvents {
     public static final DeferredHolder<SoundEvent, SoundEvent> ITEM_MUSIC_DISC_CHINCHILLA = register("item.music_disc.chinchilla");
     public static final DeferredHolder<SoundEvent, SoundEvent> ITEM_MUSIC_DISC_HIGH = register("item.music_disc.high");
     public static final DeferredHolder<SoundEvent, SoundEvent> ITEM_MUSIC_DISC_KLEPTO = register("item.music_disc.klepto");
+    public static final DeferredHolder<SoundEvent, SoundEvent> ITEM_MUSIC_DISC_SLIDERS_WRATH = register("item.music_disc.sliders_wrath");
 
     public static final DeferredHolder<SoundEvent, SoundEvent> ENTITY_PHYG_AMBIENT = register("entity.phyg.ambient");
     public static final DeferredHolder<SoundEvent, SoundEvent> ENTITY_PHYG_DEATH = register("entity.phyg.death");
