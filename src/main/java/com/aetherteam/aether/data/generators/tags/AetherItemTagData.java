ed4334117c6f723de1daaf220282301b8c054a21
Katie "Oz" Payn <oscar@gildedgames.com>
2024-10-04 19:13:41 +0100

    feat: Slider's Wrath music disc (#2295)
    Co-authored-by: bconlon <bconlon.gml@gmail.com>

--- src/main/java/com/aetherteam/aether/data/generators/tags/AetherItemTagData.java
+++ src/main/java/com/aetherteam/aether/data/generators/tags/AetherItemTagData.java
@@ -396,7 +396,8 @@ public class AetherItemTagData extends ItemTagsProvider {
                 AetherItems.MUSIC_DISC_ASCENDING_DAWN.get(),
                 AetherItems.MUSIC_DISC_CHINCHILLA.get(),
                 AetherItems.MUSIC_DISC_HIGH.get(),
-                AetherItems.MUSIC_DISC_KLEPTO.get());
+                AetherItems.MUSIC_DISC_KLEPTO.get(),
+                AetherItems.MUSIC_DISC_SLIDERS_WRATH.get());
         this.tag(ItemTags.BOOKSHELF_BOOKS).add(AetherItems.BOOK_OF_LORE.get());
         this.tag(ItemTags.BEACON_PAYMENT_ITEMS).add(
                 AetherItems.ZANITE_GEMSTONE.get(),
