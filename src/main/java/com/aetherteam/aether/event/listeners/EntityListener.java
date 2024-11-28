7925857f1990586f36de33f79b317c6b0adfc055
Dragon-Seeker <timekeeperguild@gmail.com>
2024-11-28 15:40:59 -0600

    fix: adjust Projectile Impact event hook to only adjust event cancellation when invalid for hooking entities

--- src/main/java/com/aetherteam/aether/event/listeners/EntityListener.java
+++ src/main/java/com/aetherteam/aether/event/listeners/EntityListener.java
@@ -126,7 +126,7 @@ public class EntityListener {
     public static void onProjectileHitEntity(ProjectileImpactEvent event) {
         Entity projectileEntity = event.getEntity();
         HitResult rayTraceResult = event.getRayTraceResult();
-        event.setCanceled(EntityHooks.preventEntityHooked(projectileEntity, rayTraceResult));
+        if (EntityHooks.preventEntityHooked(projectileEntity, rayTraceResult)) event.setCanceled(true);
     }
 
     /**
