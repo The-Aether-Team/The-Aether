536f23d3c1d18fef8371ed7c322b611a41db7593
vaniron <hrdlicka.luk@gmail.com>
2025-06-04 11:31:18 +0200

    Fix ClassCastException in DoubleDropsModifier by handling DIRECT_ATTACKING_ENTITY correctly

--- src/main/java/com/aetherteam/aether/loot/modifiers/DoubleDropsModifier.java
+++ src/main/java/com/aetherteam/aether/loot/modifiers/DoubleDropsModifier.java
@@ -13,14 +13,14 @@ import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 
 public class DoubleDropsModifier extends LootModifier {
-    //public static final MapCodec<DoubleDropsModifier> CODEC = RecordCodecBuilder.mapCodec((instance) -> LootModifier.codecStart(instance).apply(instance, DoubleDropsModifier::new));
-
     public DoubleDropsModifier(LootItemCondition[] conditions) {
         super(conditions);
     }
 
     /**
-     * Doubles mob drops if a mob is attacked with full strength with an item that implements {@link SkyrootWeapon} if the mob isn't tagged with {@link AetherTags.Entities#NO_SKYROOT_DOUBLE_DROPS} and the item isn't tagged with {@link AetherTags.Items#NO_SKYROOT_DOUBLE_DROPS}.
+     * Doubles mob drops if a mob is attacked with full strength with an item that implements {@link SkyrootWeapon}
+     * if the mob isn't tagged with {@link AetherTags.Entities#NO_SKYROOT_DOUBLE_DROPS} and the item isn't tagged
+     * with {@link AetherTags.Items#NO_SKYROOT_DOUBLE_DROPS}.
      *
      * @param lootStacks Result items from a loot table as an {@link ObjectArrayList} of {@link ItemStack}s.
      * @param context    The {@link LootContext}.
@@ -28,23 +28,31 @@ public class DoubleDropsModifier extends LootModifier {
      */
     @Override
     public ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> lootStacks, LootContext context) {
-        Entity entity = context.getParamOrNull(LootContextParams.DIRECT_ATTACKING_ENTITY);
-        Entity target = context.getParamOrNull(LootContextParams.THIS_ENTITY);
         ObjectArrayList<ItemStack> newStacks = new ObjectArrayList<>(lootStacks);
-        if (entity instanceof LivingEntity livingEntity && target != null) {
-            if (EquipmentUtil.isFullStrength(livingEntity) && livingEntity.getMainHandItem().getItem() instanceof SkyrootWeapon && !target.getType().is(AetherTags.Entities.NO_SKYROOT_DOUBLE_DROPS)) {
-                for (ItemStack stack : lootStacks) {
-                    if (!stack.is(AetherTags.Items.NO_SKYROOT_DOUBLE_DROPS)) {
-                        newStacks.add(stack);
-                    }
+
+        // Get the entity that was killed
+        Entity target = context.getParamOrNull(LootContextParams.THIS_ENTITY);
+        if (target == null) {
+            return newStacks; // Return original loot if no target entity
+        }
+
+        // Get the attacking entity, which could be null or a DamageSource
+        Object attacker = context.getParamOrNull(LootContextParams.DIRECT_ATTACKING_ENTITY);
+        if (!(attacker instanceof LivingEntity livingEntity)) {
+            return newStacks; // Return original loot if attacker is not a LivingEntity
+        }
+
+        // Check conditions for doubling drops
+        if (EquipmentUtil.isFullStrength(livingEntity)
+            && livingEntity.getMainHandItem().getItem() instanceof SkyrootWeapon
+            && !target.getType().is(AetherTags.Entities.NO_SKYROOT_DOUBLE_DROPS)) {
+            for (ItemStack stack : lootStacks) {
+                if (!stack.is(AetherTags.Items.NO_SKYROOT_DOUBLE_DROPS)) {
+                    newStacks.add(stack.copy()); // Add a copy to avoid modifying the original stack
                 }
             }
         }
+
         return newStacks;
     }
-
-//    @Override
-//    public MapCodec<? extends IGlobalLootModifier> codec() {
-//        return DoubleDropsModifier.CODEC;
-//    }
 }
